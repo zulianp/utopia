7a8a310ac985fd8cbdb231ffa7127e6c94ac1c68
maria giuseppina chiara <nestom@usi.ch>
2017-02-03 15:02:18 +0100

    added assembling ---to be tested

--- utopia_fe/utopia_ContactSimParams.hpp
+++ utopia_fe/utopia_ContactSimParams.hpp
@@ -26,16 +26,16 @@ namespace utopia {
 	static const double LARGE_VAL = 1e8;
 
 	static const ContactSimParams contact18	  			  = { "../data/contact18.e", 1, 2, -1, 0.1, -0.2, 0.2, LARGE_VAL, false };
-	static const ContactSimParams contact8	  			  = { "../data/contact8.e", 1, 2, -1, 0.1, -0.2, 0.2, LARGE_VAL,false };
+	static const ContactSimParams contact8	  			  = { "../data/contact8.e", 1, 2, -1, 0.2, 0, 0, LARGE_VAL,false };
 	static const ContactSimParams contact8_tris	  		  = { "../data/contact8_tris.e", 1, 2, -1, 0.1, -0.2, 0.2, LARGE_VAL,false };
 	static const ContactSimParams contact_3D_tets	      = { "../data/contact_3D_tets.e", 1, 2, -1, 0.1, -0.2, 0.2, LARGE_VAL,false };
 	static const ContactSimParams contact_cylinder	  	  = { "../data/contact_cylinder.e", 1, 2, -1, 0.1, -0.2, 0.2, LARGE_VAL,false };
 	static const ContactSimParams contact_sphere	  	  = { "../data/contact_sphere.e", 1, 2, -1, 0.3, -0.2, 0.2, LARGE_VAL,false };
 	static const ContactSimParams contact_cuboids	  	  = { "../data/contact_cuboids.e", 1, 2, -1, 0.1, -0.2, 0.2, LARGE_VAL,false };
 	static const ContactSimParams triple_contact_circle	  = { "../data/triple_contact_circle.e", 1, 2, -1, 0.1, -0.2, 0.2, LARGE_VAL,false };
-	static const ContactSimParams contact2	  			  = { "../data/contact2.e", 1, 2, -1, 0.1, -0.2, 0.2, LARGE_VAL,false };
+	static const ContactSimParams contact2	  			  = { "../data/contact3D.e", 1, 2, -1, 0.1, -0.2, 0.2, LARGE_VAL,false };
 	static const ContactSimParams contact_quads	  		  = { "../data/contact_quads.e", 1, 2, -1, 0.1, -0.2, 0.2, LARGE_VAL,false };
-	static const ContactSimParams multi_contact	  		  = { "../data/multi_contact.e", 1, 2, -1, 0.1, -0.2, 0.2, LARGE_VAL,false };
+	static const ContactSimParams multi_contact	  		  = { "../data/multi_contact.e", 1, 2, -1, 0.05, 0.0, 0.0, LARGE_VAL,false };
 	static const ContactSimParams multi_contact_3D_3	  = { " /Users/patrick/Desktop/Code/moonolith_meshes/exodus/contact_ti_coarse.e", 1, 0, -1, 0.1, -0.2, 0.2, LARGE_VAL,false };
 	static const ContactSimParams leaflets_contact	  	  = { "../data/leaflets.e", 101, 102, 103, 0.1, 0.05, 0.05, 0.05, true }; 
 	static const ContactSimParams multi_contact_3D	  	  = { "/Users/patrick/Desktop/Code/moonolith_meshes/exodus/contact_mb_4.e", 1, 2, -1, 0.28, -0.59, 1.1, LARGE_VAL, false };
--- utopia_fe/utopia_FEDSLMortarExamples.cpp
+++ utopia_fe/utopia_FEDSLMortarExamples.cpp
@@ -308,9 +308,11 @@ namespace utopia {
 	void mortar_transfer_2D_monolithic(LibMeshInit &init)
 	{
 		auto mesh = make_shared<Mesh>(init.comm());
+        EXPRESS_EVENT_BEGIN("set_up");
 		//mesh->partitioner().reset(new LinearPartitioner());
 		mesh->read("../data/master_slave2D_new.e");
 		par_mortar_transfer_aux(init.comm(),mesh);
+        EXPRESS_EVENT_END("set_up");
 	}
 
 	// #define F_MINUS(x) (1.0-x)
@@ -441,12 +443,12 @@ namespace utopia {
 		EXPRESS_EVENT_BEGIN("set_up");
 		auto mesh = make_shared<Mesh>(init.comm());
 
-		mesh->partitioner().reset(new LinearPartitioner());
+		//mesh->partitioner().reset(new LinearPartitioner());
             // Read the mesh file. Here the file lshape.unv contains
             // an L--shaped domain in .unv format.
-       mesh->read("../data/cube12_space5.e"); //("../data/master_slave3D_translated.e");
-       // mesh->read("../data/master_slave3D_translated.e");
-
+       // mesh->read("../data/cube12_space5.e"); //("../data/master_slave3D_translated.e");
+       //mesh->read("../data/master_slave3D_translated.e");
+        mesh->read("../data/rect.e");
 
             // Print information about the mesh to the screen.
 		// mesh->print_info();
@@ -520,19 +522,19 @@ namespace utopia {
 		//////////////////////////////////////////////////
 		//////////////////////////////////////////////////
 
-   	// static const bool is_leaflet = true;
-   	// ContactSimParams params = leaflets_contact;
+		//ContactSimParams params = leaflets_contact;
+		 ContactSimParams params = contact8;
 
-   	static const bool is_leaflet = false;
-
-		// ContactSimParams params = contact8;
     	// ContactSimParams params = multi_contact_quads;
 		// ContactSimParams params = triple_contact_circle;
 		ContactSimParams params = multi_contact_3D;
 
-   	auto mesh = make_shared<Mesh>(init.comm());		
-   	mesh->read(params.mesh_path);
-   	plot_mesh(*mesh, "mesh");
+
+		auto mesh = make_shared<Mesh>(init.comm());		
+		mesh->read(params.mesh_path);
+//		plot_mesh(*mesh, "mesh");
+        
+
 
    	const int dim = mesh->mesh_dimension();
 
