252d5f82ac50d3e5942b64b62a4e2be617a1b902
Alena Kopanicakova <kopana@usi.ch>
2017-06-06 08:56:12 +0200

    merging

--- utopia/utils/utopia_Utils.hpp
+++ utopia/utils/utopia_Utils.hpp
@@ -69,6 +69,47 @@ namespace utopia
 	    return std::shared_ptr<const T>(&obj, EmptyDeleter<const T>());
 	}
 
+
+
+
+
+    enum ColorCode {
+        FG_RED      		= 31,
+        FG_GREEN    		= 32,
+        FG_YELLOW 			= 33,
+        FG_BLUE     		= 34,
+        FG_MAGENTA 			= 35, 
+		FG_CYAN 			= 36, 
+		FG_LIGHT_GRAY 		= 37,
+        FG_DEFAULT  		= 39,
+		FG_DARK_GRAY 		= 90, 
+		FG_LIGHT_RED 		= 91, 
+		FG_LIGHT_GREEN 		= 92, 
+		FG_LIGHT_YELLOW		= 93, 
+		FG_LIGHT_BLUE 		= 94, 
+		FG_LIGHT_MAGENTA	= 95, 
+		FG_LIGHT_CYAN 		= 96, 
+		FG_WHITE	 		= 97,
+
+        BG_RED      = 41,
+        BG_GREEN    = 42,
+        BG_BLUE     = 44,
+        BG_DEFAULT  = 49
+    };
+
+    class ColorModifier {
+        ColorCode code;
+    public:
+        ColorModifier(ColorCode pCode) : code(pCode) {}
+        friend std::ostream&
+        operator<<(std::ostream& os, const ColorModifier& mod) {
+            return os << "\033[" << mod.code << "m";
+        }
+    };
+
+
+
+
 	  /** @}*/
 }
 
