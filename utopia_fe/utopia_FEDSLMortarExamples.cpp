b7f6080d48189e06b4fece5afbe951bb7c93fe53
Patrick Zulian <patrick.zulian@gmail.com>
2017-06-20 16:02:50 +0200

    Fixed example

--- utopia_fe/utopia_FEDSLMortarExamples.cpp
+++ utopia_fe/utopia_FEDSLMortarExamples.cpp
@@ -325,8 +325,8 @@ namespace utopia {
         
         unsigned int variable_number = 0;
         
-        // MooseSurfaceAssemble(expressComm, (master_slave), utopia::make_ref(master_slave_context.system.get_dof_map()), utopia::make_ref(variable_number), matrix, orthogonal_trafos, gap, normals, is_contact_node, 0.01, 102, 101);
-        MooseSurfaceAssemble(expressComm, (master_slave), utopia::make_ref(master_slave_context.system.get_dof_map()), utopia::make_ref(variable_number), matrix, orthogonal_trafos, gap, normals, is_contact_node, 0.1, 1, 2);
+        MooseSurfaceAssemble(expressComm, (master_slave), utopia::make_ref(master_slave_context.system.get_dof_map()), utopia::make_ref(variable_number), matrix, orthogonal_trafos, gap, normals, is_contact_node, 0.01, 102, 101);
+        // MooseSurfaceAssemble(expressComm, (master_slave), utopia::make_ref(master_slave_context.system.get_dof_map()), utopia::make_ref(variable_number), matrix, orthogonal_trafos, gap, normals, is_contact_node, 0.1, 1, 2);
 
         
         DVectord v = local_zeros(local_size(matrix).get(1));
