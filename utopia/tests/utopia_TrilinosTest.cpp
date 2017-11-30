2b403a667bf779cbf4416bb0b94c3229cf1c1ff1
Nur Aiman Fadel <nur.fadel@cscs.ch>
2017-11-30 19:01:52 +0100

    commit trilinos test

--- utopia/tests/utopia_TrilinosTest.cpp
+++ utopia/tests/utopia_TrilinosTest.cpp
@@ -1,4 +1,5 @@
 #include "utopia_Base.hpp"
+<<<<<<< Updated upstream
 //#include "utopia_trilinos_Vector.hpp"
 #ifdef WITH_TRILINOS
 #include "utopia_TrilinosTest.hpp"
@@ -127,3 +128,37 @@ void run_trilinos_test() {}
   }*/
 }
 #endif //WITH_TRILINOS
+=======
+#include "utopia_TrilinosTest.hpp"
+
+#ifdef WITH_TRILINOS
+
+
+#include "utopia.hpp"
+
+using namespace Teuchos;
+
+void trilinos_example_test()
+{
+	RCP<const Comm<int> > comm = rcp(new MpiComm<int>(MPI_COMM_WORLD));
+  if (comm->getRank () == 0) {
+    // On (MPI) Process 0, print out the Tpetra software version.
+    std::cout << Tpetra::version () << std::endl << std::endl;
+  }
+}
+
+namespace utopia {
+	void run_trilinos_test()
+	{
+		UTOPIA_UNIT_TEST_BEGIN("TrilinosTest");
+		UTOPIA_RUN_TEST(trilinos_example_test);
+		UTOPIA_UNIT_TEST_END("TrilinosTest");
+	}
+}
+
+#else //WITH_TRILINOS
+namespace utopia {
+	void run_trilinos_test() {}
+}
+#endif //WITH_TRILINOS
+>>>>>>> Stashed changes
