9d84289f7235ee2dc3fca94af96c8842c4f89281
Patrick Zulian <patrick.zulian@gmail.com>
2018-02-27 12:26:23 +0100

    Added optimize_nnz for reducing zero entries in petsc matrix

--- utopia/backend/petsc/utopia_petsc.cpp
+++ utopia/backend/petsc/utopia_petsc.cpp
@@ -17,5 +17,48 @@ namespace utopia {
 	template class SemismoothNewton<DSMatrixd, DVectord, PETSC_EXPERIMENTAL>;
 
 	template class Multigrid<DSMatrixd, DVectord, PETSC_EXPERIMENTAL>;
+
+	void optimize_nnz(DSMatrixd &A)
+	{
+		auto rr = row_range(A);
+		auto cr = A.implementation().col_range();
+		auto ls = local_size(A);
+		auto gs = size(A);
+
+		std::vector<PetscInt> d_nnz(rr.extent(), 0), o_nnz(rr.extent(), 0);
+		each_read(A, [&](const utopia::SizeType i, const utopia::SizeType j, const PetscScalar val) {
+			if(std::abs(val) > 1e-18) {
+				if(cr.inside(j)) {
+					++d_nnz[i - rr.begin()];
+				} else {
+					++o_nnz[i - rr.begin()];
+				}
+			}
+		});
+
+		DSMatrixd A_opt;
+
+		A_opt.implementation().matij_init(
+			A.implementation().communicator(),
+			ls.get(0),
+			ls.get(1),
+			gs.get(0),
+			gs.get(1),
+			d_nnz,
+			o_nnz
+			);
+
+		{
+			Write<DSMatrixd> w_A(A_opt);
+			each_read(A, [&](const SizeType i, const SizeType j, const PetscScalar val) {
+				if(std::abs(val) > 1e-18) {
+					A_opt.set(i, j, val);
+				}
+			});
+
+		}
+
+		A = std::move(A_opt);
+	}
 }
 
