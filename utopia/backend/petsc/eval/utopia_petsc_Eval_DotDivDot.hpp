941e52fe15ef7284a336dbd37393b77fb8c868dd
Patrick Zulian <patrick.zulian@gmail.com>
2018-03-23 14:29:02 +0100

    More generla DotDivDot

--- utopia/backend/petsc/eval/utopia_petsc_Eval_DotDivDot.hpp
+++ utopia/backend/petsc/eval/utopia_petsc_Eval_DotDivDot.hpp
@@ -19,16 +19,14 @@
 // |	|	Plus
 
 namespace utopia {
-	template<class X, class A, class Traits>
+	template<class X, class FunOfX, class Traits>
 	class Eval< Binary< 
 					Dot<Wrapper<X, 1>, Wrapper<X, 1> >, 
-					Dot<Multiply<Wrapper<A, 2>, Wrapper<X, 1>>, Wrapper<X, 1> >,
+					Dot<FunOfX, Wrapper<X, 1> >,
 					Divides>, Traits, PETSC> {
 	public:
-		typedef utopia::Multiply<Wrapper<A, 2>, Wrapper<X, 1>> MultT;
-
 		typedef Binary< Dot<Wrapper<X, 1>, Wrapper<X, 1> >, 
-					    Dot<Multiply<Wrapper<A, 2>, Wrapper<X, 1>>, Wrapper<X, 1> >,
+					    Dot<FunOfX, Wrapper<X, 1> >,
 					    Divides> Expr;
 
 	    typedef typename Traits::Scalar Scalar;
@@ -39,18 +37,37 @@ namespace utopia {
 			const auto &x1 = expr.left().expr().left().implementation();
 			const auto &x2 = expr.left().expr().right().implementation();
 
-			auto &&x3 = Eval<MultT, Traits>::apply(expr.right().expr().left());
+			auto &&x3 = Eval<FunOfX, Traits>::apply(expr.right().expr().left());
 			const auto &x4 = expr.right().expr().right().implementation();
 
+
 			PetscScalar result_num = 0., result_denom = 0.;
-			auto ierr = VecDotBegin(x1.implementation(), x2.implementation(), &result_num); assert(ierr == 0);
-			ierr = VecDotBegin(x3.implementation(), x4.implementation(), &result_denom); assert(ierr == 0);
 
+			PetscErrorCode ierr = 0;
+			if(x1.implementation() == x4.implementation()) {
+				Vec vecs[2] = { x2.implementation(), x3.implementation() };
+				PetscScalar vals[2] = { 0., 0.};
+				ierr = VecMDot(x1.implementation(), 2, vecs, vals); assert(ierr == 0);
+				result_num = vals[0];
+				result_denom = vals[1];
+			} else if(x2.implementation() == x4.implementation()) {
+				Vec vecs[2] = { x1.implementation(), x3.implementation() };
+				PetscScalar vals[2] = { 0., 0.};
+				ierr = VecMDot(x2.implementation(), 2, vecs, vals); assert(ierr == 0);
+				result_num = vals[0];
+				result_denom = vals[1];
+			} else { 
+				ierr  = VecDotBegin(x1.implementation(), x2.implementation(), &result_num); assert(ierr == 0);
+				ierr = VecDotBegin(x3.implementation(), x4.implementation(), &result_denom); assert(ierr == 0);
 
-			ierr = VecDotEnd(x1.implementation(), x2.implementation(), &result_num); assert(ierr == 0);
-			ierr = VecDotEnd(x3.implementation(), x4.implementation(), &result_denom); assert(ierr == 0);
-
+				ierr = VecDotEnd(x1.implementation(), x2.implementation(), &result_num); assert(ierr == 0);
+				ierr = VecDotEnd(x3.implementation(), x4.implementation(), &result_denom); assert(ierr == 0);
+			}
+			
 			Scalar r = result_num/result_denom;
+			if(result_denom == 0. || ierr != 0) {
+				r = 0.;
+			}
 
 			UTOPIA_LOG_END(expr);
 			return r;
