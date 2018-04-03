907625428d5472398180792b7c8f234581b68ad3
Patrick Zulian <patrick.zulian@gmail.com>
2018-04-03 15:03:09 +0200

    Added test for TaoSolver

--- utopia/backend/petsc/solvers/nonlinear/utopia_petsc_TaoSolver.hpp
+++ utopia/backend/petsc/solvers/nonlinear/utopia_petsc_TaoSolver.hpp
@@ -5,6 +5,7 @@
 #include "utopia_NonLinearSolver.hpp"
 #include "utopia_petsc_ForwardDeclarations.hpp"
 #include "utopia_petsc_Types.hpp"
+#include "utopia_petsc_Function.hpp"
 #include <mpi.h>
 
 namespace utopia {
@@ -15,7 +16,7 @@ namespace utopia {
 		void destroy();
 		bool init(MPI_Comm comm);
 		bool set_bounds(const PetscVector &lb, const PetscVector &ub);
-		bool solve();
+		bool solve(PetscVector &x);
 
 		void set_function(Function<DMatrixd, DVectord> &fun);
 		void set_function(Function<DSMatrixd, DVectord> &fun);
@@ -30,8 +31,27 @@ namespace utopia {
 	public:
 		typedef utopia::BoxConstraints<Vector> BoxConstraints;
 
+		TaoSolver(const std::shared_ptr<LinearSolver<Matrix, Vector>> &linear_solver)
+		: NonLinearSolver<Matrix, Vector>(linear_solver)
+		{}
+
+		virtual bool solve(Function<Matrix, Vector> &fun, Vector &x)
+		{
+			impl_.init(x.implementation().communicator());
+			
+			if(box_constraints_.has_bound()) {
+				impl_.set_bounds(
+					box_constraints_.lower_bound()->implementation(),
+					box_constraints_.upper_bound()->implementation()
+				);
+			}
+
+			impl_.set_function(fun);
+			return impl_.solve(x.implementation());
+		}
 
 	private:
+		TaoSolverWrapper impl_;
 		BoxConstraints box_constraints_;
 	};
 }
