f1733535f2f029abc7abcdc8654d3c6f339f17bb
Patrick Zulian <patrick.zulian@gmail.com>
2018-04-03 15:49:19 +0200

    Added unstested example for Monodomain model

--- utopia_fe/functions/utopia_MonodomainModel.hpp
+++ utopia_fe/functions/utopia_MonodomainModel.hpp
@@ -3,56 +3,78 @@
 
 #include "utopia.hpp"
 #include "utopia_fe_core.hpp"
+#include "utopia_libmesh_NonLinearFEFunction.hpp"
 
 namespace utopia {
-    template<class Matrix, class Vector, int Backend = Traits<Vector>::Backend>
-    class MonodomainModel : public Function <Matrix, Vector, Backend> {
+    template<class FunctionSpace, class Matrix, class Vector, int Backend = Traits<Vector>::Backend>
+    class MonodomainModel : public Function<Matrix, Vector> {
     public:
-        DEF_UTOPIA_SCALAR(Matrix); 
-
+        DEF_UTOPIA_SCALAR(Matrix);
+        
         virtual ~MonodomainModel() { }
-
-        virtual bool value(const Vector &point, Scalar &value) const
+        
+        virtual bool value(const Vector &x, Scalar &value) const
         {
             //FIXME
-            return 0;
+            assert(false && "implement me");
+            return -1;
         }
-
-        virtual bool gradient(const Vector &point, Vector &result) const
-        {
-
-        }
-
-        virtual bool hessian(const Vector &x, Matrix &H) const
+        
+        virtual bool gradient(const Vector &x, Vector &result) const
         {
             auto u = trial(*V_);
             auto v = test(*V_);
 
-            if(empty(lapl_)) {
-                auto b_form = (inner(grad(u), grad(v)) * dX);
-                utopia::assemble(b_form, lapl_);
-                lapl_ *= diffusivity_;
-                // JacIonCurrent_ = lapl_;
-            }
+            auto uk     = interpolate(x, u);
+            auto u_min  = coeff(u_min_);
+            auto u_max  = coeff(u_max_);
+            auto u_unst = coeff(u_unst_);
 
-            // JacIonCurrent_ *= 0.;
+            auto l = (uk - u_min) * (uk - u_max) * (uk - u_unst);
+            auto f_form = alpha_ * inner(l, v) * dX;
 
-
-            auto uk = interpolate(x, u);
-
-            auto c1 = coeff(u_unst_ - u_max_ - u_min_);
-            auto c2 = coeff(u_min_ * u_max_ + u_min_ * u_unst_ - u_max_ * u_unst_);
-            auto j_form = alpha * inner(u * u - u * c1 + c2, v) * dX;
-            utopia::assemble(j_form, JacIonCurrent_);
-
-            H = lapl_ + JacIonCurrent_;
+            utopia::assemble(f_form, result);
+            result -= lapl_ * x;
+            return false;
         }
+        
+        virtual bool hessian(const Vector &x, Matrix &H) const
+        {
+            auto u = trial(*V_);
+            auto v = test(*V_);
+            
+            auto uk     = interpolate(x, u);
+            auto u_min  = coeff(u_min_);
+            auto u_max  = coeff(u_max_);
+            auto u_unst = coeff(u_unst_);
+            
+            auto l = ( (uk - u_min) * (uk - u_max) + (uk - u_min) * (uk - u_unst) + (uk - u_max) * (uk - u_unst) );
+            auto j_form = alpha_ * inner(l * u, v) * dX;
+            
+            Matrix deriv_Ion;
+            utopia::assemble(j_form, deriv_Ion);
+            
+            H = lapl_ - deriv_Ion;
+            return true;
+        }
+        
+        void initialize(const std::shared_ptr<FunctionSpace> &V)
+        {
+            V_ = V;
 
+            auto u = trial(*V_);
+            auto v = test(*V_);
+            
+            auto b_form = (inner(grad(u), grad(v)) * dX);
+            utopia::assemble(b_form, lapl_);
+            lapl_ *= diffusivity_;
+        }
+        
         void set_function_space(const std::shared_ptr<FunctionSpace> &V)
         {
             V_ = V;
         }
-
+        
         void set_diffusivity(const Scalar diffusivity)
         {
             diffusivity_ = diffusivity;
@@ -60,19 +82,19 @@ namespace utopia {
         
         void set_ion_current_params(const Scalar alpha, const Scalar u_min, const Scalar u_max, const Scalar u_unst)
         {
-           alpha_ = alpha;
-           u_min_ = u_min;
-           u_max_ = u_max;
-           u_unst_ = u_unst;
-       }
-
-   private:
-    std::shared_ptr<FunctionSpace> V_;
-    Matrix lapl_;
-    Matrix JacIonCurrent_;
-    Scalar diffusivity_;
-    Scalar u_min_, u_max_, u_unst_;
-    Scalar alpha_;
-};
+            alpha_ = alpha;
+            u_min_ = u_min;
+            u_max_ = u_max;
+            u_unst_ = u_unst;
+        }
+        
+    private:
+        std::shared_ptr<FunctionSpace> V_;
+        Matrix lapl_;
+        Matrix deriv_Ion;
+        Scalar diffusivity_;
+        Scalar u_min_, u_max_, u_unst_;
+        Scalar alpha_;
+    };
 }
 #endif //UTOPIA_MONODOMAIN_MODEL_HPP
