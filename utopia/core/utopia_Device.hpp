d38be7aa157312eda78fb149c5263d8516ca7e97
Patrick Zulian <patrick.zulian@gmail.com>
2018-04-10 17:32:25 +0200

    ...

---
+++ utopia/core/utopia_Device.hpp
@@ -0,0 +1,14 @@
+#ifndef UTOPIA_DEVICE_HPP
+#define UTOPIA_DEVICE_HPP
+
+namespace utopia {
+	class MultiCore {};
+	class GPU {};
+
+
+	namespace device {
+		static utopia::GPU gpu;
+	}
+}
+
+#endif //UTOPIA_DEVICE_HPP
