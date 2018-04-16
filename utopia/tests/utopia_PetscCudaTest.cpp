b54bde3de16dc82105abdddf2a1b3103f5658e64
zulianp <patrick.zulian@gmail.com>
2018-04-16 15:29:24 +0200

    comparison with standard and cuda petsc backends

--- utopia/tests/utopia_PetscCudaTest.cpp
+++ utopia/tests/utopia_PetscCudaTest.cpp
@@ -20,19 +20,54 @@ namespace utopia {
         CuVectord y;
         y = m * x;
 
-        // disp(y);
-
         CuVectord sol = local_zeros(local_size(y).get(0));
 
         //test mat-residual
         ConjugateGradient<CuSMatrixd, CuVectord, HOMEMADE>  cg;
-        cg.verbose(true);
         cg.max_it(n);
         cg.solve(m, y, sol);
 
         CuVectord r = y - m * sol; 
         double err = norm2(r);
         assert(err < 1e-10);
+    }
+
+    template<class Matrix, class Vector>
+    static void generic_linear_solver_test()
+    {
+        // SizeType n = 1000;
+        SizeType n = 10000;
+        Size ls{ n, n };
+        Size gs{ ls.get(0) * mpi_world_size(), ls.get(1) * mpi_world_size() };
+
+        Matrix m = local_sparse(ls.get(0), ls.get(1), 3);
+        assemble_laplacian_1D(size(m).get(0), m);
+
+        Vector x = local_values(ls.get(0), 0.);
+        Vector b = local_values(ls.get(0), 1.);
+
+        //test mat-residual
+        KSPSolver<Matrix, Vector> ksp;
+        // ksp.pc_type("sor");
+        ksp.pc_type("jacobi");
+        ksp.ksp_type("cg");
+        ksp.max_it(n);
+        ksp.solve(m, b, x);
+
+        Vector r = b - m * x; 
+        double err = norm2(r);
+        assert(err < 1e-10);
+    }
+
+    void petsc_cuda_linear_solver()
+    {
+       generic_linear_solver_test<CuSMatrixd, CuVectord>();
+    }
+
+    void petsc_no_cuda_linear_solver()
+    {
+        //normal matrices for comparing performance
+        generic_linear_solver_test<DSMatrixd, DVectord>();
     }
 
     void petsc_cuda_mg()
@@ -64,7 +99,7 @@ namespace utopia {
         multigrid.atol(1e-10);
         multigrid.stol(1e-10);
         multigrid.rtol(1e-10);
-        multigrid.verbose(true);
+  //      multigrid.verbose(true);
 
         CuVectord x = zeros(A.size().get(0));
         multigrid.update(make_ref(A));
@@ -81,8 +116,10 @@ namespace utopia {
     void run_petsc_cuda_test() {
 #ifdef PETSC_HAVE_CUDA
         UTOPIA_UNIT_TEST_BEGIN("PetscCudaTest");
-        UTOPIA_RUN_TEST(petsc_cuda_init);
-        UTOPIA_RUN_TEST(petsc_cuda_mg);
+        // UTOPIA_RUN_TEST(petsc_cuda_init);
+        // UTOPIA_RUN_TEST(petsc_cuda_mg);
+        UTOPIA_RUN_TEST(petsc_cuda_linear_solver);
+        UTOPIA_RUN_TEST(petsc_no_cuda_linear_solver);
         UTOPIA_UNIT_TEST_END("PetscCudaTest");
 #endif //PETSC_HAVE_CUDA
     }
