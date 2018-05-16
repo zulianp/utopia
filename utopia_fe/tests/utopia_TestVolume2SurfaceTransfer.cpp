2bf8b387dc6f15f9ac120b8dc7df03fa2838ace7
Patrick Zulian <patrick.zulian@gmail.com>
2018-05-16 11:17:57 +0200

    Added adaptive refinement hack

--- utopia_fe/tests/utopia_TestVolume2SurfaceTransfer.cpp
+++ utopia_fe/tests/utopia_TestVolume2SurfaceTransfer.cpp
@@ -7,23 +7,101 @@
 #include "libmesh/mesh_generation.h"
 #include "libmesh/nemesis_io.h"
 #include "libmesh/mesh_refinement.h"
+#include <algorithm>
 
 
 typedef utopia::LibMeshFunctionSpace FunctionSpaceT;
 
 namespace utopia {
+	void refine_around_fractures(
+		const std::shared_ptr<libMesh::UnstructuredMesh> &fracture_network,
+		const libMesh::Order &elem_order,
+		const std::shared_ptr<libMesh::UnstructuredMesh> &mesh,
+		const int refinement_loops = 1
+		)
+	{
+
+		for(int i = 0; i < refinement_loops; ++i) {
+		//equations system
+			auto vol_equation_systems = std::make_shared<libMesh::EquationSystems>(*mesh);
+			auto &vol_sys = vol_equation_systems->add_system<libMesh::LinearImplicitSystem>("vol_sys");
+
+			auto surf_equation_systems = std::make_shared<libMesh::EquationSystems>(*fracture_network);
+			auto &surf_sys = surf_equation_systems->add_system<libMesh::LinearImplicitSystem>("surf_sys");
+
+		//scalar function space
+			auto V_vol  = FunctionSpaceT(vol_equation_systems, libMesh::LAGRANGE, elem_order,      "u_vol");
+			auto V_surf = FunctionSpaceT(surf_equation_systems, libMesh::LAGRANGE, libMesh::FIRST, "u_surf");
+
+			V_vol.initialize();
+			V_surf.initialize();
+
+			Chrono c;
+			c.start();
+			DSMatrixd B;
+			moonolith::Communicator comm(mesh->comm().get());
+			if(assemble_volume_transfer(
+				comm,
+				mesh,
+				fracture_network,
+				make_ref(V_vol.dof_map()),
+				make_ref(V_surf.dof_map()),
+				0,
+				0,
+				true, 
+				1,
+				B))
+			{
+				c.stop();
+				std::cout << c << std::endl;
+
+				DSMatrixd D_inv = diag(1./sum(B, 1));
+				DSMatrixd T = D_inv * B;
+
+				DSMatrixd T_t = transpose(T);
+				DVectord t_temp = sum(T_t, 1);
+				DVectord t = ghosted(local_size(t_temp).get(0), size(t_temp).get(0), V_vol.dof_map().get_send_list());
+				t = t_temp;
+
+
+				std::vector<libMesh::dof_id_type> indices;
+				std::vector<double> values;
+
+				Read<DVectord> r_(t);
+				for(auto e_it = elements_begin(*mesh); e_it != elements_end(*mesh); ++e_it) {
+					V_vol.dof_map().dof_indices(*e_it, indices);
+					t.get(indices, values);
+
+					double val = std::accumulate(values.begin(), values.end(), 0.,  std::plus<double>());
+					if(val > 0) {
+						(*e_it)->set_refinement_flag(libMesh::Elem::REFINE);
+					}
+
+				}
+
+				libMesh::MeshRefinement mesh_refinement(*mesh);
+				mesh_refinement.make_flags_parallel_consistent();
+				mesh_refinement.refine_elements();
+
+			} else {
+				assert(false);
+			}
+		}
+	}
+
+
 	void run_volume_to_surface_transfer_test(libMesh::LibMeshInit &init)
 	{
-		auto n = 5;
+		auto n = 3;
 		// auto elem_type  = libMesh::TET10;
-		auto elem_type  = libMesh::TET4;
-		// auto elem_type  = libMesh::HEX8;
+		// auto elem_type  = libMesh::TET4;
+		auto elem_type  = libMesh::HEX8;
 		
 		auto elem_order = libMesh::FIRST;
 		// auto elem_order = libMesh::SECOND;
 
 		// bool is_test_case = true;
-		bool is_test_case = false;
+		bool is_test_case = true;
 
 		auto vol_mesh = std::make_shared<libMesh::DistributedMesh>(init.comm());	
 		auto surf_mesh = std::make_shared<libMesh::DistributedMesh>(init.comm());	
@@ -33,9 +111,12 @@ namespace utopia {
 			libMesh::MeshTools::Generation::build_cube(
 				*vol_mesh,
 				n, n, n,
-				0., 1.,
-				0., 1.,
-				-0.5, 0.5,
+				// 0., 1.,
+				// 0., 1.,
+				// -0.5, 0.5,
+				-0.1, 1.1,
+				-0.1, 1.1,
+				-0.6, 0.6,
 				elem_type
 				);
 			// surf_mesh->read("../data/test/square_with_2_tri.e");
@@ -46,6 +127,8 @@ namespace utopia {
 			libMesh::MeshRefinement mesh_refinement(*surf_mesh);
 			mesh_refinement.make_flags_parallel_consistent();
 			mesh_refinement.uniformly_refine(4);
+
+			refine_around_fractures(surf_mesh, elem_order, vol_mesh, 4);
 		} else {
 			
 			libMesh::MeshTools::Generation::build_cube(
@@ -113,7 +196,7 @@ namespace utopia {
 					double x = pts[i](0) - 0.5;
 					double y = pts[i](1) - 0.5;
 					double z = pts[i](2);
-					ret[i] = x*(x*x + y*y + z*z);
+					ret[i] = sin(x)*(x*x + y*y + z*z);
 				}
 
 				return ret;
@@ -130,7 +213,12 @@ namespace utopia {
 
 			utopia::assemble(p_form, scaled_sol);
 			utopia::assemble(m_form, mass_mat);
-			Factorization<DSMatrixd, DVectord>().solve(mass_mat, scaled_sol, v_vol);
+
+			if(elem_order == libMesh::FIRST) {
+				v_vol = e_mul(1./sum(mass_mat, 1), scaled_sol);
+			} else {
+				Factorization<DSMatrixd, DVectord>().solve(mass_mat, scaled_sol, v_vol);
+			}
 
 			DVectord v_surf = T * v_vol;
 
