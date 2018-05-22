82fb2fb6e1d2a8d18d0135bc3c9804c60c33a55a
Patrick Zulian <patrick.zulian@gmail.com>
2018-05-22 14:17:17 +0200

    ...

--- utopia/tests/utopia_TaoSolverTest.cpp
+++ utopia/tests/utopia_TaoSolverTest.cpp
@@ -118,7 +118,8 @@ namespace utopia {
 		auto box = make_upper_bound_constraints(make_ref(upper_bound));
 		QuadraticFunction<DSMatrixd, DVectord> fun(make_ref(m), make_ref(rhs));
 
-		auto lsolver = std::make_shared<LUDecomposition<DSMatrixd, DVectord> >();
+		// auto lsolver = std::make_shared<LUDecomposition<DSMatrixd, DVectord> >();
+		auto lsolver = std::make_shared<BiCGStab<DSMatrixd, DVectord> >();
         auto qp_solver = std::make_shared<TaoTRSubproblem<DSMatrixd, DVectord> >(lsolver); 
 
         TrustRegionVariableBound<DSMatrixd, DVectord>  tr_solver(qp_solver); 
