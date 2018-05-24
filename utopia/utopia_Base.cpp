45582a76166c33a335897675f84b09d11913ea91
Patrick Zulian <patrick.zulian@gmail.com>
2018-05-24 17:54:10 +0200

    ...

---
+++ utopia/utopia_Base.cpp
@@ -0,0 +1,15 @@
+#include "utopia_Base.hpp"
+#include "utopia_Instance.hpp"
+
+#include <iostream>
+
+#ifdef NDEBUG
+
+namespace utopia {
+	void test_check_assertion(const bool expr, const std::string &filename, const int line, const std::string &expr_string)
+	{
+		if(!expr) { utopia::Utopia::instance().set_exit_code(1); std::cerr << filename << ": " << line << "\ntest failure: " << expr_string << std::endl; }
+	}
+}
+
+#endif //NDEBUG
