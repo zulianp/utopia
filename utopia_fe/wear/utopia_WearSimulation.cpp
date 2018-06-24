c0b108d82a98cd9370730091a2e6c2cdaaac976b
Patrick Zulian <patrick.zulian@gmail.com>
2018-06-24 14:17:35 -0700

    ...

--- utopia_fe/wear/utopia_WearSimulation.cpp
+++ utopia_fe/wear/utopia_WearSimulation.cpp
@@ -8,6 +8,8 @@
 #include "utopia_LameeParameters.hpp"
 #include "utopia_ElasticMaterial.hpp"
 #include "utopia_LinearElasticity.hpp"
+#include "utopia_NeoHookean.hpp"
+#include "utopia_SaintVenantKirchoff.hpp"
 
 #include <iostream>
 #include <fstream>
@@ -17,6 +19,7 @@ namespace utopia {
 	public:
 
 		Impl()
+		: output_path("./")
 		{}
 
 		bool run(libMesh::LibMeshInit &init, std::istream &is)
@@ -40,6 +43,8 @@ namespace utopia {
 				return false;
 			}
 
+			std::cout << "mesh:   " << xml_mesh->value() << std::endl;
+
 			mesh = std::make_shared<libMesh::DistributedMesh>(init.comm());
 			mesh->read(xml_mesh->value());
 			auto dim = mesh->mesh_dimension();
@@ -57,6 +62,31 @@ namespace utopia {
 				V *= LibMeshFunctionSpace(equation_systems, libMesh::LAGRANGE, elem_order, "disp_z");
 			}
 
+			/////////////////////////////////////////////////////////////////////////////
+
+
+			xml_node<> *xml_bc = wear_simulation->first_node("boundary-conditions");
+			xml_node<> *xml_dirichlet = xml_bc->first_node("dirichlet");
+
+			for (xml_node<> *xml_cond = xml_dirichlet->first_node(); xml_cond; xml_cond = xml_cond->next_sibling())
+			{
+				xml_attribute<> *cond_side_attr  = xml_cond->first_attribute("side"); assert(cond_side_attr);
+				xml_attribute<> *cond_coord_attr = xml_cond->first_attribute("coord"); assert(cond_coord_attr);
+
+				auto side_set = atoi(cond_side_attr->value());
+				auto coord    = atoi(cond_coord_attr->value());
+				auto value    = atof(xml_cond->value());
+
+				std::cout << side_set << " " << coord << " " << value << std::endl;
+
+				auto u = trial(V[coord]);
+				init_constraints(constraints(
+					boundary_conditions(u == coeff(value), {side_set})
+				));
+			}
+
+			/////////////////////////////////////////////////////////////////////////////
+
 			xml_node<> *xml_model 	   = wear_simulation->first_node("model");
 			xml_node<> *xml_material   = xml_model->first_node("material");
 			xml_node<> *xml_parameters = xml_model->first_node("parameters");
@@ -71,30 +101,36 @@ namespace utopia {
 				params = LameeParameters(mu, lambda);
 			}
 
-
-			if(xml_model) {
-				if(xml_model->value() == std::string("NeoHookean")) {
-
+			if(xml_material) {
+				if(xml_material->value() == std::string("NeoHookean")) {
+					std::cout << "material: NeoHookean" << std::endl;
+					material = std::make_shared<NeoHookean<decltype(V), DSMatrixd, DVectord>>(V, params);
 				} else if(xml_model->value() == std::string("SaintVenantKirchoff")) {
-
+					std::cout << "material: SaintVenantKirchoff" << std::endl;
+					material = std::make_shared<SaintVenantKirchoff<decltype(V), DSMatrixd, DVectord>>(V, params);
 				} else //if(mode->value() == "LinearElasticity") 
 				{
+					std::cout << "material: LinearElasticity" << std::endl;
 					material = std::make_shared<LinearElasticity<decltype(V), DSMatrixd, DVectord>>(V, params);
 				}
+			} else {
+				material = std::make_shared<LinearElasticity<decltype(V), DSMatrixd, DVectord>>(V, params);
 			}
 
-			std::cout << "mesh:   " << xml_mesh->value() << std::endl;
+			/////////////////////////////////////////////////////////////////////////////////////
+
+			
 			params.describe(std::cout);
 			return true;
 		}
 
 		std::string mesh_path;
+		std::string output_path;
 		LameeParameters params;
 		std::shared_ptr<libMesh::DistributedMesh> mesh;
 		ProductFunctionSpace<LibMeshFunctionSpace> V;
 		std::shared_ptr<libMesh::EquationSystems> equation_systems;
 		std::shared_ptr<ElasticMaterial<DSMatrixd, DVectord> > material;
-
 	};
 
 	void WearSimulation::run(libMesh::LibMeshInit &init, const std::string &xml_file_path)
