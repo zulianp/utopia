24c3b5fc496d132fc69d0f5ef76e8c92f098a6f9
Patrick Zulian <patrick.zulian@gmail.com>
2018-09-07 15:41:08 +0200

    ...

--- utopia/ui/utopia_ui.cpp
+++ utopia/ui/utopia_ui.cpp
@@ -8,14 +8,15 @@ namespace utopia {
 	std::unique_ptr<InputStream> open_istream(const Path &path)
 	{
 		if(path.extension() == "xml") {
-			auto ret = make_unique<XMLInputStream>();
-			ret->open(path);
-			return ret;
-			
-			// auto ret = new XMLInputStream();
-			// auto ret_ptr = std::unique_ptr<InputStream>(ret);
+			// auto ret = make_unique<XMLInputStream>();
 			// ret->open(path);
-			// return ret_ptr;
+			// return ret;
+			
+			//portable version when compuling with nvcc
+			auto ret = new XMLInputStream();
+			auto ret_ptr = std::unique_ptr<InputStream>(ret);
+			ret->open(path);
+			return ret_ptr;
 		} else {
 			std::cerr << "[Error] format not supported" << std::endl;
 			return nullptr;
