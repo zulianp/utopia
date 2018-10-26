b799f6024da2277e39172e6bc6cd0d84df4610a5
Patrick Zulian <patrick.zulian@gmail.com>
2018-10-26 15:07:47 +0200

    more flexible json inputs

--- utopia/ui/utopia_JSONInput.cpp
+++ utopia/ui/utopia_JSONInput.cpp
@@ -52,6 +52,167 @@ namespace utopia {
 
 	private:
 		std::shared_ptr<const nlohmann::json> j;
+
+		// struct UtopiaJSONType {
+		// 	/// type for (signed) integers
+		// 	using number_integer_t = long;
+		// 	/// type for unsigned integers
+		// 	using number_unsigned_t = unsigned long;
+		// 	/// type for floating-point numbers
+		// 	using number_float_t = double;
+		// 	/// type for strings
+		// 	using string_t = std::string;
+		// };
+
+		// template<typename T>
+		// class SAXJSONParser : nlohmann::json_sax<UtopiaJSONType> {
+		// public:
+
+		// 	SAXJSONParser(const T &val) 
+		// 	: val_(val)
+		// 	{}
+
+		// 	// called when null is parsed
+		// 	bool null()
+		// 	{
+		// 		return true;
+		// 	}
+
+
+		// 	// called when a boolean is parsed; value is passed
+		// 	bool boolean(bool val)
+		// 	{
+		// 		val_.set(val);
+		// 		return true;
+		// 	}
+
+
+		// 	// called when a signed or unsigned integer number is parsed; value is passed
+		// 	bool number_integer(long val)
+		// 	{
+		// 		val_.set(val);
+		// 		return true;
+		// 	}
+
+		// 	bool number_unsigned(unsigned long val)
+		// 	{
+		// 		val_.set(static_cast<long>(val)); //FIXME
+		// 		return true;
+		// 	}
+
+
+		// 	// called when a floating-point number is parsed; value and original string is passed
+		// 	bool number_float(double val, const std::string& s)
+		// 	{
+		// 		val_.set(val);
+		// 		return true;
+		// 	}
+
+
+		// 	// called when a string is parsed; value is passed and can be safely moved away
+		// 	bool string(std::string& val)
+		// 	{
+		// 		val_.set(val);
+		// 		return true;
+		// 	}
+
+
+		// 	// called when an object or array begins or ends, resp. The number of elements is passed (or -1 if not known)
+		// 	bool start_object(std::size_t elements)
+		// 	{
+		// 		return true;
+		// 	}
+
+		// 	bool end_object()
+		// 	{
+		// 		return true;
+		// 	}
+
+		// 	bool start_array(std::size_t elements)
+		// 	{
+		// 		return true;
+		// 	}
+
+		// 	bool end_array()
+		// 	{
+		// 		return true;
+		// 	}
+
+		// 	// called when an object key is parsed; value is passed and can be safely moved away
+		// 	bool key(std::string& val)
+		// 	{
+		// 		// val_.set(val);
+		// 		return true;
+		// 	}
+
+
+		// 	// called when a parse error occurs; byte position, the last token, and an exception is passed
+		// 	bool parse_error(std::size_t position, const std::string& last_token, const nlohmann::detail::exception& ex)
+		// 	{
+		// 		assert(false);
+		// 		return true;
+		// 	}
+
+		// 	inline T get() const
+		// 	{	
+		// 		T ret;
+		// 		val_.get(ret);
+		// 		return ret;
+		// 	}
+
+		// 	Convertible<T> val_;
+		// };
+
+	public:
+
+		template<typename T>
+		void get(const std::string &key, T &value) const
+		{
+			auto it = json().find(key);
+			if(it == json().end()) {
+				return;
+			}
+
+			// SAXJSONParser<T> parser(value);
+			// nlohmann::json::sax_parse(*it, &parser);
+			// value = parser.get();
+
+			const auto &j = *it;
+			
+			if(!j.is_null()) {
+				Convertible<T> c(value);
+				if(j.is_boolean()) {
+					c.set(j.get<bool>());
+
+				} else if(j.is_number()) {
+					
+					if(c.is_double()) {
+						c.set(j.get<double>());
+					} else if(c.is_float()) { 
+						c.set(j.get<float>());
+					} else if(c.is_int()) {
+						c.set(j.get<int>());
+					} else if(c.is_long()) {
+						c.set(j.get<long>());
+					} else if(c.is_string()) {
+						c.set(j.get<double>());
+					}
+
+				} else if(j.is_string()) {
+					c.set(j.get<std::string>());
+				} 
+
+				// else if(j.is_array()) {
+				// 	c.set(j.get<bool>());
+				// } 
+
+				// else if(j.is_object()) {
+				// 	c.set(j.get<bool>());
+				// } 
+
+				c.get(value);
+			}
+		}
 	};
 
 	JSONInput::JSONInput()
@@ -93,54 +254,70 @@ namespace utopia {
 	void JSONInput::get(const std::string &key, bool &val)
 	{
 		assert(impl_);
-		val = impl_->json()[key];
+		impl_->get(key, val);
 	}
 
 	void JSONInput::get(const std::string &key, double &val)
 	{
 		assert(impl_);
-		val = impl_->json()[key];
+		impl_->get(key, val);
 	}
 
 	void JSONInput::get(const std::string &key, int &val)
 	{
 		assert(impl_);
-		val = impl_->json()[key];
-
+		impl_->get(key, val);
 	}
 
 	void JSONInput::get(const std::string &key, SizeType &val)
 	{
 		assert(impl_);
-		val = impl_->json()[key];
+		impl_->get(key, val);
 	}
 
 	void JSONInput::get(const std::string &key, std::string &val)
 	{
 		assert(impl_);
-		val = impl_->json()[key];
+		impl_->get(key, val);
 	}
 
 	void JSONInput::get(const std::string &key, Configurable &val)
 	{
 		assert(impl_);
-		JSONInput child;
-		child.impl_ = utopia::make_unique<Impl>(make_ref(impl_->json()[key]));
-		val.read(child);
+		auto it = impl_->json().find(key);
+		if(it == impl_->json().end()) {
+			return;
+		}
+
+		const auto &j = *it;
+
+		if(!j.is_null()) {
+			JSONInput child;
+			child.impl_ = utopia::make_unique<Impl>(make_ref(impl_->json()[key]));
+			val.read(child);
+		}
 	}
 
 	void JSONInput::get(const std::string &key, std::function<void(Input &)> lambda)
 	{
 		assert(impl_);
-		JSONInput child;
-		child.impl_ = utopia::make_unique<Impl>(make_ref(impl_->json()[key]));
-		lambda(child);
+		auto it = impl_->json().find(key);
+		if(it == impl_->json().end()) {
+			return;
+		}
+
+		const auto &j = *it;
+
+		if(!j.is_null()) {
+			JSONInput child;
+			child.impl_ = utopia::make_unique<Impl>(make_ref(impl_->json()[key]));
+			lambda(child);
+		}
 	}
 
 	bool JSONInput::good() const
 	{
 		return (impl_) && impl_->good();
 	}
-
 
 }
