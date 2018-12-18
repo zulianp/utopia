e637e19ac95bb5e3b81238cf280616f2d6f1111a
Patrick Zulian <patrick.zulian@gmail.com>
2018-12-18 13:59:29 +0100

    specialization of each for trilinos

--- utopia/backend/trilinos/utopia_trilinos_Each.hpp
+++ utopia/backend/trilinos/utopia_trilinos_Each.hpp
@@ -2,6 +2,7 @@
 #define UTOPIA_TRILINOS_EACH_HPP
 
 #include "utopia_trilinos_Types.hpp"
+#include "utopia_For.hpp"
 
 namespace utopia {
 
@@ -55,7 +56,76 @@ namespace utopia {
 			}
 		}
 	}
+
+	template<class Fun>
+	inline void each_read(const TVectord &v, Fun fun) 
+	{
+		auto view = raw_type(v)->getLocalView<Kokkos::HostSpace>();
+
+		const auto r = range(v);
+		const std::size_t r_begin = r.begin();
 		
+		For<>::apply(
+			r_begin,
+			r.end(),
+			[r_begin, &fun, &view](const std::size_t i) {
+				fun(i, view(i - r_begin, 0));
+			}
+		);
+	}
+
+	template<class Fun>
+	inline void each_write(TVectord &v, Fun fun) 
+	{
+		auto view = raw_type(v)->getLocalView<Kokkos::HostSpace>();
+
+		const auto r = range(v);
+		const std::size_t r_begin = r.begin();
+		
+		For<>::apply(
+			r_begin,
+			r.end(),
+			[r_begin, &fun, &view](const std::size_t i) {
+				view(i - r_begin, 0) = fun(i);
+			}
+		);
+	}
+
+	template<class Fun>
+	inline void each_transform(const TVectord &in, TVectord &out, Fun fun) 
+	{
+		const auto r = range(out);
+		const std::size_t r_begin = r.begin();
+
+		if(&in == &out) {
+			auto view = raw_type(out)->getLocalView<Kokkos::HostSpace>();
+
+			For<>::apply(
+				r_begin,
+				r.end(),
+				[r_begin, &fun, &view](const std::size_t i) {
+					const auto idx = i - r_begin;
+					auto &val = view(idx, 0);
+					val = fun(i, val);
+				}
+			);
+		} else {
+
+			auto view_in  = raw_type(in)->getLocalView<Kokkos::HostSpace>();
+			auto view_out = raw_type(out)->getLocalView<Kokkos::HostSpace>();
+
+			For<>::apply(
+				r_begin,
+				r.end(),
+				[r_begin, &fun, &view_in, &view_out](const std::size_t i) {
+					const auto idx = i - r_begin;
+					const auto &val = view_in(idx, 0);
+					view_out(idx, 0) = fun(i, val);
+				}
+			);
+		}
+	}
+
 }
 
 #endif //UTOPIA_TRILINOS_EACH_HPP
