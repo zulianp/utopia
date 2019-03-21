af1b28ad5b5bc0bfd2666d7760e6b02353325af8
zulianp <patrick.zulian@gmail.com>
2019-03-21 14:03:47 +0100

    handled VecScatterCreate quirks in separate file

---
+++ utopia/backend/petsc/utopia_petsc_quirks.hpp
@@ -0,0 +1,22 @@
+#ifndef UTOPIA_PETSC_QUIRKS_HPP
+#define UTOPIA_PETSC_QUIRKS_HPP
+
+#include "petscvec.h"    
+#include "utopia_Config.hpp"
+	
+
+	//Weird name changing in minor versions
+	inline PetscErrorCode UtopiaVecScatterCreate(Vec xin, IS ix, Vec yin, IS iy, VecScatter *newctx)
+	{
+#if UTOPIA_PETSC_VERSION_GREATER_EQUAL_THAN(3,10,2)
+		return VecScatterCreate(xin, ix, yin, iy, newctx);
+#else
+	#if UTOPIA_PETSC_VERSION_GREATER_EQUAL_THAN(3,11,0) || (UTOPIA_PETSC_VERSION_GREATER_EQUAL_THAN(3,10,0) && PETSC_VERSION_RELEASE == 0)
+		return VecScatterCreateWithData(xin, ix, yin, iy, newctx);
+	#else
+		return VecScatterCreate(xin, ix, yin, iy, newctx);
+	#endif
+#endif
+	}
+
+#endif //UTOPIA_PETSC_QUIRKS_HPP
