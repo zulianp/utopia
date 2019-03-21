0254478885c1c1a556245ff85fbc7b12d30250e2
zulianp <patrick.zulian@gmail.com>
2019-03-21 15:41:52 +0100

    quirks,,,

--- utopia/backend/petsc/utopia_petsc_quirks.hpp
+++ utopia/backend/petsc/utopia_petsc_quirks.hpp
@@ -8,7 +8,7 @@
 	//Weird name changing in minor versions
 	inline PetscErrorCode UtopiaVecScatterCreate(Vec xin, IS ix, Vec yin, IS iy, VecScatter *newctx)
 	{
-#if UTOPIA_PETSC_VERSION_GREATER_EQUAL_THAN(3,10,3)
+#if UTOPIA_PETSC_VERSION_GREATER_EQUAL_THAN(3,10,4)
 		return VecScatterCreate(xin, ix, yin, iy, newctx);
 #else
 	#if UTOPIA_PETSC_VERSION_GREATER_EQUAL_THAN(3,11,0) || (UTOPIA_PETSC_VERSION_GREATER_EQUAL_THAN(3,10,0) && PETSC_VERSION_RELEASE == 0)
