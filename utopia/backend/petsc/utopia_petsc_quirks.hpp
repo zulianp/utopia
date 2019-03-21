ba7b34d7d451050db93421b11f4aa87c2cf00da5
zulianp <patrick.zulian@gmail.com>
2019-03-21 15:43:17 +0100

    ...

--- utopia/backend/petsc/utopia_petsc_quirks.hpp
+++ utopia/backend/petsc/utopia_petsc_quirks.hpp
@@ -11,7 +11,8 @@
 #if UTOPIA_PETSC_VERSION_GREATER_EQUAL_THAN(3,10,4)
 		return VecScatterCreate(xin, ix, yin, iy, newctx);
 #else
-	#if UTOPIA_PETSC_VERSION_GREATER_EQUAL_THAN(3,11,0) || (UTOPIA_PETSC_VERSION_GREATER_EQUAL_THAN(3,10,0) && PETSC_VERSION_RELEASE == 0)
+	// #if UTOPIA_PETSC_VERSION_GREATER_EQUAL_THAN(3,11,0) || (UTOPIA_PETSC_VERSION_GREATER_EQUAL_THAN(3,10,0) && PETSC_VERSION_RELEASE == 0)
+	#if (UTOPIA_PETSC_VERSION_GREATER_EQUAL_THAN(3,10,0) && PETSC_VERSION_RELEASE == 0)
 		return VecScatterCreateWithData(xin, ix, yin, iy, newctx);
 	#else
 		return VecScatterCreate(xin, ix, yin, iy, newctx);
