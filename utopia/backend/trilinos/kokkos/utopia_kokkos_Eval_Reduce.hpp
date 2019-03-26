92fbcc7a2c6176e95385c8bed608f07919505cf4
Andreas Fink <andreas.fink@cscs.ch>
2019-03-26 16:57:55 +0100

    *fix* correct reduction behaviour with Kokkos The default join for each threaded reduced value is to use operator+=, which is clearly wrong for e.g. Max()

--- utopia/backend/trilinos/kokkos/utopia_kokkos_Eval_Reduce.hpp
+++ utopia/backend/trilinos/kokkos/utopia_kokkos_Eval_Reduce.hpp
@@ -15,18 +15,29 @@ namespace utopia {
 		{
 			using ExecutionSpaceT = typename Vector::vector_type::execution_space;
 			using Scalar = typename Vector::Scalar;
+                        using Data = decltype(vec.implementation().template getLocalView<ExecutionSpaceT>());
+
+                        struct OpFunctor {
+                            KOKKOS_INLINE_FUNCTION void join(volatile Scalar &val, const volatile Scalar &other) const {
+                                // Kokkos forces us to have the input values being declared volatile. Hence we need to make copies for the reduction operations
+                                double tmp=val, tmp2=other;
+                                val = _op.apply(tmp, tmp2);
+                            }
+                            KOKKOS_INLINE_FUNCTION void operator()(const int& i, Scalar &val) const { 
+                                 val = _op.apply(val, _data(i, 0));
+                            }
+                            const KokkosOp<Scalar, Op>& _op;
+                            const Data& _data;
+                        };
 		    
 		    assert(!vec.empty());
 		    auto data = vec.implementation().template getLocalView<ExecutionSpaceT>();
 
             Scalar ret = initial_value;
-
             KokkosOp<Scalar, Op> k_op;
-            
-            Kokkos::parallel_reduce(data.extent(0), KOKKOS_LAMBDA(const int i, Scalar &val) {
-                val = k_op.apply(val, data(i, 0));
-            }, ret);
-
+            OpFunctor functor{k_op, data};
+            Kokkos::parallel_reduce(data.extent(0), functor, ret);
+ 
     	    return ret;
 		}
 	};
