5622e4c21b08f7487440b7010ed379ff5f3295d8
Nur Aiman Fadel <nur.fadel@cscs.ch>
2019-03-27 14:55:13 +0100

    added FEForwardDeclarations in Intrepid2 backend

---
+++ utopia_fe/backend/intrepid2/utopia_intrepid2_FEForwardDeclarations.hpp
@@ -0,0 +1,18 @@
+#ifndef UTOPIA_INTREPID2_FE_FORWARD_DECLARATIONS_HPP
+#define UTOPIA_INTREPID2_FE_FORWARD_DECLARATIONS_HPP 
+
+#include "utopia_FEForwardDeclarations.hpp"
+
+#ifdef NDEBUG
+#if defined(DEBUG)
+#error "DEBUG macro defined when it should not be defined"
+#endif
+#endif //NDEBUG
+
+static const int INTREPID2_TAG = 1002;
+
+namespace utopia {
+	class Intrepid2FunctionSpace;
+}
+
+#endif //UTOPIA_INTREPID2_FE_FORWARD_DECLARATIONS_HPP
