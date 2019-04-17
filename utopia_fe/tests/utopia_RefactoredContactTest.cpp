1e39b83df98dd9b31a9873a3a86964140f50d48d
zulianp <patrick.zulian@gmail.com>
2019-04-17 17:03:38 +0200

    ...

--- utopia_fe/tests/utopia_RefactoredContactTest.cpp
+++ utopia_fe/tests/utopia_RefactoredContactTest.cpp
@@ -251,41 +251,6 @@ namespace utopia {
             }
         }
 
-        //assemble for p2
-        static void assemble_local_trafo(
-            const libMesh::Elem &el,
-            const int el_order,
-            const double alpha,
-            libMesh::DenseMatrix<libMesh::Real> &trafo,
-            libMesh::DenseMatrix<libMesh::Real> &inv_trafo)
-        {
-            assert(el_order == 2);
-            int n = is_tri(el.type()) ? 3 : (is_quad(el.type()) ? 4 : 2);
-            int n_nodes = el.n_nodes();
-
-            assert(n * 2 == n_nodes);
-            trafo.resize(n_nodes, n_nodes);
-
-            /////////////////////////////////
-            for(int i = 0; i < n; ++i) {
-                //block (0,0)
-                trafo(i, i) = 1.0;
-
-                //block (1,1)
-                trafo(n + i, n + i) = (1 - 2*alpha);
-            }
-
-            /////////////////////////////////
-            //block (1,0)
-            for(int i = 0; i < n-1; ++i) {
-                trafo(n + i, i + 1) = alpha;
-            }
-
-            trafo(n_nodes-1, n-1) = alpha;
-            trafo(n_nodes-1, 0)   = alpha;
-            /////////////////////////////////
-        }
-
         void assemble_biorth_weights(
                 const libMesh::Elem &el,
                 const int el_order,
