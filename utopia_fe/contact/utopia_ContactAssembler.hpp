fc61d65d4ec17b250a7d4ff246705f7bc76161e7
zulianp <patrick.zulian@gmail.com>
2019-05-03 11:06:28 +0200

    reorg code

--- utopia_fe/contact/utopia_ContactAssembler.hpp
+++ utopia_fe/contact/utopia_ContactAssembler.hpp
@@ -8,12 +8,46 @@
 
 namespace utopia {
 
-   class ContactAssembler {
-   public:
-    bool assemble(
-        libMesh::MeshBase &mesh,
-        libMesh::DofMap &dof_map,
-        const ContactParams &params);
+    class ContactTensors {
+    public:
+
+        void convert(
+            const USparseMatrix &perm,
+            const USparseMatrix &vector_perm,
+            ContactTensors &out) const;
+
+        
+        void finalize(const SizeType spatial_dim, const bool normalize = true);
+
+        static bool check_op(const USparseMatrix &T);
+
+    // private:
+        USparseMatrix B_x, D_x, Q_x;
+
+        USparseMatrix B, D, Q, T, orthogonal_trafo, complete_transformation;
+        UVector weighted_gap, gap;
+        UVector weighted_normal, normal;
+        UVector area;
+        UVector is_contact;
+
+        Factorization<USparseMatrix, UVector> solver;
+    };
+
+    class ContactAssembler {
+    public:
+        bool assemble(
+            libMesh::MeshBase &mesh,
+            libMesh::DofMap &dof_map,
+            const ContactParams &params);
+
+        void couple(const UVector &in, UVector &out);
+        void uncouple(const UVector &in, UVector &out);
+        void couple(const USparseMatrix &in, USparseMatrix &out);
+
+        const UVector &gap() const;
+
+    private:
+        std::shared_ptr<ContactTensors> contact_tensors_;
     };
 
 }
---
+++ utopia_fe/contact/utopia_OrthogonalTransformation.hpp
@@ -0,0 +1,64 @@
+#ifndef UTOPIA_ORTHOGONAL_TRANSFORMATION_HPP
+#define UTOPIA_ORTHOGONAL_TRANSFORMATION_HPP
+
+#include "moonolith_vector.hpp"
+#include "moonolith_householder.hpp"
+
+#include <libmesh/dense_matrix.h>
+
+namespace utopia {
+
+    class OrthogonalTransformation {
+    public:
+        void build(const std::vector<double> &n, libMesh::DenseMatrix<double> &mat)
+        {
+            auto spatial_dim = n.size();
+
+            if(spatial_dim == 2) {
+                v2.x = n[0];
+                v2.y = n[1];
+
+                build_aux(v2, h2, mat);
+
+            } else if(spatial_dim == 3) {
+                v3.x = n[0];
+                v3.y = n[1];
+                v3.z = n[2];
+
+                build_aux(v3, h3, mat);
+            } else {
+                assert(false);
+            }
+        }
+
+    private:
+        moonolith::Vector<double, 2> v2;
+        moonolith::Vector<double, 3> v3;
+
+        moonolith::HouseholderTransformation<double, 2> h2;
+        moonolith::HouseholderTransformation<double, 3> h3;
+
+        template<int Dim>
+        static void build_aux(
+            moonolith::Vector<double, Dim> &v,
+            moonolith::HouseholderTransformation<double, Dim> &H,
+            libMesh::DenseMatrix<double> &mat)
+        {
+            v[0] -= 1;
+            v /= length(v);
+            H.init(v);
+
+            mat.resize(Dim, Dim);
+
+            for(int i = 0; i < Dim; ++i) {
+                for(int j = 0; j < Dim; ++j) {
+                    mat(i, j) = H(i, j);
+                }
+            }
+        }
+    };
+
+
+}
+
+#endif //UTOPIA_ORTHOGONAL_TRANSFORMATION_HPP
