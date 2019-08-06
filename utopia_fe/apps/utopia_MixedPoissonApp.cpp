eb48f0d886339b984fb48711d5331c2e9130277d
zulianp <patrick.zulian@gmail.com>
2019-08-06 14:53:04 +0200

    added stabilization for darcy eq

--- utopia_fe/apps/utopia_MixedPoissonApp.cpp
+++ utopia_fe/apps/utopia_MixedPoissonApp.cpp
@@ -22,14 +22,23 @@ namespace utopia {
 
     void MixedPoissonApp::run(Input &in)
     {
+        using FS   = utopia::LibMeshFunctionSpace;
+        using PFS = utopia::ProductFunctionSpace<FS>;
+
         UIMesh<libMesh::DistributedMesh> mesh(comm());
-        UIFunctionSpace<LibMeshFunctionSpace> space(make_ref(mesh));
+        UIFunctionSpace<FS> space(make_ref(mesh));
 
         in.get("mesh", mesh);
         in.get("space", space);
 
+   
+
         auto dim = mesh.mesh().mesh_dimension();
         auto &M  = space.space();
+
+
+        UIForcingFunction<PFS, UVector> forcing_function(M);
+        in.get("forcing-functions", forcing_function);
         
         if(dim + 1 != M.n_subspaces()) {
             std::cerr << "[Error] wrong number of subspaces, expected " <<   (dim + 1) << std::endl;
@@ -50,24 +59,37 @@ namespace utopia {
         auto tau_y = test(W[1]);
 
         //
-        auto a   = -(inner(sigma, tau) * dX);
-        auto b_t = inner(u, div(tau)) * dX;
+        auto a   = (inner(sigma, tau) * dX);
+        auto b_t = inner(grad(u), tau) * dX; 
+        auto b   = inner(sigma, grad(v)) * dX; // negative sign must be considered for the rhs
 
-        auto b   = inner(div(sigma), v) * dX;
+        //Arif Masud and Thomas JR Hughes. A stabilized mixed finite element method for darcy flow
+        auto stab = 0.5 * (
+            inner(grad(u), grad(v)) * dX - inner(sigma, tau) * dX + 
+            inner(sigma, grad(v)) * dX   - inner(grad(u), tau) * dX
+        );
 
+        auto bilinear_form = a + b + b_t + stab;
 
-        auto bilinear_form = a + b + b_t;
-
-        auto fv   = inner(coeff(0.0), v) * dX;
-
-        auto ftau = inner(coeff(0.0), tau_x) * dX + inner(coeff(0.0), tau_y) * dX;
-
-        auto linear_form = fv + ftau;
+        // auto fv   = inner(coeff(0.0), v) * dX;
+        // auto ftau = inner(coeff(0.0), tau_x) * dX + inner(coeff(0.0), tau_y) * dX;
+        // auto linear_form = fv + ftau;
 
         USparseMatrix A;
         UVector rhs, x;
 
-        assemble(bilinear_form == linear_form, A, rhs);
+        x = local_zeros(V.dof_map().n_local_dofs());
+        forcing_function.eval(x, rhs);
+
+        // write("f.m", rhs);
+        rhs *= -1.0;
+
+        double norm_rhs = norm2(rhs);
+        std::cout << norm_rhs << std::endl;
+
+
+        // assemble(bilinear_form == linear_form, A, rhs);
+        assemble(bilinear_form, A);
 
         write("A_neu.m", A);
 
@@ -75,7 +97,7 @@ namespace utopia {
 
         write("A.m", A);
 
-        x = local_zeros(local_size(rhs));
+        
 
         Factorization<USparseMatrix, UVector>().solve(A, rhs, x);
 
