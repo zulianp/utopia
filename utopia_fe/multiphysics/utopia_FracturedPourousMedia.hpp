5888da56d505d778e3c33e374de932214dbf2ea3
zulianp <patrick.zulian@gmail.com>
2019-09-17 11:40:02 +0200

    fixed a bug

--- utopia_fe/multiphysics/utopia_FracturedPourousMedia.hpp
+++ utopia_fe/multiphysics/utopia_FracturedPourousMedia.hpp
@@ -160,7 +160,10 @@ namespace utopia {
             if(is_dual()) {
                 NewTransferAssembler transfer_assembler;
                 transfer_assembler.remove_incomplete_intersections(false);
-                transfer_assembler.constraint_matrix_from(constraint_matrix_pm);
+
+                if(constraint_matrix_pm) {
+                    transfer_assembler.constraint_matrix_from(constraint_matrix_pm);
+                }
 
                 if(constraint_matrix_pm && !empty(*constraint_matrix_pm)) {
                     m_utopia_status("using constraint matrix from mortar in porous-matrix");
@@ -227,7 +230,10 @@ namespace utopia {
             (*mass_matrix_t_)     = transpose(*mass_matrix_);
 
             set_zero_at_constraint_rows(pourous_matrix.dof_map(),   *coupling_matrix_t_);
-            set_zero_at_constraint_rows(fracture_newtork.dof_map(), *mass_matrix_t_);
+            // set_zero_at_constraint_rows(fracture_newtork.dof_map(), *mass_matrix_t_);
+            m_utopia_warning("set_zero_at_constraint_rows(fracture_newtork.dof_map(), *mass_matrix_t_)");
+
+            is_constrained_ = sum(*transfer_matrix(), 1);
             return true;
         }
 
@@ -256,6 +262,11 @@ namespace utopia {
             return mass_matrix_t_;
         }
 
+        inline const UVector &is_constrained() const
+        {
+            return is_constrained_;
+        }
+
     private:
         std::string type_;
         bool use_interpolation_;
@@ -272,6 +283,8 @@ namespace utopia {
         std::shared_ptr<Matrix> mass_matrix_t_;
 
         TransferOptions opts_;
+
+        UVector is_constrained_;
 
     };
 
