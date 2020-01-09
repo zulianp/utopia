8b2ca2f7bfe2164bcc23641203b7c3e0c8805615
kopanicakova <kopana@usi.ch>
2020-01-09 10:14:42 +0100

    introducing more tests for RMTR_l2, QuasiRMTR_l2

--- utopia/tests/utopia_ExprTest.cpp
+++ utopia/tests/utopia_ExprTest.cpp
@@ -48,6 +48,8 @@ namespace utopia {
             UTOPIA_RUN_TEST(e_pseudo_inv_test);
             UTOPIA_RUN_TEST(mat_vec_multiply_test);
             UTOPIA_RUN_TEST(vec_add_add_add_test);
+            UTOPIA_RUN_TEST(convert_test); 
+            UTOPIA_RUN_TEST(emul_test);
 
             // FIXME (mem allocs)
 
@@ -55,6 +57,27 @@ namespace utopia {
             // Seq. fault
             // UTOPIA_RUN_TEST(mat_transp_mult_test);
         }
+
+
+
+        void convert_test()
+        {
+            #ifdef WITH_PETSC
+                if(Traits<Vector>::Backend == PETSC) {
+                    Vector ut_vec = local_zeros(10); 
+                    
+                    Vec x; 
+                    VecDuplicate(raw_type(ut_vec), &x); 
+                    convert(ut_vec, x); 
+                    ut_vec *= 500; 
+
+                    UTOPIA_NO_ALLOC_BEGIN("Ellipse::convert_test");
+                    convert(x, ut_vec); 
+                    UTOPIA_NO_ALLOC_END();
+                }
+            #endif
+        }
+
 
         void vec_add_add_add_test()
         {
@@ -104,8 +127,15 @@ namespace utopia {
             Vector y = values(n_, 10.0);
             Vector z = values(n_, 10.0);
 
+            double scaling_param = 10.0; 
+
             UTOPIA_NO_ALLOC_BEGIN("emul_test");
             z = e_mul(x, y);
+            UTOPIA_NO_ALLOC_END();
+
+
+            UTOPIA_NO_ALLOC_BEGIN("emul2_test");
+            y = scaling_param * e_mul(x, y);
             UTOPIA_NO_ALLOC_END();
         }
 
@@ -483,6 +513,10 @@ namespace utopia {
             UTOPIA_NO_ALLOC_BEGIN("bratu_grad");
             result = x - (0.5 * exp(x));
             UTOPIA_NO_ALLOC_END();
+
+            UTOPIA_NO_ALLOC_BEGIN("bratu_grad2");
+            result = x - (0.5 * 0.2* exp(x));
+            UTOPIA_NO_ALLOC_END();            
         }
 
         void diag_mult()
