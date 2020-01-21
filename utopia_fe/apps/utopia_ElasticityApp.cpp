818689b03a036066423d03246872d3238dde0efb
zulianp <patrick.zulian@gmail.com>
2020-01-21 10:58:14 +0100

    adding smg to elasticityapp

--- utopia_fe/apps/utopia_ElasticityApp.cpp
+++ utopia_fe/apps/utopia_ElasticityApp.cpp
@@ -17,6 +17,7 @@
 #include "utopia_InputParameters.hpp"
 #include "utopia_polymorphic_QPSolver.hpp"
 #include "utopia_ContactStress.hpp"
+// #include "utopia_FractureFlowUtils.hpp"
 
 #include "libmesh/mesh_refinement.h"
 
@@ -120,6 +121,7 @@ namespace utopia {
         UIndexSet ghost_nodes;
         convert(dof_map.get_send_list(), ghost_nodes);
         x = ghosted(dof_map.n_local_dofs(), dof_map.n_dofs(), ghost_nodes);
+        c = x;
 
         material.assemble_hessian_and_gradient(x, H, g);
         g *= -1.0;
@@ -135,7 +137,24 @@ namespace utopia {
             write("M.n", M);
         }
 
-        Factorization<USparseMatrix, UVector> solver;
+        // Factorization<USparseMatrix, UVector> solver;
+
+        auto linear_solver = std::make_shared<Factorization<USparseMatrix, UVector>>();
+        auto smoother      = std::make_shared<GaussSeidel<USparseMatrix, UVector>>();
+        // auto smoother = std::make_shared<ProjectedGaussSeidel<USparseMatrix, UVector>>();
+        // auto smoother = std::make_shared<ConjugateGradient<USparseMatrix, UVector, HOMEMADE>>();
+        // linear_solver->verbose(true);
+        SemiGeometricMultigrid mg(smoother, linear_solver);
+        // mg.algebraic().rtol(1e-16);
+        // mg.algebraic().atol(1e-16);
+        // mg.algebraic().max_it(400);
+        // mg.verbose(true);
+        mg.init(Vx.equation_system(), 4);
+        mg.max_it(1);
+
+        ConjugateGradient<USparseMatrix, UVector, HOMEMADE> solver;
+        solver.set_preconditioner(make_ref(mg));
+        solver.verbose(true);
 
         if(!solver.solve(H, g, c)) {
             write("H.m", H);
