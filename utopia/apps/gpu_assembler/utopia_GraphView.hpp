a2798d709079d1c3b6ec8df7aa14b8b5961f6213
zulianp <patrick.zulian@gmail.com>
2020-01-24 17:39:36 +0100

    fixes for localsizetype in trilinos backend

--- utopia/apps/gpu_assembler/utopia_GraphView.hpp
+++ utopia/apps/gpu_assembler/utopia_GraphView.hpp
@@ -32,15 +32,16 @@ namespace utopia {
     template<class Elem, class Comm, class ExecutionSpace, typename...Args>
     class Graph<Mesh<Elem, Comm, ExecutionSpace, Uniform<Args...>>> {
     public:
-        using Mesh        = utopia::Mesh<Elem, Comm, ExecutionSpace, Uniform<Args...>>;
-        using MeshView    = typename Mesh::ViewDevice;
-        using StencilView = utopia::StencilView<MeshView>;
-        using Dev         = Traits<TpetraVector>::Device;
-        using SizeType    = Traits<TpetraVector>::SizeType;
-        using MapType     = TpetraVector::MapType;
-        using GraphType   = Tpetra::FECrsGraph<SizeType, SizeType>;
-        using View        = Kokkos::View<SizeType*, ExecutionSpace>;
-        using DualNNZView = Kokkos::DualView<std::size_t*, ExecutionSpace>;
+        using Mesh          = utopia::Mesh<Elem, Comm, ExecutionSpace, Uniform<Args...>>;
+        using MeshView      = typename Mesh::ViewDevice;
+        using StencilView   = utopia::StencilView<MeshView>;
+        using Dev           = Traits<TpetraVector>::Device;
+        using SizeType      = Traits<TpetraVector>::SizeType;
+        using LocalSizeType = Traits<TpetraVector>::LocalSizeType;
+        using MapType       = TpetraVector::MapType;
+        using GraphType     = Tpetra::FECrsGraph<LocalSizeType, SizeType>;
+        using View          = Kokkos::View<SizeType*, ExecutionSpace>;
+        using DualNNZView   = Kokkos::DualView<std::size_t*, ExecutionSpace>;
 
         void init(const Mesh &mesh)
         {
