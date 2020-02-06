3594794b2a55558413ed2f6f81929336140dc021
zulianp <patrick.zulian@gmail.com>
2020-02-06 13:42:24 +0100

    separate file for poisson and elast

---
+++ utopia/apps/utopia_poisson_app.cpp
@@ -0,0 +1,332 @@
+#include "utopia_Base.hpp"
+
+//include edsl components
+#include "utopia_AppRunner.hpp"
+#include "utopia_Core.hpp"
+#include "utopia_PetscDM.hpp"
+#include "utopia_petsc_Matrix.hpp"
+#include "utopia_AssemblyView.hpp"
+#include "utopia_DeviceView.hpp"
+#include "utopia_petsc.hpp"
+#include "utopia_ConjugateGradient.hpp"
+#include "utopia_TrivialPreconditioners.hpp"
+#include "utopia_LaplacianView.hpp"
+#include "utopia_MPITimeStatistics.hpp"
+#include "utopia_BratuFE.hpp"
+#include "utopia_PoissonFE.hpp"
+#include "utopia_MassMatrixView.hpp"
+#include "utopia_petsc_dma_FunctionSpace.hpp"
+#include "utopia_petsc_DirichletBoundaryConditions.hpp"
+#include "utopia_LinearElasticityView.hpp"
+#include "utopia_GradInterpolate.hpp"
+#include "utopia_PrincipalStrainsView.hpp"
+#include "utopia_PhaseField.hpp"
+#include "utopia_FEFunction.hpp"
+#include "utopia_SampleView.hpp"
+
+#include <cmath>
+
+
+
+namespace utopia {
+
+    template<class FunctionSpace>
+    static void poisson_problem(FunctionSpace &space, const bool use_direct_solver, const bool debug_matrices = false)
+    {
+        // using Mesh             = typename FunctionSpace::Mesh;
+        using Elem             = typename FunctionSpace::Elem;
+        using Dev              = typename FunctionSpace::Device;
+        using Vector           = typename FunctionSpace::Vector;
+        using Matrix           = typename FunctionSpace::Matrix;
+        using Comm             = typename FunctionSpace::Comm;
+
+        static const int Dim    = Elem::Dim;
+        // static const int NNodes = Elem::NNodes;
+        static const int NFunctions = Elem::NFunctions;
+
+        // using DevFunctionSpace = typename FunctionSpace::ViewDevice;
+        using Point            = typename FunctionSpace::Point;
+        using Scalar           = typename FunctionSpace::Scalar;
+        using SizeType         = typename FunctionSpace::SizeType;
+        using Quadrature       = utopia::Quadrature<Elem, 2>;
+        using ElementMatrix    = utopia::StaticMatrix<Scalar, NFunctions, NFunctions>;
+        // using ElementVector    = utopia::StaticVector<Scalar, NFunctions>;
+
+        Comm &comm = space.comm();
+
+        MPITimeStatistics stats(comm);
+
+        Quadrature quadrature;
+        auto &&space_view = space.view_device();
+
+        comm.barrier();
+        stats.start();
+
+        Matrix mat, mass_mat;
+        space.create_matrix(mat);
+        // space.create_matrix(mass_mat);
+        //copying is faster than create_matrix
+        mass_mat = mat;
+
+        Vector rhs;
+        space.create_vector(rhs);
+
+        stats.stop_and_collect("create-matrix");
+
+        for(int c = 0; c < space.n_components(); ++c) {
+            space.emplace_dirichlet_condition(
+                SideSet::left(),
+                UTOPIA_LAMBDA(const Point &) -> Scalar {
+                    return 1.0;
+                },
+                c
+            );
+
+            space.emplace_dirichlet_condition(
+                SideSet::right(),
+                UTOPIA_LAMBDA(const Point &) -> Scalar {
+                    return -1.0;
+                },
+                c
+            );
+        }
+
+        auto diffusivity = UTOPIA_LAMBDA(const Point &p) -> Scalar {
+            Scalar dist = 0.0;
+            for(SizeType i = 0; i < p.size(); ++i) {
+                Scalar v = p[i] - 0.5;
+                dist += v*v;
+            }
+
+            if(device::sqrt(dist) > 0.2) {
+                return 1.0;
+            } else {
+                return 1e-4;
+            }
+        };
+
+        stats.start();
+
+
+        auto lapl   = laplacian(space, quadrature);
+        auto mass_m = mass_matrix(space, quadrature);
+
+        {
+            //GPU assembly mock-prototype
+            auto mat_view      = space.assembly_view_device(mat);
+            auto mass_mat_view = space.assembly_view_device(mass_mat);
+            auto rhs_view      = space.assembly_view_device(rhs);
+
+            auto l_view = lapl.view_device();
+            auto m_view = mass_m.view_device();
+
+            if(debug_matrices) {
+                disp("lapl");
+                l_view.describe();
+
+                disp("mass_m");
+                m_view.describe();
+            }
+
+            Dev::parallel_for(
+                space.local_element_range(),
+                UTOPIA_LAMBDA(const SizeType &i)
+            {
+                Elem e;
+
+                //FIXME this is too big for GPU stack memory for hexas
+                ElementMatrix el_mat;
+                Point c;
+                space_view.elem(i, e);
+
+                //Assemble local laplacian
+                el_mat.set(0.0);
+                l_view.assemble(i, e, el_mat);
+                e.centroid(c);
+                el_mat *= diffusivity(c);
+                space_view.add_matrix(e, el_mat, mat_view);
+
+                //Assemble local mass-matrix and reuse el_mat
+                el_mat.set(0.0);
+                m_view.assemble(i, e, el_mat);
+                space_view.add_matrix(e, el_mat, mass_mat_view);
+            });
+        }
+
+        stats.stop_and_collect("assemblies");
+
+        stats.start();
+        rhs = mass_mat * rhs;
+
+        // rename("m",  mass_mat);
+        // write("M.m", mass_mat);
+
+        // rename("a",  mat);
+        // write("A.m", mat);
+
+        Scalar vol = sum(mass_mat);
+        std::cout << "vol: " << vol << std::endl;
+
+        Scalar zero = sum(mat);
+        std::cout << "zero: " << zero << std::endl;
+
+        space.apply_constraints(mat, rhs);
+
+        stats.stop_and_collect("boundary conditions ");
+
+        stats.start();
+        Vector x = rhs;
+        x.set(0.0);
+
+        if(use_direct_solver) {
+            Factorization<Matrix, Vector> solver;
+            solver.solve(mat, rhs, x);
+        }  else {
+            ConjugateGradient<Matrix, Vector, HOMEMADE> cg;
+            auto prec = std::make_shared<InvDiagPreconditioner<Matrix, Vector>>();
+            cg.set_preconditioner(prec);
+            cg.verbose(true);
+
+            const SizeType n_iter = space.n_dofs();
+
+            assert(n_iter > 0);
+
+            cg.max_it(n_iter);
+            cg.rtol(1e-8);
+            cg.solve(mat, rhs, x);
+        }
+
+        stats.stop_and_collect("solve");
+
+        stats.start();
+        rename("x", x);
+        space.write("X.vtk", x);
+        stats.stop_and_collect("write");
+
+        if(comm.rank() == 0) std::cout << "n_dofs: " << space.n_dofs() << std::endl;
+
+        stats.describe(std::cout);
+    }
+
+    static void petsc_dm_assemble_2()
+    {
+        static const int Dim = 2;
+        static const int NNodes = 4;
+
+        using Mesh             = utopia::PetscDM<Dim>;
+        using Elem             = utopia::PetscUniformQuad4;
+        using FunctionSpace    = utopia::FunctionSpace<Mesh, 1, Elem>;
+        using SizeType         = Mesh::SizeType;
+
+        PetscCommunicator world;
+
+        SizeType scale = (world.size() + 1);
+        SizeType nx = scale * 10;
+        SizeType ny = scale * 10;
+        SizeType nz = 10;
+
+        Mesh mesh(
+            world,
+            {nx, ny},
+            {0.0, 0.0},
+            {1.0, 1.0}
+        );
+
+        FunctionSpace space(mesh);
+        poisson_problem(space, true);
+    }
+
+    UTOPIA_REGISTER_APP(petsc_dm_assemble_2);
+
+    static void petsc_dm_assemble_3()
+    {
+        static const int Dim = 3;
+        static const int NNodes = 8;
+
+        using Mesh             = utopia::PetscDM<Dim>;
+        using Elem             = utopia::PetscUniformHex8;
+        using FunctionSpace    = utopia::FunctionSpace<Mesh, 1, Elem>;
+        using SizeType         = Mesh::SizeType;
+
+        PetscCommunicator world;
+
+        SizeType scale = (world.size() + 1);
+        SizeType nx = scale * 15;
+        SizeType ny = scale * 15;
+        SizeType nz = scale * 15;
+
+        Mesh mesh(
+            world,
+            {nx, ny, nz},
+            {0.0, 0.0, 0.0},
+            {1.0, 1.0, 1.0}
+        );
+
+        FunctionSpace space(mesh);
+        poisson_problem(space, false);
+    }
+
+    UTOPIA_REGISTER_APP(petsc_dm_assemble_3);
+
+
+    static void petsc_dm_mvar_poisson_2()
+    {
+        static const int Dim = 2;
+        static const int NVars = Dim;
+
+        using Mesh             = utopia::PetscDM<Dim>;
+        using Elem             = utopia::PetscUniformQuad4;
+        using FunctionSpace    = utopia::FunctionSpace<Mesh, NVars, Elem>;
+        using SizeType         = Mesh::SizeType;
+
+        PetscCommunicator world;
+
+        SizeType scale = (world.size() + 1);
+        SizeType nx = scale * 30;
+        SizeType ny = scale * 30;
+
+        FunctionSpace space;
+        space.build(
+            world,
+            {nx, ny},
+            {0.0, 0.0},
+            {1.0, 1.0}
+        );
+
+        poisson_problem(space, true, true);
+    }
+
+    UTOPIA_REGISTER_APP(petsc_dm_mvar_poisson_2);
+
+    static void petsc_dm_mvar_poisson_3()
+    {
+        static const int Dim = 3;
+        static const int NVars = Dim;
+
+        using Mesh             = utopia::PetscDM<Dim>;
+        using Elem             = utopia::PetscUniformHex8;
+        using FunctionSpace    = utopia::FunctionSpace<Mesh, NVars, Elem>;
+        using SizeType         = Mesh::SizeType;
+
+        PetscCommunicator world;
+
+        SizeType scale = (world.size() + 1);
+        SizeType nx = scale * 20;
+        SizeType ny = scale * 20;
+        SizeType nz = scale * 20;
+
+        FunctionSpace space;
+
+        space.build(
+            world,
+            {nx, ny, nz},
+            {0.0, 0.0, 0.0},
+            {1.0, 1.0, 1.0}
+        );
+
+        poisson_problem(space, false);
+    }
+
+    UTOPIA_REGISTER_APP(petsc_dm_mvar_poisson_3);
+
+}
