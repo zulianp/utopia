502680dc3e50c4623d932d11b62613e4d9d54953
zulianp <patrick.zulian@gmail.com>
2020-03-19 12:22:54 +0100

    tri3 with only jacobian

--- utopia/apps/gpu_assembler/utopia_Tri3.hpp
+++ utopia/apps/gpu_assembler/utopia_Tri3.hpp
@@ -4,7 +4,8 @@
 #include "utopia_UniformTri3.hpp"
 
 namespace utopia {
-    template<typename Scalar_>
+
+    template<typename Scalar_, int PhysicalDim = 2>
     class Tri3 : public Elem {
     public:
         using Scalar = Scalar_;
@@ -14,30 +15,48 @@ namespace utopia {
         static const int NSides     = 3;
         static const int NFunctions = 3;
 
-        using Side      = utopia::Edge2<Scalar, Dim>;
-        using Point     = utopia::StaticVector<Scalar, Dim>;
-        using GradValue = utopia::StaticVector<Scalar, Dim>;
-        using STGradX   = utopia::StaticVector<Scalar, Dim-1>;
-        using FunValue  = Scalar;
+        using Side             = utopia::Edge2<Scalar, PhysicalDim>;
+        using Point            = utopia::StaticVector<Scalar, PhysicalDim>;
+        using RefPoint         = utopia::StaticVector<Scalar, Dim>;
+        using GradValue        = utopia::StaticVector<Scalar, PhysicalDim>;
+        using STGradX          = utopia::StaticVector<Scalar, PhysicalDim-1>;
+        using FunValue         = Scalar;
 
+        using Jacobian         = utopia::StaticMatrix<Scalar, PhysicalDim, Dim>;
+        using JacobianInverse  = utopia::StaticMatrix<Scalar, Dim, PhysicalDim>;
 
         template<typename Point>
-        UTOPIA_INLINE_FUNCTION static auto fun(const int i, const Point &p) -> decltype(RefTri3::fun(i, p))
+        UTOPIA_INLINE_FUNCTION static Scalar fun(const int i, const Point &p)
         {
-          return RefTri3::fun(i, p);
+            return RefTri3::fun(i, p);
         }
 
         template<typename Point>
         UTOPIA_INLINE_FUNCTION void node(const std::size_t &i, Point &p) const
         {
-            p[0] = translation_[0];
-            p[1] = translation_[1];
+            p = translation_;
+            auto &&data = jacobian_.raw_type();
 
             switch(i) {
                 case 0: { return; }
-                case 1: { p += u_; return; }
-                case 2: { p += v_; return; }
-                default: {
+                case 1:
+                {
+                    for(int d = 0; d < PhysicalDim; ++d)  {
+                        p(d) += data[d*PhysicalDim];
+                    }
+
+                    return;
+                }
+                case 2:
+                {
+                    for(int d = 0; d < PhysicalDim; ++d)  {
+                        p(d) += data[d*PhysicalDim + 1];
+                    }
+
+                    return;
+                }
+                default:
+                {
                     UTOPIA_DEVICE_ASSERT(false);
                     return;
                 }
@@ -50,20 +69,20 @@ namespace utopia {
             switch(i) {
                 case 0:
                 {
-                    local_side_idx[0]= 0;
-                    local_side_idx[1]= 1;
+                    local_side_idx[0] = 0;
+                    local_side_idx[1] = 1;
                     return;
                 }
                 case 1:
                 {
-                    local_side_idx[0]= 1;
-                    local_side_idx[1]= 2;
+                    local_side_idx[0] = 1;
+                    local_side_idx[1] = 2;
                     return;
                 }
                 case 2:
                 {
-                    local_side_idx[0]= 2;
-                    local_side_idx[1]= 0;
+                    local_side_idx[0] = 2;
+                    local_side_idx[1] = 0;
                     return;
                 }
                 default:
@@ -72,7 +91,6 @@ namespace utopia {
                 }
             }
         }
-
 
         UTOPIA_INLINE_FUNCTION void side(const std::size_t &i, Side &side) const
         {
@@ -105,14 +123,16 @@ namespace utopia {
             side.init();
         }
 
-        template<typename PhysicalPoint>
-        UTOPIA_INLINE_FUNCTION void centroid(PhysicalPoint &out) const
+        template<typename PhysicalPointT>
+        UTOPIA_INLINE_FUNCTION void centroid(PhysicalPointT &out) const
         {
+            auto &&data = jacobian_.raw_type();
+
             for(int i = 0; i < Dim; ++i) {
-                out[i] = (3.0 * translation_[i] + u_[i] + v_[i])/3.0;
+                const int offset = i*PhysicalDim;
+                out[i] = (3.0 * translation_[i] + data[offset] + data[offset + 1])/3.0;
             }
         }
-
 
         // //space-time spatial gradient
         // template<typename Point>
@@ -137,13 +157,13 @@ namespace utopia {
         //     dst[0] /= (h_[0]*h_[1]);
         // }
 
-        // template<typename Point, typename Grad>
-        // UTOPIA_INLINE_FUNCTION void grad(const int i, const Point &p, Grad &g) const
-        // {
-        //     RefTri3::grad(i, p, g);
-        //     g[0] /= h_[0];
-        //     g[1] /= h_[1];
-        // }
+        template<typename Point, typename Grad>
+        UTOPIA_INLINE_FUNCTION void grad(const int i, const Point &p, Grad &g) const
+        {
+            RefPoint g_ref;
+            RefTri3::grad(i, p, g_ref);
+            g = jacobian_inverse_ * g_ref;
+        }
 
         UTOPIA_INLINE_FUNCTION constexpr static bool is_affine()
         {
@@ -157,37 +177,38 @@ namespace utopia {
 
         UTOPIA_INLINE_FUNCTION Scalar measure() const
         {
-            return (u_[0] * v_[1]) - ( u_[1] * v_[0] );
+            return measure_;
         }
 
-        template<typename RefPoint, typename PhysicalPoint>
-        UTOPIA_INLINE_FUNCTION void point(const RefPoint &in, PhysicalPoint &out) const
+        template<typename RefPointT, typename PhysicalPointT>
+        UTOPIA_INLINE_FUNCTION void point(const RefPointT &in, PhysicalPointT &out) const
         {
-            out[0] = in[0] * u_[0] + in[1] * v_[0] + translation_[0];
-            out[1] = in[0] * v_[1] + in[1] * v_[1] + translation_[1];
+            out = jacobian_ * in + translation_;
         }
 
-        template<typename PhysicalPoint, typename RefPoint>
-        UTOPIA_INLINE_FUNCTION void inverse_transform(const PhysicalPoint &in, RefPoint &out) const
+        template<typename PhysicalPointT, typename RefPointT>
+        UTOPIA_INLINE_FUNCTION void inverse_transform(const PhysicalPointT &in, RefPointT &out) const
         {
-            out[0] = (in[0] - translation_[0])/h_[0];
-            out[1] = (in[1] - translation_[1])/h_[1];
+            out = jacobian_inverse_ * (in - translation_);
         }
 
         UTOPIA_INLINE_FUNCTION Tri3() {}
 
-        template<class U, class V>
-        UTOPIA_INLINE_FUNCTION void set(
-            const StaticVector2<Scalar> &translation,
-            const U &u,
-            const V &v
-            )
+        template<class P0, class P1, class P2>
+        UTOPIA_INLINE_FUNCTION void init(
+            const P0 &p0,
+            const P1 &p1,
+            const P2 &p2
+        )
         {
-            translation_(0) = translation(0);
-            translation_(1) = translation(1);
+            translation_= p0;
 
-            u_ = u;
-            v_ = v;
+            jacobian_.set_col(0, p1 - translation_);
+            jacobian_.set_col(1, p2 - translation_);
+
+            jacobian_inverse_ = inv(jacobian_);
+
+            measure_ = det(jacobian_);
         }
 
         UTOPIA_INLINE_FUNCTION constexpr static int n_nodes()
@@ -195,16 +216,18 @@ namespace utopia {
             return NNodes;
         }
 
-        UTOPIA_INLINE_FUNCTION const StaticVector2<Scalar> &translation() const
+        UTOPIA_INLINE_FUNCTION const Point &translation() const
         {
             return translation_;
         }
 
     private:
-        //u = p2 - p1; v = p3 - p1
-        StaticVector2<Scalar> u_, v_;
-        StaticVector2<Scalar> translation_;
+        Point translation_;
+        Jacobian jacobian_;
+        JacobianInverse jacobian_inverse_;
+        Scalar measure_;
     };
+
 }
 
 #endif //UTOPIA_TRI_3_HPP
