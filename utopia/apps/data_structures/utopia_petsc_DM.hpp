a0b7fff994cb75c21c886e25cd0ae110f0289147
zulianp <patrick.zulian@gmail.com>
2020-03-24 19:23:18 +0100

    petsc-io

--- utopia/apps/data_structures/utopia_petsc_DM.hpp
+++ utopia/apps/data_structures/utopia_petsc_DM.hpp
@@ -8,9 +8,9 @@
 #include "utopia_Path.hpp"
 #include "utopia_petsc_Vector.hpp"
 #include "utopia_petsc_Matrix.hpp"
+#include "utopia_petsc_IO.hpp"
 
 #include <petscdm.h>
-#include <petscviewerhdf5.h>
 
 namespace utopia {
     class PetscDMBase : public Configurable {
@@ -120,63 +120,11 @@ namespace utopia {
 
         bool write(const Path &path, const PetscVector &x) const
         {
-            PetscErrorCode ierr = 0;
-            PetscViewer    viewer;
-
-            auto mpi_comm = comm().get();
-
-            const auto ext = path.extension();
-            if(ext == "vtk") {
-                ierr = PetscViewerASCIIOpen(mpi_comm, path.c_str(), &viewer);
-                if(ierr != 0) { assert(false); return false; }
-
-                ierr = PetscViewerPushFormat(viewer,  PETSC_VIEWER_ASCII_VTK); assert(ierr == 0);
-            } else if(ext == "vts") {
-
-                ierr = PetscViewerVTKOpen(mpi_comm, path.c_str(), FILE_MODE_WRITE, &viewer);
-                if(ierr != 0) { assert(false); return false; }
-
-                ierr = PetscViewerPushFormat(viewer,  PETSC_VIEWER_VTK_VTS); assert(ierr == 0);
-            } else if(ext == "vtr") {
-
-                ierr = PetscViewerVTKOpen(mpi_comm, path.c_str(), FILE_MODE_WRITE, &viewer);
-                if(ierr != 0) { assert(false); return false; }
-
-                ierr = PetscViewerPushFormat(viewer,  PETSC_VIEWER_VTK_VTR); assert(ierr == 0);
-            } else if(ext == "vtu") {
-
-                ierr = PetscViewerVTKOpen(mpi_comm, path.c_str(), FILE_MODE_WRITE, &viewer);
-                if(ierr != 0) { assert(false); return false; }
-
-                ierr = PetscViewerPushFormat(viewer,  PETSC_VIEWER_VTK_VTU); assert(ierr == 0);
-            }
-#if defined(PETSC_HAVE_HDF5)
-            else if(ext == "h5") {
-                PetscViewerHDF5Open(mpi_comm, path.c_str(), FILE_MODE_WRITE, &viewer);
-
-                ierr = DMView(raw_type(*mesh_), viewer);    assert(ierr == 0);
-                ierr = VecView(raw_type(x), viewer);        assert(ierr == 0);
-
-                ierr = PetscViewerDestroy(&viewer);  assert(ierr == 0);
-                return true;
-            }
-#endif
-            else if(ext == "png") {
-                //It would be nice to support direct rendering for 2D
-                std::cerr << "unsupported format " << ext << std::endl;
-                return false;
-            } else {
-                std::cerr << "unknown format " << ext << std::endl;
-                return false;
-            }
-
-
-            ierr = DMView(this->raw_type(), viewer);    assert(ierr == 0);
-            ierr = VecView(x.raw_type(), viewer);        assert(ierr == 0);
-
-            ierr = PetscViewerPopFormat(viewer); assert(ierr == 0);
-            ierr = PetscViewerDestroy(&viewer);  assert(ierr == 0);
-            return ierr == 0;
+            PetscIO io;
+            if(!io.open(comm(), path)) { return false; }
+            if(!io.write(*this))       { return false; }
+            if(!io.write(x))           { return false; }
+            return true;
         }
 
     private:
