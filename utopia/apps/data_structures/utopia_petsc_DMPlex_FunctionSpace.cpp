d4cf25bf8e726060d2facd7bbfbead858d690510
zulianp <patrick.zulian@gmail.com>
2020-05-08 16:53:50 +0200

    dmplex cpp checks petsc version

--- utopia/apps/data_structures/utopia_petsc_DMPlex_FunctionSpace.cpp
+++ utopia/apps/data_structures/utopia_petsc_DMPlex_FunctionSpace.cpp
@@ -1,6 +1,12 @@
-#include "utopia_petsc_DMPlex_FunctionSpace.hpp"
+#include "utopia_petsc_Base.hpp"
+
+#if UTOPIA_PETSC_VERSION_GREATER_EQUAL_THAN(3, 12, 4)
+
 #include "utopia_Quad4.hpp"
+#include "utopia_petsc_DMPlex_FunctionSpace.hpp"
 
 namespace utopia {
     template class FunctionSpace<PetscDMPlex<StaticVector<PetscScalar, 2>, ArrayView<PetscInt, 2>>, 1>;
 }
+
+#endif  // UTOPIA_PETSC_VERSION_GREATER_EQUAL_THAN(3, 12, 4)
