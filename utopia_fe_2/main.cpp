15975f5758dd7a0fbf37c7a4efee89d035059e28
zulianp <patrick.zulian@gmail.com>
2020-06-08 09:41:02 +0200

    created new skeleton project

---
+++ utopia_fe_2/main.cpp
@@ -0,0 +1,17 @@
+#include "utopia.hpp"
+#include "utopia_AppRunner.hpp"
+
+int main(const int argc, char *argv[]) {
+    using namespace utopia;
+
+    Utopia::Init(argc, argv);
+    UTOPIA_TRACE_REGION_BEGIN("main");
+
+    {
+        AppRunner app_runner;
+        app_runner.run(argc, argv);
+    }
+
+    UTOPIA_TRACE_REGION_END("main");
+    return Utopia::Finalize();
+}
