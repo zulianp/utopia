6406404654fbc27c23ca76992ab6718fbd774bcd
zulianp <patrick.zulian@gmail.com>
2020-06-08 10:21:47 +0200

    ...

---
+++ utopia_fe_2/test.cpp
@@ -0,0 +1,18 @@
+#include "utopia.hpp"
+#include "utopia_Testing.hpp"
+#include "utopia_Version.hpp"
+
+int main(const int argc, char *argv[]) {
+    using namespace utopia;
+
+    Utopia::Init(argc, argv);
+    UTOPIA_TRACE_REGION_BEGIN("main");
+
+    {
+        TestRunner app_runner;
+        app_runner.run(argc, argv);
+    }
+
+    UTOPIA_TRACE_REGION_END("main");
+    return Utopia::Finalize();
+}
