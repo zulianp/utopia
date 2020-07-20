2919278a5c4c7629f46675ea4518f134dd545b13
Filippo Cesana <filippo.cesana@gmail.com>
2020-07-20 14:20:21 +0200

    Dylan and I are committing almost complete programm

--- utopia/examples/example_comparison_linear_solvers.cpp
+++ utopia/examples/example_comparison_linear_solvers.cpp
@@ -1,31 +1,80 @@
 #include "utopia.hpp"
-#include "utopia_Version.hpp"
+#include "utopia_BiCGStab.hpp"
+#include "utopia_ConjugateGradient.hpp"
+#include "utopia_GaussSeidel.hpp"
 #include "utopia_Jacobi.hpp"
+#include "utopia_PointJacobi.hpp"
+#include "utopia_Version.hpp"
+#include <cstdlib>
+
+#include <fstream>
+#include <iomanip>
 #include <iostream>
 #include <string>
 #include <vector>
-#include <iomanip>
 using namespace std;
 
-
 struct MySolver {
- string name;
- utopia::Chrono time;
+    string name;
+    utopia::Chrono time;
 };
 
-struct less_than_key {
-    inline bool operator() (const MySolver& struct1, const MySolver& struct2) {
-        return (struct1.name < struct2.name);
+// struct less_than_key {
+//     inline bool operator() (const MySolver& struct1, const MySolver& struct2) {
+//         return (struct1.time.get_seconds() < struct2.time.get_seconds());
+//     }
+// };
+
+template <class Solver, class Matrix, class Vector>
+void measure_solver(Matrix& A, Vector& b, const string solver_name) {
+    using namespace utopia;
+    using Scalar = typename Traits<Vector>::Scalar;
+
+    Solver s;
+    Chrono c;
+    Vector x(layout(b), 0);
+
+    c.start();
+    // Setting a conditioner is important with iterative linear solver.
+    // The more a problem is bad conditioned, that is the more the difference
+    // between the largest and small eigenvalues is significant, the more
+    // the solution will deviate from the exact solution.
+    // A preconditioner allows to make this difference smaller, so that
+    // the solution found with the iterative system will be more
+    // precise.
+    // You can uncomment it.
+    // s.set_preconditioner(make_shared<InvDiagPreconditioner<Matrix, Vector>>());
+    s.solve(A, b, x);
+    c.stop();
+
+    Vector r = A * x;
+    r = b - r;
+    Scalar norm_r = norm2(r);
+
+    // stringstream ss;
+    cout << solver_name << endl << "norm_r: " << norm_r << " time: " << c.get_seconds() << endl << endl;
+
+    std::ofstream myfile;
+    myfile.open("./../examples/linear_solvers.csv", ios_base::app);
+    if (!myfile.good()) {
+        std::cerr << "Could not open file for writing" << std::endl;
+    } else {
+        myfile << solver_name << ";"<< norm_r<< ";"<< c.get_seconds() << "\n";
+        myfile.close();
     }
-};
+
+    
+
+
+}
 
 template <class Matrix, class Vector>
-void fastest_solver() {
+void test_linear_solver() {
     using namespace utopia;
     vector<MySolver> solvers;
-    
+
     // ---------------------------------------------------------------
-    // The code inside here is only to initialise the vectors 
+    // The code inside here is only to initialise the vectors
     // and the matrix.
 
     using Comm = typename Traits<Vector>::Communicator;
@@ -40,7 +89,7 @@ void fastest_solver() {
     SizeType n_global = n_local * comm.size();
 
     auto l = layout(comm, n_local, n_global);
-    
+
     // Vectors
     b.zeros(l);
     x.zeros(l);
@@ -54,8 +103,7 @@ void fastest_solver() {
             });
     }
 
-
-    // Matrix 
+    // Matrix
     SizeType n_local_rows = 10;
     SizeType n_local_cols = 10;
     SizeType d_nnz = 3;
@@ -84,118 +132,54 @@ void fastest_solver() {
         }
     }
 
-// End of the initialisation. 
-// ---------------------------------------------------------------    
-// -------------------- CONJUGATE GRADIENT -----------------------
+    // ofstream ofs;
+    // ofs.open("./../examples/linear_solver.csv", ofstream::out | ofstream::trunc);
+    // ofs.close();
 
-    Chrono timeCG;
-    timeCG.start();
-    {
-        ConjugateGradient<Matrix, Vector> cg;
-        // Setting a conditioner is important with iterative linear solver.
-        // The more a problem is bad conditioned, that is the more the difference
-        // between the largest and small eigenvalues is significant, the more
-        // the solution will deviate from the exact solution. 
-        // A preconditioner allows to make this difference smaller, so that
-        // the solution found with the iterative system will be more 
-        // precise. 
-        // Set the preconditioner by uncommenting the following line. 
-        //cg.set_preconditioner(make_shared<InvDiagPreconditioner<Matrix, Vector>>());
-        cg.solve(A, b, x);
-    }
-    timeCG.stop();
+    measure_solver<ConjugateGradient<Matrix, Vector>>(A, b, "ConjugateGradient");
+    measure_solver<GaussSeidel<Matrix, Vector>>(A, b, "GaussSeidel");
+    measure_solver<BiCGStab<Matrix, Vector>>(A, b, "BiCGStab");
+    measure_solver<Jacobi<Matrix, Vector>>(A, b, "Jacobi");
+    measure_solver<PointJacobi<Matrix, Vector>>(A, b, "PointJacobi");
 
-    solvers.push_back(MySolver());
-    solvers[0].name = "Conjugate Gradient";
-    solvers[0].time = timeCG;
-  
-// ---------------------------------------------------------------    
-// -------------------- GAUSS SIEDEL -----------------------------
+    cout << system("/usr/bin/python2.7 ./../examples/hello_world.py 1");
 
-    Chrono timeGS;
-    timeGS.start();
-    {
-        GaussSeidel<Matrix, Vector> gs;
-    //  Set the preconditioner by uncommenting the following line. 
-    //  (Somehow not working in this example)  
-    //  gs.set_preconditioner(make_shared<InvDiagPreconditioner<Matrix, Vector>>());
-        gs.solve(A, b, x);
-    }
-    timeGS.stop();
+    // // -------------- PRINTING THE RESULTS ---------------------------
 
-    solvers.push_back(MySolver());
-    solvers[1].name = "Gauss Siedel";
-    solvers[1].time = timeGS;
+    //     sort(solvers.begin(), solvers.end(), less_than_key());
+    //     for (auto it = solvers.begin(); it != solvers.end(); ++it){
+    //         cout << it->name << ":" << it->time <<endl;
+    //     }
 
-// ---------------------------------------------------------------    
-// ------------------------- BiCGStab ------------------------------
+    // measure_solver<ConjugateGradient, MatrixT, VectorT>(Matrix &A, Vector &b, "Conjugate Gradient");
 
-    Chrono timeBi;
-    timeBi.start();
-    {
-        BiCGStab<Matrix, Vector> Bi;
-        // Set the preconditioner by uncommenting the following line. 
-        // Bi.set_preconditioner(make_shared<InvDiagPreconditioner<Matrix, Vector>>());
-        Bi.solve(A, b, x);
-    }
-    timeBi.stop();
-
-    solvers.push_back(MySolver());
-    solvers[2].name = "BiCGStab";
-    solvers[2].time = timeBi;
-
-
-
-    // ---------------------------------------------------------------    
-    // ------------------------- Jacobi ------------------------------
-
-    Chrono timeJ;
-    timeJ.start();
-    {
-        Jacobi<Matrix, Vector> J;
-        // Set the preconditioner by uncommenting the following line. 
-        // J.set_preconditioner(make_shared<InvDiagPreconditioner<Matrix, Vector>>());
-        J.solve(A, b, x);
-    }
-    timeJ.stop();
-
-    solvers.push_back(MySolver());
-    solvers[3].name = "Jacobi";
-    solvers[3].time = timeJ;
-
-// -------------- PRINTING THE RESULTS ---------------------------
-
-    sort(solvers.begin(), solvers.end(), less_than_key());
-    for (auto it = solvers.begin(); it != solvers.end(); ++it){
-        cout << it->name << ":" << it->time <<endl;
-    }
 
 }
 
 // -------------------------------------------------------------
 
 // Run it with `./examples/example_comparison_linear_solvers
-int main(const int argc, char *argv[]) {
+int main(const int argc, char* argv[]) {
     using namespace utopia;
 
-    // Checking which backend is active.
-    #ifdef WITH_PETSC
-        using MatrixT = PetscMatrix;
-        using VectorT = PetscVector;
-    #else    
-    #ifdef WITH_TRILINOS
-        using MatrixT = TpetraMatrixd;
-        using VectorT = TpetraVectord;
-    #else
-        using MatrixT = BlasMatrixd;
-        using VectorT = BlasVectord;
-    #endif
-    #endif
+// Checking which backend is active.
+#ifdef WITH_PETSC
+    using MatrixT = PetscMatrix;
+    using VectorT = PetscVector;
+#else
+#ifdef WITH_TRILINOS
+    using MatrixT = TpetraMatrixd;
+    using VectorT = TpetraVectord;
+#else
+    using MatrixT = BlasMatrixd;
+    using VectorT = BlasVectord;
+#endif
+#endif
 
     Utopia::Init(argc, argv);
 
-    fastest_solver<MatrixT, VectorT>();
+    test_linear_solver<MatrixT, VectorT>();
+    // print_statistics_test();
 
     return Utopia::Finalize();
 }
-
