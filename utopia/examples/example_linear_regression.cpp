4dd6d68aea661149e319a266dbaad1881078d10f
Filippo Cesana <filippo.cesana@gmail.com>
2020-08-12 12:58:05 +0200

    final (hopefullly) version of the linear regression example

--- utopia/examples/example_linear_regression.cpp
+++ utopia/examples/example_linear_regression.cpp
@@ -13,8 +13,27 @@
 #include "utopia_Version.hpp"
 using namespace std;
 
+////////////////////////////////////////////////////////////////////////////////
+// With linear regression, we are searching for parameters, such that our model
+// gives the most reasonable prediction. To do this, we need to minimise the cost
+// function J(theta), which basically
+// tells us the difference between the actual value y and the value predicted by
+// our hypothesis function.
+// For this example, we rely on a dataset consisting of s samples. Each sample is
+// defined by an input xi and an output yi. The input xi expresses a
+// population and the output yi expresses the profit earned by a foodtruck of
+// a certain company. Supposing that we would like to have a foodtruck in a city
+// with a certain population, we would be able to predict the profit for the food
+// truck, thanks to our model parametrized by theta. For examples with a population
+// of 43'000, we will be able to write a vector [1, 4.3], - with 1 being the bias,
+// -  and by multiplying this vector by theta we will obtain an estimation
+// of the profit. The cost function is defined as:
+////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////
 // Contains a file whose first columns contain a population/10000 and the profit
-// of a foodtrack in a place with that population/10000
+// of a foodtrack in a place with that population/10000.
+////////////////////////////////////////////////////////////////////////////////
 const char* file = "./../examples/profit.csv";
 
 namespace utopia {
@@ -27,8 +46,10 @@ namespace utopia {
         using SizeType = typename Traits::SizeType;
         using Comm = typename Traits::Communicator;
 
+        ////////////////////////////////////////////////////////////////////////////////
         // Through this constructor, we read the file csv and we save the data in
         // a matrix X and a vector y.
+        ////////////////////////////////////////////////////////////////////////////////
         LinearRegression() : num_of_samples_(96) {
             fstream data_file;
             string line, number;
@@ -55,12 +76,11 @@ namespace utopia {
                 Range r = row_range(X_);
                 SizeType j = r.begin();
 
-                // Check if the csv file opens.
                 int i = 0;
                 data_file.open(file, ios::in);
                 if (!data_file) {
                     cout << "Unable to open file";
-                    exit(1);  // terminate with error
+                    exit(1);
                 }
 
                 // Saving all the data
@@ -85,8 +105,10 @@ namespace utopia {
 
             Xt_ = transpose(X_);
 
+            ////////////////////////////////////////////////////////////////////////////////
             // Initialise the vector fo the squared errors and
-            // the hypothesis function
+            // the hypothesis function.
+            ////////////////////////////////////////////////////////////////////////////////
             errors_ = make_unique<Vector>(l, 0.0);
             h_ = make_unique<Vector>(l, 0.0);
         }
@@ -100,8 +122,10 @@ namespace utopia {
             // Our initial hypothesis
             *h_ = X_ * theta;
 
-            // Evaluating the hypothesis by summing all the differences before the actual value and expected
-            // value
+            ////////////////////////////////////////////////////////////////////////////////
+            // Evaluating the hypothesis by summing all the differences before the actual
+            // value and expected value.
+            ////////////////////////////////////////////////////////////////////////////////
             {
                 auto squared_errors_view = local_view_device(*errors_);
                 auto h_view = const_local_view_device(*h_);
@@ -115,13 +139,23 @@ namespace utopia {
                     });
             }
 
+            ////////////////////////////////////////////////////////////////////////////////
             // Computing the cost, which should go down at each new iteration of the
             // gradient we are using.
+            ////////////////////////////////////////////////////////////////////////////////
             cost = 1. / (2. * num_of_samples_) * sum(*errors_);
 
             return true;
         }
 
+        ////////////////////////////////////////////////////////////////////////////////
+        // The theta values are the paramater of our model and we try to find optimal
+        // some theta and we keep updating them until we find a minumum.
+        // For example, to reach
+        // this minimum, we can use a gradient based algorithm which update theta at each
+        // iteration using gradient information.values which minimise the cost function
+        // J(theta) which is our goal. So we start with
+        ////////////////////////////////////////////////////////////////////////////////
         bool gradient(const Vector& theta, Vector& g) const override {
             if (theta.comm().size() > 1) {
                 utopia_error("Function is not supported in parallel... \n");
@@ -154,45 +188,79 @@ template <class Matrix, class Vector>
 void test() {
     using namespace utopia;
 
+    ////////////////////////////////////////////////////////////////////////////////
     // construct our problem
+    ////////////////////////////////////////////////////////////////////////////////
     LinearRegression<Matrix, Vector> newLinearRegression;
 
+    ////////////////////////////////////////////////////////////////////////////////
     // constructing initial guess
     Vector x;
+    ////////////////////////////////////////////////////////////////////////////////
     x.values(serial_layout(2), 0);
 
-    // // // nonlinear solve
-    // GradientDescent<Vector> solver;
+    ////////////////////////////////////////////////////////////////////////////////
+    // Gradient Descent solver
+    ////////////////////////////////////////////////////////////////////////////////
+    Chrono GD;
+    GD.start();
+    GradientDescent<Vector> solverGD;
 
-    // // set parameters
-    // solver.verbose(true);
-    // solver.max_it(1500);
-    // // solver.dumping_parameter(0.01);
-    // auto strategy_to_choose_alpha = std::make_shared<utopia::Backtracking<Vector>>();
-    // solver.set_line_search_strategy(strategy_to_choose_alpha);
+    ////////////////////////////////////////////////////////////////////////////////
+    // set parameters
+    // solverGD.verbose(true); // uncomment to see full output
+    ////////////////////////////////////////////////////////////////////////////////
+    solverGD.max_it(1500);
+    ////////////////////////////////////////////////////////////////////////////////
+    // solverGD.dumping_parameter(0.01);
+    ////////////////////////////////////////////////////////////////////////////////
+    auto strategy_to_choose_alpha = std::make_shared<utopia::Backtracking<Vector>>();
+    solverGD.set_line_search_strategy(strategy_to_choose_alpha);
 
-    // // solve
-    // solver.solve(newLinearRegression, x);
+    ////////////////////////////////////////////////////////////////////////////////
+    // solve
+    ////////////////////////////////////////////////////////////////////////////////
+    solverGD.solve(newLinearRegression, x);
+    GD.stop();
 
+    ////////////////////////////////////////////////////////////////////////////////
+    // display the solution
+    ////////////////////////////////////////////////////////////////////////////////
+    disp(x, "my_solution with Gradient");
+
+    ////////////////////////////////////////////////////////////////////////////////
+    // Quasi Newton solver
+    ////////////////////////////////////////////////////////////////////////////////
+    Chrono QN;
+    QN.start();
     auto hess_approx = std::make_shared<LBFGS<Vector>>(5);
     auto lsolver = std::make_shared<EmptyPrecondMatrixFreeLinearSolver<Vector>>();
 
     auto precond = hess_approx->build_Hinv_precond();
     lsolver->set_preconditioner(precond);
 
-    QuasiNewton<Vector> solver(hess_approx, lsolver);
+    QuasiNewton<Vector> solverQN(hess_approx, lsolver);
     // solve
-    solver.verbose(true);
-    solver.solve(newLinearRegression, x);
+    solverQN.verbose(true);
+    solverQN.solve(newLinearRegression, x);
+    QN.stop();
 
     // display solution
-    disp(x, "my_solution");
+    disp(x, "my_solution with with QuasiNewton");
+
+    cout << "Gradient Descent took " << GD.get_seconds() << " seconds to converge." << endl;
+    cout << "Quasi Newton took " << QN.get_seconds() << " seconds to converge and was " << flush;
+    cout << GD.get_seconds() - QN.get_seconds() << " seconds faster than graident descent." << endl;
 }
 
+////////////////////////////////////////////////////////////////////////////////
+// This example does not work in parallel.
+// Please run the example with the following command:
+// make -j 4  utopia_examples && ./examples/example_linear_regression
+////////////////////////////////////////////////////////////////////////////////
 int main(int argc, char** argv) {
     using namespace utopia;
 
-// Checking which backend is active.
 #ifdef WITH_PETSC
     using MatrixT = PetscMatrix;
     using VectorT = PetscVector;
