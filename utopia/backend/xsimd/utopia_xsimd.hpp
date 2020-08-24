eb50bc24781248f419f1ae4faf500681e13a664c
zulianp <patrick.zulian@gmail.com>
2020-08-24 19:04:04 +0200

    added cmake stuff for xsimd

---
+++ utopia/backend/xsimd/utopia_xsimd.hpp
@@ -0,0 +1,27 @@
+#ifndef UTOPIA_XSIMD_HPP
+#define UTOPIA_XSIMD_HPP
+
+#include "utopia_Base.hpp"
+
+#ifdef UTOPIA_WITH_XSIMD
+
+#include <xsimd/xsimd.hpp>
+
+namespace utopia {
+    template <typename T, int N>
+    using Batch = xsimd::batch<T, N>;
+
+    using xsimd::load_unaligned;
+    using xsimd::store_unaligned;
+
+    using xsimd::load_aligned;
+    using xsimd::store_aligned;
+}  // namespace utopia
+
+#else  // UTOPIA_WITH_XSIMD
+
+namespace utopia {}
+
+#endif  // UTOPIA_WITH_XSIMD
+
+#endif  // UTOPIA_XSIMD_HPP
