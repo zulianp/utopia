6c258915aa8d40b9e1d5f4c4cd0fe83cd6f5cbf9
Dylan Ramelli <rameld@usi.ch>
2020-08-28 16:30:25 +0200

    Added config file for cygwin-packages

---
+++ utopia/scripts/utopia_cygwin_install_packages.sh
@@ -0,0 +1,10 @@
+apt-cyg install gcc-core
+apt-cyg install gcc-fortran
+apt-cyg install gcc-g++
+apt-cyg install gdb
+apt-cyg install make
+apt-cyg install cmake 
+apt-cyg install openmpi
+apt-cyg install libopenmpi-devel
+apt-cyg install python
+
