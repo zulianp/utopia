e7ea20cc16d097997fcdd66ad7fc364b16488132
Dylan Ramelli <rameld@usi.ch>
2020-08-29 12:01:22 +0200

    if cond for package installation

--- utopia/scripts/utopia_cygwin_install_packages.sh
+++ utopia/scripts/utopia_cygwin_install_packages.sh
@@ -1,3 +1,5 @@
 lynx -source rawgit.com/transcode-open/apt-cyg/master/apt-cyg > apt-cyg
-install apt-cyg /bin
+if ! type "$apt-cyg" > /dev/null; then
+  install apt-cyg /bin
+fi
 apt-cyg install gcc-fortran gcc-g++ gdb make cmake openmpi libopenmpi-devel libopenmpicxx1 liblapack-devel libopenblas python
