f1bc9f9b836fce0b00786d572d6c6f6e21743d69
Dylan Ramelli <rameld@usi.ch>
2020-09-15 09:23:24 +0200

    testing on windows

--- utopia/scripts/utopia_cygwin_install_packages.sh
+++ utopia/scripts/utopia_cygwin_install_packages.sh
@@ -2,4 +2,4 @@ lynx -source rawgit.com/transcode-open/apt-cyg/master/apt-cyg > apt-cyg
 if ! type "$apt-cyg" > /dev/null; then
   install apt-cyg /bin
 fi
-apt-cyg install gcc-fortran gcc-g++ gdb make cmake openmpi libopenmpi-devel libopenmpicxx1 liblapack-devel libopenblas python
+apt-cyg install gcc-fortran
