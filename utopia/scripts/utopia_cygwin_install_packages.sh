dbf44a03c21823812d5242fd7d194424d90b518c
Dylan Ramelli <rameld@usi.ch>
2020-09-15 09:53:46 +0200

    update again

--- utopia/scripts/utopia_cygwin_install_packages.sh
+++ utopia/scripts/utopia_cygwin_install_packages.sh
@@ -2,4 +2,15 @@ lynx -source rawgit.com/transcode-open/apt-cyg/master/apt-cyg > apt-cyg
 if ! type "$apt-cyg" > /dev/null; then
   install apt-cyg /bin
 fi
-apt-cyg install gcc-fortran gcc-g++ gdb make cmake openmpi libopenmpi-devel libopenmpicxx1 libopenblas liblapack-devel python doxygen
+apt-cyg install gcc-fortran
+apt-cyg install gcc-g++
+apt-cyg install gdb
+apt-cyg install make
+apt-cyg install cmake
+apt-cyg install openmpi
+apt-cyg install libopenmpi-devel
+apt-cyg install libopenmpicxx1
+apt-cyg install libopenblas
+apt-cyg install liblapack-devel
+apt-cyg install python
+apt-cyg install doxygen
