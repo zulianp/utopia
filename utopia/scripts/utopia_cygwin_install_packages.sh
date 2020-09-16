0787ad671dafead009c9f9343688b96d9293cfef
Dylan Ramelli <rameld@usi.ch>
2020-09-16 13:45:29 +0200

    Adding script for installing dependencies on windows

---
+++ utopia/scripts/utopia_cygwin_install_packages.sh
@@ -0,0 +1,16 @@
+lynx -source rawgit.com/transcode-open/apt-cyg/master/apt-cyg > apt-cyg
+if ! type "$apt-cyg" > /dev/null; then
+  install apt-cyg /bin
+fi
+apt-cyg install gcc-fortran
+apt-cyg install gcc-g++
+apt-cyg install gdb
+apt-cyg install make
+apt-cyg install cmake
+apt-cyg install openmpi
+apt-cyg install libopenmpi-devel
+apt-cyg install libopenmpicxx1
+apt-cyg install libopenblas
+apt-cyg install liblapack-devel
+apt-cyg install python
+apt-cyg install doxygen
