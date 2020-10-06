eb459ee50400e88c07a3ace17b42042cbc8f31af
Filippo Cesana <filippo.cesana@gmail.com>
2020-10-06 11:32:00 +0200

    fixed last commit

--- utopia/examples/example_linear_regression.cpp
+++ utopia/examples/example_linear_regression.cpp
@@ -14,29 +14,34 @@
 using namespace std;
 
 /**
- * With linear regression, we are searching for parameters, such that our model
- * gives the most reasonable prediction. To do this, we need to minimise the cost
- * function J(theta), which basically
- * tells us the difference between the actual value y and the value predicted by
- * our hypothesis function.
- * For this example, we rely on a dataset consisting of s samples. Each sample is
- * defined by an input xi and an output yi. The input xi expresses a
- * population and the output yi expresses the profit earned by a foodtruck of
- * a certain company. Supposing that we would like to have a foodtruck in a city
- * with a certain population, we would be able to predict the profit for the food
- * truck, thanks to our model parametrized by theta. For examples with a population
- * of 43'000, we will be able to write a vector [1, 4.3], - with 1 being the bias,
- * -  and by multiplying this vector by theta we will obtain an estimation
- * of the profit. The cost function is defined as:
+ * With linear regression, we are searching for parameters, such that our model gives
+ * the most reasonable prediction. To do this, we need to minimise the cost function
+ * \f$J({\Theta}) : \mathbb{R}^2 \rightarrow  \mathbb{R}\f$, which basically tells us the
+ * difference between the actual value y and the value predicted by our hypothesis function.
  */
 
 /**
- * Contains a file whose first columns contain a population/10000 and the profit
+ * Contains a file whose first column states a population/10000 and the second columns states the profit
  * of a foodtrack in a place with that population/10000.
  */
+const char* file = "./../examples/data_for_examples/profit.csv";
 
-/** An example:< \f$x_0 + y^3\f$ */
-const char* file = "./../examples/profit.csv";
+/**
+ * For this example, we rely on a dataset consisting of s samples. Each sample is defined by an
+ * input \f$x_i$ and an output $y_{i}\f$. The input $x_{i}$ expresses a population and the output \f$y_{i}\f$
+ * expresses the profit earned by a foodtruck of a certain company. Supposing that we would like to have
+ * a foodtruck in a city with a certain population, we would be able to predict the profit for the food
+ * truck, thanks to our model parametrized by \f${\Theta} \in \mathbb{R}^{2}\f$. For examples with a
+ * population of 43'000, we will be able to write a vector [1, 4.3], - with 1 being the bias, -  and
+ * by multiplying this vector by \f${\Theta}\f$ we will obtain an estimation of the profit.
+ */
+
+/**
+ * In our implementation, each population example corresponds to a row in our vector \f$x \in \mathbb{R}^{s}\f$,
+ * to which we add an additional columns and we set it to all ones to add the bias. The vector x then becomes
+ * a matrix \f$X \in \mathbb{R}^{s \times 2}$\f. Similarly, \f$y\in \mathbb{R}^{s}\f$ is a vector with each row
+ * correspondig to an example of the profit.
+ */
 
 namespace utopia {
 
@@ -117,10 +122,14 @@ namespace utopia {
             h_ = make_unique<Vector>(l, 0.0);
         }
 
-        /** \f\begin{equation*}
+        /**
+         * Our cost function is defined as it follows:
+         * \f{equation*}{
          * 	J(\Theta)
          * =\frac{1}{2s}\mathlarger{\mathlarger{‎‎\sum}}_{i=1}^{s}(h_{\Theta}({\Theta}^{(i)})-y^{(i)})^2
-         * \end{equation*}
+         *  \f}
+         * Also, the mean si halved since it makes more convienent to compute the gradient descent: the
+         * derivative term will delete the \f$\frac{1}{2}\f$ term.
          */
         bool value(const Vector& theta, Scalar& cost) const override {
             if (theta.comm().size() > 1) {
@@ -160,13 +169,42 @@ namespace utopia {
         }
 
         /**
-         * The theta values are the paramater of our model and we try to find optimal
-         * some theta and we keep updating them until we find a minumum.
-         * For example, to reach
-         * this minimum, we can use a gradient based algorithm which update theta at each
-         * iteration using gradient information.values which minimise the cost function
-         * J(theta) which is our goal. So we start with
+         * \f$ h_{\Theta}(x, {\Theta})\f$ is the hypothesis function  \f$h_{\Theta} : \mathbb{R}^{2} \rightarrow
+         * \mathbb{R}\f$ and it is given by the following linear model:
+         * \f{equation*}{
+         * h_{\Theta}({\Theta}) = {\Theta}X
+         * \f}
          */
+
+        /**
+         * The \f${\Theta}\f$ values are the paramaters of our model and we try to find optimal values which minimise
+         * the cost function \f$J({\Theta})\f$, which is our goal. So we start with some \f$\Theta\f$ and we keep
+         * updating them until we find a minimum.
+         */
+
+        /**
+         * For example, to reach this minimum, we can use a gradient based algorithm which update \f${\Theta}\f$ at each
+         * iteration using gradient information.
+         */
+
+        /**
+         * For \f$j = \{0,1\}\f$, the gradient has the following form:
+         */
+
+        /**
+         * \f{equation*}{
+         * \frac{\partial}{\partial \Theta_{0}}J(\Theta) = \frac{1}{s}
+         * \mathlarger{\mathlarger{‎‎\sum}}_{i=1}^{s}(h_{\Theta}({\Theta}^{(i)})-y^{(i)}),
+         * \f}
+         */
+
+        /**
+         * \f{equation*}{
+         * \frac{\partial}{\partial \Theta_{1}}J(\Theta) = \frac{1}{s}
+         * \mathlarger{\mathlarger{‎‎\sum}}_{i=1}^{s}(h_{\Theta}({\Theta}^{(i)})-y^{(i)})x^{(i)}
+         * \f}
+         */
+
         bool gradient(const Vector& theta, Vector& g) const override {
             if (theta.comm().size() > 1) {
                 utopia_error("Function is not supported in parallel... \n");
@@ -184,7 +222,7 @@ namespace utopia {
             return true;
         }
 
-    private:
+    private:  // Constructor
         SizeType num_of_samples_;
         Matrix X_;
         Matrix Xt_;
@@ -227,7 +265,7 @@ void test() {
     GD.stop();
 
     /** Display the solution*/
-    disp(x, "my_solution with Gradient");
+    disp(x, "The solution with Gradient");
 
     /** Quasi Newton Solver */
     Chrono QN;
@@ -245,7 +283,7 @@ void test() {
     QN.stop();
 
     /** Display solution */
-    disp(x, "my_solution with with QuasiNewton");
+    disp(x, "The solution with with QuasiNewton");
 
     cout << "Gradient Descent took " << GD.get_seconds() << " seconds to converge." << endl;
     cout << "Quasi Newton took " << QN.get_seconds() << " seconds to converge and was " << flush;
