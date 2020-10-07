0a031dceeafd497759e941685173b2bf88ebdd6a
U-DYLAN-PC\Dylan <rameld@usi.ch>
2020-10-07 13:01:16 +0200

    Fixed MPIclang problem and added zlib to packages script, still need to make findmpi better

--- utopia/cmake/FindMPIExtended.cmake
+++ utopia/cmake/FindMPIExtended.cmake
@@ -1,5 +1,72 @@
 #First with try with clang compile or else
 
+
+   # message(STATUS "On windows skipping preamble for FindMPIExtended = ${CYGWIN}")
+
+if(CYGWIN)
+  #  message(STATUS "On windows skipping preamble for FindMPIExtended")
+
+    find_library(MPI_C_LIBRARY
+    NAMES mpi
+    PATHS ${MPI_DIR}/lib
+          $ENV{MPI_DIR}/lib
+          /usr/lib
+          DOC "The MPI_C_LIBRARY library to link against"
+    )
+
+    if(NOT MPI_C_LIBRARY)
+      message(FATAL_ERROR "Could not find MPI_C_LIBRARY")
+    endif()
+
+    list(APPEND MPI_CXX_LIBRARIES ${MPI_C_LIBRARY})
+
+    get_filename_component(MPI_LIB_DIR ${MPI_C_LIBRARY} PATH)
+
+        find_path(MPI_C_HEADERS mpi.h
+        HINTS ${MPI_DIR}/include
+              $ENV{MPI_DIR}/include
+              ${MPI_LIB_DIR}/../../include
+              ${MPI_LIB_DIR}/../include
+              /usr/include
+        DOC "The MPI_C_HEADERS path"
+    )
+
+    IF(MPI_C_HEADERS)
+        find_file(MPI_CXX_COMPILER
+            NAMES mpic++
+                  mpicxx
+                  mpicxx-openmpi-mp
+                  mpicxx-mpich-clang
+                  mpic++-mpich-clang
+                  mpic++-mpich-mp
+            HINTS ${MPI_DIR}/bin
+                  $ENV{MPI_DIR}/bin
+                  ${MPI_C_HEADERS}/../bin
+                  /usr/bin
+            DOC "the MPI_CXX_COMPILER path"
+        )
+
+        find_file(MPI_C_COMPILER
+            NAMES mpicc-openmpi-mp
+                  mpicc-mpich-clang
+                  mpicc-mpich-mp
+                  mpicc
+            HINTS ${MPI_DIR}/bin
+                  $ENV{MPI_DIR}/bin
+                  ${MPI_C_HEADERS}/../bin
+                              /usr/bin
+                              DOC "the MPI_C_COMPILER path"
+        )
+
+          set(MPI_FOUND TRUE)
+
+            list(APPEND MPI_CXX_INCLUDE_PATH ${MPI_C_HEADERS})
+            message(STATUS "MPI_CXX_INCLUDE_PATH=${MPI_CXX_INCLUDE_PATH}\nMPI_CXX_LIBRARIES=${MPI_CXX_LIBRARIES}\nMPI_CXX_COMPILER=${MPI_CXX_COMPILER}\nMPI_C_COMPILER=${MPI_C_COMPILER}")
+    endif()
+
+
+else()
+
 find_library(MPI_CLANG_C_LIBRARY
     NAMES mpi
           mpi-mpich-clang
@@ -23,6 +90,7 @@ find_library(MPI_CLANG_CXX_LIBRARY
           /opt/local/lib/mpich-mp/
           /opt/local/lib/mpich-clang/
           /opt/local/lib
+          /usr/lib/
 
     DOC "The MPI_CLANG_CXX_LIBRARY library to link against"
 )
@@ -91,7 +159,7 @@ IF(MPI_CLANG_CXX_LIBRARY)
 ENDIF()
 
 MESSAGE(STATUS "MPI: ${MPI_CXX_LIBRARIES} ${MPI_CLANG_HEADERS} ${MPI_CXX_COMPILER}")
-
+endif()
 
 IF(NOT MPI_FOUND)
     find_package(MPI)
