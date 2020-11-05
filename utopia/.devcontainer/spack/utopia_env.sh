ea63ac5580815c1d556a85e945bbd354acac78dd
zulianp <patrick.zulian@gmail.com>
2020-11-05 18:44:39 +0100

    utopia and pytorch coexist in alpine linux

--- utopia/.devcontainer/spack/utopia_env.sh
+++ utopia/.devcontainer/spack/utopia_env.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 # utopia_env.sh
 
-
 DEFAULT_SPACK_DIR=$PWD/spack
 SPACK_DIR=${1:-$DEFAULT_SPACK_DIR}
-
 . $SPACK_DIR/share/spack/setup-env.sh
 
-spack load cmake doxygen clang-format gfortran python swig 
+spack load trilinos
 spack load anaconda3
-spack load openblas
-spack load lapack
-# spack load petsc
-# spack load trilinos
+spack load cmake doxygen clang-format gfortran python swig 
--- utopia/.devcontainer/spack/utopia_install_spack.sh
+++ utopia/.devcontainer/spack/utopia_install_spack.sh
@@ -3,15 +3,15 @@
 
 DEFAULT_SPACK_DIR=$PWD/spack
 SPACK_DIR=${1:-$DEFAULT_SPACK_DIR}
+git clone https://github.com/spack/spack $SPACK_DIR
+. $SPACK_DIR/share/spack/setup-env.sh && export PATH=$SPACK_DIR/bin:$PATH
 
-git clone https://github.com/spack/spack $SPACK_DIR && \
-    . $SPACK_DIR/share/spack/setup-env.sh && export PATH=$SPACK_DIR/bin:$PATH && \
-    spack install cmake doxygen clang-format gfortran python swig &&  \
-    spack install anaconda3 &&  \
-    spack install openblas  &&  \
-    spack install lapack    &&  \
-    spack load anaconda3    &&  \
-    conda install --yes  pytorch torchvision torchaudio cpuonly -c pytorch
+spack install trilinos +tpetra +openmp +muelu +amesos2 +ifpack2 +ml #+adios2
+spack install doxygen python swig
+
+# Install pytorch
+spack install anaconda3
+spack load anaconda3
+conda install --yes  pytorch torchvision torchaudio cpuonly -c pytorch
 
 # spack install petsc +superlu-dist +mumps +metis +exodusii
-# spack install trilinos +tpetra +openmp +muelu +amesos2 +adios2 +ifpack2 +ml
