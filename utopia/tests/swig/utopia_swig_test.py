774fc58186ff1a98ede19f26a0408df6d7e7461f
Dylan Ramelli <rameld@usi.ch>
2020-11-23 09:45:51 +0100

    create swig test in test folder /swig

---
+++ utopia/tests/swig/utopia_swig_test.py
@@ -0,0 +1,27 @@
+import utopia as u
+
+#Initialize utopia
+u.init()
+
+def test_vector():
+	assert u.Vector()
+
+def test_matrix():
+
+def test_add():
+
+def test_solve():
+
+def test_disp():
+
+
+
+
+
+u.finalize()
+if __name__ == '__main__':
+	test_vector()
+	# test_matrix()
+	# test_add()
+	# test_solve()
+	# test_disp()
