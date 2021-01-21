47735e8f5fa79bbbf575fecbc6926fdbc320f824
zulianp <patrick.zulian@gmail.com>
2021-01-21 10:36:56 +0100

    added app ls_solve

--- utopia/apps/ls_solve/utopia_ls_solve.cpp
+++ utopia/apps/ls_solve/utopia_ls_solve.cpp
@@ -15,39 +15,109 @@
 
 namespace utopia {
 
+    void convert_mm_matrix_to_petsc_bin(Input &in) {
+        std::string path_in, path_out;
+        in.get("in", path_in);
+        in.get("out", path_out);
+
+        TpetraMatrix mat;
+        read(path_in, mat);
+
+        PetscMatrix petsc_mat;
+        convert(mat, petsc_mat);
+        write(path_out, petsc_mat);
+    }
+
+    UTOPIA_REGISTER_APP(convert_mm_matrix_to_petsc_bin);
+
+    void convert_mm_vector_to_petsc_bin(Input &in) {
+        std::string path_in, path_out;
+        in.get("in", path_in);
+        in.get("out", path_out);
+
+        TpetraVector mat;
+        read(path_in, mat);
+
+        PetscVector petsc_mat;
+        convert(mat, petsc_mat);
+        write(path_out, petsc_mat);
+    }
+
+    UTOPIA_REGISTER_APP(convert_mm_vector_to_petsc_bin);
+
+    template <class Matrix, class Vector>
+    class LSolveApp : public Configurable {
+    public:
+        void read(Input &in) override {
+            Matrix A;
+            Vector x, b, oracle;
+
+            MPITimeStatistics stats(x.comm());
+
+            //////////////////////////////////////////
+            stats.start();
+
+            std::string path_A, path_b, path_oracle, path_output = "out.mm";
+
+            in.get("A", path_A);
+            in.get("b", path_b);
+            in.get("path_output", path_output);
+
+            if (path_A.empty()) {
+                utopia::err() << "[Error] A undefined!!!\n";
+                return;
+            }
+
+            if (path_b.empty()) {
+                utopia::err() << "[Warning] b is undefined, using vector of ones!!!\n";
+                b.values(row_layout(A), 1.0);
+            } else {
+                utopia::read(path_b, b);
+            }
+
+            utopia::read(path_A, A);
+
+            if (!path_oracle.empty()) {
+                utopia::read("../data/knf/matrices/NavierStokes-O-0.sln.1", oracle);
+            }
+
+            stats.stop_collect_and_restart("read_files");
+
+            x.zeros(row_layout(A));
+
+            KSPSolver<Matrix, Vector> solver;
+            solver.read(in);
+
+            stats.stop_collect_and_restart("read_settings");
+
+            utopia::out() << "ndofs " << x.size() << std::endl;
+            solver.solve(A, b, x);
+
+            stats.stop_collect_and_restart("solve");
+
+            write(path_output, x);
+
+            stats.stop_collect_and_restart("write");
+            stats.describe(utopia::out().stream());
+        }
+    };
+
     void ls_solve(Input &in) {
-        TpetraMatrix A;
-        TpetraVector x, b, sol;
+#ifdef UTOPIA_WITH_PETSC
+        std::string backend = "petsc";
+#else
+        std::string backend = "trilinos";
+#endif
 
-        MPITimeStatistics stats(x.comm());
+        in.get("backend", backend);
 
-        //////////////////////////////////////////
-        stats.start();
-
-        read("../data/knf/matrices/NavierStokes-O-0.mm.1", A);
-        read("../data/knf/matrices/NavierStokes-O-0.rhs.1", b);
-        read("../data/knf/matrices/NavierStokes-O-0.sln.1", sol);
-
-        stats.stop_collect_and_restart("read_files");
-
-        // disp(b);
-
-        // BelosSolver<TpetraMatrix, TpetraVector> solver;
-        KSPSolver<TpetraMatrix, TpetraVector> solver;
-        solver.read(in);
-
-        stats.stop_collect_and_restart("read_settings");
-
-        disp("solve begin");
-        solver.solve(A, b, x);
-        disp("solve end");
-
-        stats.stop_collect_and_restart("solve");
-
-        write("../data/knf/matrices/result.mm", x);
-
-        stats.stop_collect_and_restart("write");
-        stats.describe(utopia::out().stream());
+        if (backend == "petsc") {
+            LSolveApp<PetscMatrix, PetscVector> app;
+            app.read(in);
+        } else {
+            LSolveApp<TpetraMatrix, TpetraVector> app;
+            app.read(in);
+        }
     }
 
     UTOPIA_REGISTER_APP(ls_solve);
