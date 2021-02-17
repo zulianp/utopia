b5c5438d5a4bb2a9a3a27f29b49fa223e40b133c
zulianp <patrick.zulian@gmail.com>
2021-02-17 14:51:56 +0100

    rolled back changes on PGS simd

---
+++ utopia/apps/PF_frac/inputs/THPC/Sneddon3D/sneddon_3D_strong_scaling.py
@@ -0,0 +1,39 @@
+import os
+import subprocess
+
+# uncomment the lines for the small/big experiment that you want to submit jobs into the queue
+# inputFile is the full path to the file containing the experiments parameters
+# basedir is the starting directory where each experiment setup will be saved (can be relative or absolute)
+
+# num_nodes = (4, 5, 6, 7, 8, 12, 16, 20, 24, 28, 32)
+num_nodes = (1, 2, 4, 8)
+inputFile = '$SCRATCH/Sneddon3D_small.json'
+basedir = 'Sneddon3D_small'
+# num_nodes = (40, 48, 56, 64, 80, 96, 112, 128, 160, 192, 224, 256)
+# inputFile = '$SCRATCH/Sneddon3D.json'
+# basedir = 'Sneddon3D_large'
+
+
+for nodes in num_nodes:
+    dirname = f'{basedir}/{nodes:03}nodes'
+    jobfile = 'job.sh'
+    if not os.path.exists(dirname):
+        os.makedirs(dirname)
+    with open(os.path.join(dirname, jobfile), 'w') as scriptfile:
+        scriptfile.write(f"""#!/bin/bash
+#SBATCH --job-name=utopia_Sneddon3d_strong_scaling
+#SBATCH --output=output_%j.out
+#SBATCH --time=02:30:00
+#SBATCH --nodes={nodes}
+#SBATCH --ntasks-per-core=1
+#SBATCH --ntasks-per-node=12
+#SBATCH --cpus-per-task=1
+#SBATCH --constraint=gpu
+#SBATCH --account=sm43
+
+export OMP_PROC_BIND=false
+export OMP_NUM_THREADS=1
+source $APPS/UES/anfink/cpu/environment
+srun $SCRATCH/utopia_exec @file {inputFile}""")
+
+    subprocess.call(['sbatch', jobfile], cwd=dirname)
