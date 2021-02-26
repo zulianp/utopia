1b00b9ecd7c4c9d42dbe7db8c1c4b4770febc142
Andreas Fink <andreas.fink@cscs.ch>
2021-02-26 18:47:41 +0100

    nvcc fixes (template parameters must have a name, move one function to public, because it uses UTOPIA_LAMDA, for which nvcc enforces public visibility

--- utopia/solvers/multilevel/utopia_RMTR_inf.hpp
+++ utopia/solvers/multilevel/utopia_RMTR_inf.hpp
@@ -245,11 +245,21 @@ namespace utopia {
                                    int> = 0>
         void execute_truncation(const SizeType & /*level*/) {}
 
+
+        // TODO:: it should not be necessary
+        void make_ml_iterate_feasible(const SizeType &level) override {
+            // this->make_iterate_feasible(this->memory_.x[level]);
+
+            // TODO:: check with levels
+            this->get_projection(this->active_lower(level), this->active_upper(level), this->memory_.x[level]);
+        }
+
+    public:  // nvcc requires it to be public when using lambda
         template <class T = MLConstraints,
                   std::enable_if_t<std::is_same<T, TRGrattonBoxKornhuberTruncation<Matrix, Vector> >::value ||
                                        std::is_same<T, TRKornhuberBoxKornhuberTruncation<Matrix, Vector> >::value ||
                                        std::is_same<T, TRGelmanMandelBoxKornhuberTruncation<Matrix, Vector> >::value,
-                                   int> = 0>
+                                   int> _nvcc_needs_a_name = 0>
         void execute_truncation(const SizeType &level) {
             // truncate_interpolation
             if (level == this->n_levels() - 1) {
@@ -299,16 +309,6 @@ namespace utopia {
 
             }  // level check
         }
-
-        // TODO:: it should not be necessary
-        void make_ml_iterate_feasible(const SizeType &level) override {
-            // this->make_iterate_feasible(this->memory_.x[level]);
-
-            // TODO:: check with levels
-            this->get_projection(this->active_lower(level), this->active_upper(level), this->memory_.x[level]);
-        }
-
-    public:  // nvcc requires it to be public when using lambdas
         bool solve_qp_subproblem(const SizeType &level, const bool &flg) override {
             Scalar radius = this->memory_.delta[level];
 
