588f61bd949fbfe5d8a1819d95555f0422a10cf3
zulianp <patrick.zulian@gmail.com>
2021-04-02 16:06:50 +0200

    fixed libmesh to moonolith conversion interface

--- utopia_fe/interoperability/moonolith_libmesh/utopia_moonolith_libmesh.cpp
+++ utopia_fe/interoperability/moonolith_libmesh/utopia_moonolith_libmesh.cpp
@@ -21,18 +21,21 @@ namespace utopia {
             case 1: {
                 auto m_mesh = std::make_shared<::moonolith::Mesh<Scalar, 1>>(in.comm().raw_comm());
                 convert(in.raw_type(), *m_mesh);
+                out.wrap(m_mesh);
                 break;
             }
 
             case 2: {
                 auto m_mesh = std::make_shared<::moonolith::Mesh<Scalar, 2>>(in.comm().raw_comm());
                 convert(in.raw_type(), *m_mesh);
+                out.wrap(m_mesh);
                 break;
             }
 
             case 3: {
                 auto m_mesh = std::make_shared<::moonolith::Mesh<Scalar, 3>>(in.comm().raw_comm());
                 convert(in.raw_type(), *m_mesh);
+                out.wrap(m_mesh);
                 break;
             }
             default: {
@@ -44,28 +47,35 @@ namespace utopia {
     void ConvertFunctionSpace<utopia::libmesh::FunctionSpace, utopia::moonolith::FunctionSpace>::apply(
         const utopia::libmesh::FunctionSpace &in,
         utopia::moonolith::FunctionSpace &out) {
-        auto m_mesh = std::make_shared<utopia::moonolith::Mesh>(in.comm());
-        convert_mesh(in.mesh(), *m_mesh);
-
-        out.init(m_mesh);
+        using Scalar = Traits<utopia::moonolith::Mesh>::Scalar;
 
         const int dim = in.mesh().spatial_dimension();
 
         switch (dim) {
             case 1: {
-                // ConvertMeshSTK2Moonolith<1>::copy_meta_info(in, out);
+                auto m_mesh = std::make_shared<::moonolith::Mesh<Scalar, 1>>(in.comm().raw_comm());
+                auto m_space = std::make_shared<::moonolith::FunctionSpace<::moonolith::Mesh<Scalar, 1>>>(m_mesh);
+                convert(in.raw_type().get_mesh(), in.raw_type_dof_map(), 0, *m_space, 0);
+                out.wrap(m_space);
                 break;
             }
 
             case 2: {
-                // ConvertMeshSTK2Moonolith<2>::copy_meta_info(in, out);
+                auto m_mesh = std::make_shared<::moonolith::Mesh<Scalar, 2>>(in.comm().raw_comm());
+                auto m_space = std::make_shared<::moonolith::FunctionSpace<::moonolith::Mesh<Scalar, 2>>>(m_mesh);
+                convert(in.raw_type().get_mesh(), in.raw_type_dof_map(), 0, *m_space, 0);
+                out.wrap(m_space);
                 break;
             }
 
             case 3: {
-                // ConvertMeshSTK2Moonolith<3>::copy_meta_info(in, out);
+                auto m_mesh = std::make_shared<::moonolith::Mesh<Scalar, 3>>(in.comm().raw_comm());
+                auto m_space = std::make_shared<::moonolith::FunctionSpace<::moonolith::Mesh<Scalar, 3>>>(m_mesh);
+                convert(in.raw_type().get_mesh(), in.raw_type_dof_map(), 0, *m_space, 0);
+                out.wrap(m_space);
                 break;
             }
+
             default: {
                 Utopia::Abort();
             }
