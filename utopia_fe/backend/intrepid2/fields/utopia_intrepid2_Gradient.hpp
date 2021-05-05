7b0a89e07fb88499f99ed72947abf8478edb68a7
zulianp <patrick.zulian@gmail.com>
2021-05-05 15:30:37 +0200

    refactored gradient in two different classes

---
+++ utopia_fe/backend/intrepid2/fields/utopia_intrepid2_Gradient.hpp
@@ -0,0 +1,129 @@
+#ifndef UTOPIA_INTREPID2_GRADIENT_HPP
+#define UTOPIA_INTREPID2_GRADIENT_HPP
+
+#include "utopia_Traits.hpp"
+#include "utopia_Views.hpp"
+
+#include "utopia_intrepid2_Commons.hpp"
+#include "utopia_intrepid2_FE.hpp"
+#include "utopia_intrepid2_FEAssembler.hpp"
+#include "utopia_intrepid2_Field.hpp"
+#include "utopia_intrepid2_TensorField.hpp"
+
+namespace utopia {
+    namespace intrepid2 {
+
+        template <typename Scalar>
+        class Gradient : public QPTensorField<Scalar> {
+        public:
+            using Super = utopia::intrepid2::QPTensorField<Scalar>;
+            using FE = utopia::intrepid2::FE<Scalar>;
+            using SizeType = typename FE::SizeType;
+            using DynRankView = typename FE::DynRankView;
+            using FunctionSpaceTools = typename FE::FunctionSpaceTools;
+            using ExecutionSpace = typename FE::ExecutionSpace;
+
+            Gradient(const std::shared_ptr<FE> &fe, const std::string &name = "Gradient") : Super(fe, name) {}
+
+            class Rank1Op {
+            public:
+                UTOPIA_INLINE_FUNCTION Rank1Op(const DynRankView &grad, const DynRankView &coeff)
+                    : grad(grad), coeff(coeff), num_fields(grad.extent(1)) {}
+
+                UTOPIA_INLINE_FUNCTION Scalar operator()(const int cell, const int qp, const int d) const {
+                    Scalar ret = 0.0;
+                    for (int i = 0; i < num_fields; ++i) {
+                        ret += coeff(cell, i) * grad(cell, i, qp, d);
+                    }
+
+                    return ret;
+                }
+
+                const DynRankView grad, coeff;
+                const int num_fields;
+            };
+
+            class Rank1OpAndStore {
+            public:
+                UTOPIA_INLINE_FUNCTION Rank1OpAndStore(const DynRankView &grad,
+                                                       const DynRankView &coeff,
+                                                       DynRankView &field)
+                    : op_(grad, coeff), field(field) {}
+
+                UTOPIA_INLINE_FUNCTION void operator()(const int cell, const int qp, const int d) const {
+                    field(cell, qp, d) = op_(cell, qp, d);
+                }
+
+                Rank1Op op_;
+                DynRankView field;
+            };
+
+            class Rank2Op {
+            public:
+                UTOPIA_INLINE_FUNCTION Rank2Op(const DynRankView &grad, const DynRankView &coeff)
+                    : grad(grad), coeff(coeff), num_fields(grad.extent(1)), n_var(coeff.extent(1) / num_fields) {}
+
+                UTOPIA_INLINE_FUNCTION Scalar operator()(const int cell,
+                                                         const int qp,
+                                                         const int var,
+                                                         const int d) const {
+                    Scalar ret = 0.0;
+                    for (int i = 0; i < num_fields; ++i) {
+                        ret += coeff(cell, i * n_var + var) * grad(cell, i, qp, d);
+                    }
+
+                    return ret;
+                }
+
+                const DynRankView grad, coeff;
+                const int num_fields;
+                const int n_var;
+            };
+
+            class Rank2OpAndStore {
+            public:
+                UTOPIA_INLINE_FUNCTION Rank2OpAndStore(const DynRankView &grad,
+                                                       const DynRankView &coeff,
+                                                       DynRankView &field)
+                    : op_(grad, coeff), spatial_dim(grad.extent(3)), field(field) {}
+
+                UTOPIA_INLINE_FUNCTION void operator()(const int cell, const int qp, const int var, const int d) const {
+                    field(cell, qp, var * spatial_dim + d) = op_(cell, qp, var, d);
+                }
+
+                Rank2Op op_;
+                const int spatial_dim;
+                DynRankView field;
+            };
+
+            void init(const Field<Scalar> &coeff) {
+                assert(coeff.tensor_size() > 0);
+                assert(coeff.is_coefficient());
+                this->set_tensor_size(coeff.tensor_size(), this->fe()->spatial_dimension());
+                init(coeff.data());
+            }
+
+            bool is_coefficient() const override { return false; }
+
+        private:
+            void init(const DynRankView &coeff) {
+                this->ensure_field();
+
+                if (this->rank() == 1) {
+                    Kokkos::parallel_for(this->name() + "::init_rank1",
+                                         this->rank1_range(),
+                                         Rank1OpAndStore(this->fe()->grad, coeff, this->data()));
+                } else {
+                    assert(this->rank() == 2);
+
+                    Kokkos::parallel_for(this->name() + "::init_rank2",
+                                         this->rank2_range(),
+                                         Rank2OpAndStore(this->fe()->grad, coeff, this->data()));
+                }
+            }
+        };
+
+    }  // namespace intrepid2
+}  // namespace utopia
+
+#endif  // UTOPIA_INTREPID2_GRADIENT_HPP
--- utopia_fe/backend/intrepid2/fields/utopia_intrepid2_TensorField.hpp
+++ utopia_fe/backend/intrepid2/fields/utopia_intrepid2_TensorField.hpp
@@ -1,5 +1,5 @@
-#ifndef UTOPIA_INTREPID2_GRADIENT_HPP
-#define UTOPIA_INTREPID2_GRADIENT_HPP
+#ifndef UTOPIA_INTREPID2_TENSOR_FIELD_HPP
+#define UTOPIA_INTREPID2_TENSOR_FIELD_HPP
 
 #include "utopia_Traits.hpp"
 #include "utopia_Views.hpp"
@@ -13,7 +13,7 @@ namespace utopia {
     namespace intrepid2 {
 
         template <typename Scalar>
-        class Gradient : public QPField<Scalar> {
+        class QPTensorField : public QPField<Scalar> {
         public:
             using Super = utopia::intrepid2::QPField<Scalar>;
             using FE = utopia::intrepid2::FE<Scalar>;
@@ -22,90 +22,17 @@ namespace utopia {
             using FunctionSpaceTools = typename FE::FunctionSpaceTools;
             using ExecutionSpace = typename FE::ExecutionSpace;
 
-            Gradient(const std::shared_ptr<FE> &fe, const std::string &name = "Gradient") : Super(fe) {
+            using Rank1Range = Kokkos::MDRangePolicy<Kokkos::Rank<3>, ExecutionSpace>;
+            using Rank2Range = Kokkos::MDRangePolicy<Kokkos::Rank<4>, ExecutionSpace>;
+
+            QPTensorField(const std::shared_ptr<FE> &fe, const std::string &name = "QPTensorField") : Super(fe) {
                 this->set_name(name);
             }
 
-            class Rank1Op {
-            public:
-                UTOPIA_INLINE_FUNCTION Rank1Op(const DynRankView &grad, const DynRankView &coeff)
-                    : grad(grad), coeff(coeff), num_fields(grad.extent(1)) {}
-
-                UTOPIA_INLINE_FUNCTION Scalar operator()(const int cell, const int qp, const int d) const {
-                    Scalar ret = 0.0;
-                    for (int i = 0; i < num_fields; ++i) {
-                        ret += coeff(cell, i) * grad(cell, i, qp, d);
-                    }
-
-                    return ret;
-                }
-
-                const DynRankView grad, coeff;
-                const int num_fields;
-            };
-
-            class Rank1OpAndStore {
-            public:
-                UTOPIA_INLINE_FUNCTION Rank1OpAndStore(const DynRankView &grad,
-                                                       const DynRankView &coeff,
-                                                       DynRankView &field)
-                    : op_(grad, coeff), field(field) {}
-
-                UTOPIA_INLINE_FUNCTION void operator()(const int cell, const int qp, const int d) const {
-                    field(cell, qp, d) = op_(cell, qp, d);
-                }
-
-                Rank1Op op_;
-                DynRankView field;
-            };
-
-            class Rank2Op {
-            public:
-                UTOPIA_INLINE_FUNCTION Rank2Op(const DynRankView &grad, const DynRankView &coeff)
-                    : grad(grad), coeff(coeff), num_fields(grad.extent(1)), n_var(coeff.extent(1) / num_fields) {}
-
-                UTOPIA_INLINE_FUNCTION Scalar operator()(const int cell,
-                                                         const int qp,
-                                                         const int var,
-                                                         const int d) const {
-                    Scalar ret = 0.0;
-                    for (int i = 0; i < num_fields; ++i) {
-                        ret += coeff(cell, i * n_var + var) * grad(cell, i, qp, d);
-                    }
-
-                    return ret;
-                }
-
-                const DynRankView grad, coeff;
-                const int num_fields;
-                const int n_var;
-            };
-
-            class Rank2OpAndStore {
-            public:
-                UTOPIA_INLINE_FUNCTION Rank2OpAndStore(const DynRankView &grad,
-                                                       const DynRankView &coeff,
-                                                       DynRankView &field)
-                    : op_(grad, coeff), spatial_dim(grad.extent(3)), field(field) {}
-
-                UTOPIA_INLINE_FUNCTION void operator()(const int cell, const int qp, const int var, const int d) const {
-                    field(cell, qp, var * spatial_dim + d) = op_(cell, qp, var, d);
-                }
-
-                Rank2Op op_;
-                const int spatial_dim;
-                DynRankView field;
-            };
-
-            void init(const Field<Scalar> &coeff) {
-                assert(coeff.tensor_size() > 0);
-                assert(coeff.is_coefficient());
-
-                var_dim_ = coeff.tensor_size();
-                this->set_tensor_size(this->fe()->spatial_dimension() * var_dim_);
-
-                ensure_field();
-                init(coeff.data());
+            void set_tensor_size(const int rows, const int cols) {
+                rows_ = rows;
+                cols_ = cols;
+                Super::set_tensor_size(rows * cols);
             }
 
             void ensure_field() override {
@@ -113,35 +40,20 @@ namespace utopia {
                 const SizeType e1 = this->data().extent(1);
                 const SizeType e2 = this->data().extent(2);
 
-                if (rank() == 1) {
-                    if (this->data().rank() != 3 || e0 < this->fe()->num_cells() || e1 < this->fe()->num_qp() ||
-                        e2 < this->fe()->spatial_dimension()) {
-                        this->data() = DynRankView(this->name(),
-                                                   this->fe()->num_cells(),
-                                                   this->fe()->num_qp(),
-                                                   this->fe()->spatial_dimension());
-                    } else {
-                        fill(this->data(), 0.0);
-                    }
+                if (this->data().rank() != 3 || e0 < this->fe()->num_cells() || e1 < this->fe()->num_qp() ||
+                    e2 < this->tensor_size()) {
+                    this->data() =
+                        DynRankView(this->name(), this->fe()->num_cells(), this->fe()->num_qp(), this->tensor_size());
                 } else {
-                    assert(rank() == 2);
-                    if (this->data().rank() != 3 || e0 < this->fe()->num_cells() || e1 < this->fe()->num_qp() ||
-                        e2 < this->fe()->spatial_dimension() * var_dim_) {
-                        this->data() = DynRankView(this->name(),
-                                                   this->fe()->num_cells(),
-                                                   this->fe()->num_qp(),
-                                                   var_dim_ * this->fe()->spatial_dimension());
-                    } else {
-                        fill(this->data(), 0.0);
-                    }
+                    fill(this->data(), 0.0);
                 }
             }
 
-            inline int rank() const { return var_dim_ == 1 ? 1 : 2; }
+            inline int rank() const { return (rows_ == 1 || cols_ == 1) ? 1 : 2; }
             bool is_coefficient() const override { return false; }
 
-            inline int rows() const { return var_dim_; }
-            inline int cols() const { return this->fe()->spatial_dimension(); }
+            inline int rows() const { return rows_; }
+            inline int cols() const { return cols_; }
 
             class AddIdentity {
             public:
@@ -287,7 +199,7 @@ namespace utopia {
             bool check_dets_are_positive() const {
                 if (rows() != cols()) {
                     assert(false);
-                    Utopia::Abort("Trying to call det on non-square Gradient");
+                    Utopia::Abort("Trying to call det on non-square QPTensorField");
                 }
 
                 CountNonPositiveDets op(this->rows(), this->data());
@@ -301,14 +213,14 @@ namespace utopia {
             void det(DynRankView &result) const {
                 if (rows() != cols()) {
                     assert(false);
-                    Utopia::Abort("Trying to call det on non-square Gradient");
+                    Utopia::Abort("Trying to call det on non-square QPTensorField");
                 }
 
                 SizeType num_cells = this->fe()->num_cells();
                 SizeType num_qp = this->fe()->num_qp();
 
                 if (result.extent(0) != num_cells || result.extent(1) != num_qp || result.extent(2) != 1) {
-                    result = DynRankView("det(Gradient)", num_cells, num_qp, 1);
+                    result = DynRankView("det(QPTensorField)", num_cells, num_qp, 1);
                 }
 
                 const int n = this->rows();
@@ -321,53 +233,33 @@ namespace utopia {
                     UTOPIA_LAMBDA(int cell, int qp) { result(cell, qp, 0) = op(cell, qp); });
             }
 
-        private:
-            int var_dim_{1};
-
-            inline Kokkos::MDRangePolicy<Kokkos::Rank<3>, ExecutionSpace> rank1_range() {
+            inline Rank1Range rank1_range() {
                 int num_cells = this->fe()->num_cells();
                 int num_qp = this->fe()->num_qp();
-                int spatial_dimension = this->fe()->spatial_dimension();
-
                 return Kokkos::MDRangePolicy<Kokkos::Rank<3>, ExecutionSpace>({0, 0, 0},
-                                                                              {num_cells, num_qp, spatial_dimension});
+                                                                              {num_cells, num_qp, this->tensor_size()});
             }
 
-            // TODO Se if we can play with the layout for better coalescing
-            inline Kokkos::MDRangePolicy<Kokkos::Rank<4>, ExecutionSpace> rank2_range() {
+            inline Rank2Range rank2_range() {
                 int num_cells = this->fe()->num_cells();
                 int num_qp = this->fe()->num_qp();
-                int spatial_dimension = this->fe()->spatial_dimension();
-
-                return Kokkos::MDRangePolicy<Kokkos::Rank<4>, ExecutionSpace>(
-                    {0, 0, 0, 0}, {num_cells, num_qp, var_dim_, spatial_dimension});
+                return Kokkos::MDRangePolicy<Kokkos::Rank<4>, ExecutionSpace>({0, 0, 0, 0},
+                                                                              {num_cells, num_qp, rows(), cols()});
             }
 
-            void init(const DynRankView &coeff) {
-                ensure_field();
-
-                if (rank() == 1) {
-                    Kokkos::parallel_for(this->name() + "::init_rank1",
-                                         rank1_range(),
-                                         Rank1OpAndStore(this->fe()->grad, coeff, this->data()));
-                } else {
-                    assert(rank() == 2);
-
-                    Kokkos::parallel_for(this->name() + "::init_rank2",
-                                         rank2_range(),
-                                         Rank2OpAndStore(this->fe()->grad, coeff, this->data()));
-                }
-            }
+        private:
+            int rows_{1}, cols_{1};
         };
 
         template <typename Scalar>
-        inline QPField<Scalar> det(const Gradient<Scalar> &grad) {
+        inline QPField<Scalar> det(const QPTensorField<Scalar> &grad) {
             QPField<Scalar> ret(grad.fe());
             ret.set_name("Determinant");
             grad.det(ret.data());
             return ret;
         }
+
     }  // namespace intrepid2
 }  // namespace utopia
 
-#endif  // UTOPIA_INTREPID2_GRADIENT_HPP
+#endif  // UTOPIA_INTREPID2_TENSOR_FIELD_HPP
