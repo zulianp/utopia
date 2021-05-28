75d7fbfb323d38223c07ca9612fda6ca5a658333
zulianp <patrick.zulian@gmail.com>
2021-05-28 15:19:03 +0200

    started refactor

--- utopia/solvers/multilevel/utopia_MonotoneMultigrid.hpp
+++ utopia/solvers/multilevel/utopia_MonotoneMultigrid.hpp
@@ -15,6 +15,7 @@
 #include "utopia_Utils.hpp"
 
 #include "utopia_ActiveSet.hpp"
+#include "utopia_Multigrid.hpp"
 #include "utopia_ProjectedGaussSeidelNew.hpp"
 #include "utopia_ProjectedGaussSeidelQR.hpp"
 
@@ -29,117 +30,94 @@ namespace utopia {
      * @tparam     Vector
      */
     template <class Matrix, class Vector, int Backend = Traits<Vector>::Backend>
-    class MonotoneMultigrid : public LinearMultiLevel<Matrix, Vector>, public VariableBoundSolverInterface<Vector> {
-        typedef utopia::LinearSolver<Matrix, Vector> Solver;
-        typedef utopia::IterativeSolver<Matrix, Vector> Smoother;
-        using SmootherPtr = std::shared_ptr<Smoother>;
-
-        typedef utopia::IterativeSolver<Matrix, Vector> IterativeSolver;
-        typedef utopia::Level<Matrix, Vector> Level;
-        typedef utopia::Transfer<Matrix, Vector> Transfer;
-        using VariableBoundSolverInterface = utopia::VariableBoundSolverInterface<Vector>;
-
-        using Super = utopia::LinearMultiLevel<Matrix, Vector>;
-
+    class MonotoneMultigrid final : public QPSolver<Matrix, Vector> {
         using Layout = typename Traits<Vector>::Layout;
         using Scalar = typename Traits<Vector>::Scalar;
         using SizeType = typename Traits<Vector>::SizeType;
 
-        typedef struct {
-            std::vector<Vector> r, x, rhs;
+        using QPSmoother = utopia::QPSolver<Matrix, Vector>;
+        using Super = utopia::QPSolver<Matrix, Vector>;
 
-            void init(const int n_levels) {
-                r.resize(n_levels);
-                x.resize(n_levels);
-                rhs.resize(n_levels);
-            }
+        using Solver = utopia::LinearSolver<Matrix, Vector>;
+        using Smoother = utopia::IterativeSolver<Matrix, Vector>;
+        using SmootherPtr = std::shared_ptr<Smoother>;
+        using IterativeSolver = utopia::IterativeSolver<Matrix, Vector>;
+        using Level = utopia::Level<Matrix, Vector>;
+        using Transfer = utopia::Transfer<Matrix, Vector>;
+        using TruncatedTransfer = utopia::IPTruncatedTransfer<Matrix, Vector>;
 
-            inline bool valid(const std::size_t n_levels) const { return r.size() == n_levels; }
-
-        } LevelMemory;
-
-        LevelMemory memory;
+        using VariableBoundSolverInterface = utopia::VariableBoundSolverInterface<Vector>;
+        using DefaultSmoother = utopia::ProjectedGaussSeidel<Matrix, Vector>;
 
     public:
         static const int V_CYCLE = 1;
         static const int W_CYCLE = 2;
 
-        MonotoneMultigrid(const std::shared_ptr<Smoother> &fine_smoother,
+        MonotoneMultigrid(const std::shared_ptr<QPSmoother> &qp_smoother,
                           const std::shared_ptr<Smoother> &coarse_smoother,
-                          const std::shared_ptr<Solver> &coarse_solver,
-                          const SizeType &num_levels)
-            : coarse_solver_(coarse_solver),
-              active_set_(std::make_shared<ActiveSet<Vector>>()),
-              use_line_search_(false) {
-            this->must_generate_masks(false);
-            this->fix_semidefinite_operators(true);
-            init(fine_smoother, coarse_smoother, coarse_solver, num_levels);
+                          const std::shared_ptr<Solver> &coarse_solver)
+            : algo_(coarse_smoother, coarse_solver),
+              qp_smoother_(qp_smoother),
+              active_set_(std::make_shared<ActiveSet<Vector>>()) {
+            algo_.must_generate_masks(false);
+            algo_.fix_semidefinite_operators(true);
         }
 
-        MonotoneMultigrid(const SizeType &num_levels = 2)
-            : active_set_(std::make_shared<ActiveSet<Vector>>()), use_line_search_(false) {
-            this->must_generate_masks(false);
-            this->fix_semidefinite_operators(true);
-            init(std::make_shared<ProjectedGaussSeidel<Matrix, Vector>>(),
-                 std::make_shared<ProjectedGaussSeidel<Matrix, Vector>>(),
-                 std::make_shared<ProjectedGaussSeidel<Matrix, Vector>>(),
-                 num_levels);
-        }
-
-        void init(const std::shared_ptr<Smoother> &fine_smoother,
-                  const std::shared_ptr<Smoother> &coarse_smoother,
-                  const std::shared_ptr<Solver> &coarse_solver,
-                  const SizeType &num_levels) {
-            coarse_solver_ = coarse_solver;
-            this->num_levels_ = num_levels;
-
-            smoothers_.resize(this->n_levels());
-            smoothers_[this->num_levels_ - 1] = fine_smoother;
-
-            for (SizeType l = 1; l < this->num_levels_ - 1; ++l) {
-                smoothers_[l] = std::shared_ptr<Smoother>(coarse_smoother->clone());
-            }
-        }
-
-        inline static std::shared_ptr<IPTruncatedTransfer<Matrix, Vector>> new_fine_level_transfer(
-            const std::shared_ptr<Matrix> &interpolation) {
-            return std::make_shared<IPTruncatedTransfer<Matrix, Vector>>(interpolation);
-        }
-
-        inline static std::shared_ptr<IPTransfer<Matrix, Vector>> new_coarse_level_transfer(
-            const std::shared_ptr<Matrix> &interpolation) {
-            return std::make_shared<IPTransfer<Matrix, Vector>>(interpolation);
+        MonotoneMultigrid()
+            : algo_(std::make_shared<DefaultSmoother>(), std::make_shared<DefaultSmoother>()),
+              qp_smoother_(std::make_shared<DefaultSmoother>()),
+              active_set_(std::make_shared<ActiveSet<Vector>>()) {
+            algo_.must_generate_masks(false);
+            algo_.fix_semidefinite_operators(true);
         }
 
         ~MonotoneMultigrid() override = default;
 
         void read(Input &in) override {
             Super::read(in);
-
-            in.get("use_line_search", use_line_search_);
-            if (coarse_solver_) {
-                in.get("coarse_solver", *coarse_solver_);
-            }
+            algo_.read(in);
 
             if (active_set_) {
                 in.get("active_set", *active_set_);
             }
+
+            if (qp_smoother_) {
+                in.get("qp_smoother", *qp_smoother_);
+            }
         }
 
-        void print_usage(std::ostream &os) const override {
-            Super::print_usage(os);
+        void set_transfer_operators(const std::vector<std::shared_ptr<Transfer>> &transfer_operators) {
+            auto last = transfer_operators.back();
 
-            this->print_param_usage(
-                os, "perform_galerkin_assembly", "bool", "Flag turning on/off galerkin assembly.", "true");
-            this->print_param_usage(os,
-                                    "use_line_search",
-                                    "bool",
-                                    "Flag turning on/off line-search after coarse grid correction.",
-                                    "false");
-            this->print_param_usage(os, "block_size", "int", "Block size for systems.", "1");
-            // this->print_param_usage(os, "smoother", "Smoother", "Input parameters for
-            // all smoothers.", "-");
-            this->print_param_usage(os, "coarse_solver", "LinearSolver", "Input parameters for coarse solver.", "-");
+            auto tt = std::dynamic_pointer_cast<TruncatedTransfer>(last);
+
+            if (!tt) {
+                Utopia::Abort(
+                    "MonotoneMultigrid::set_transfer_operators(...) last operators must be of type TruncatedTransfer");
+            }
+
+            truncated_transfer_ = tt;
+            transfer_operators.resize(transfer_operators.size() - 1);
+
+            algo_.set_transfer_operators(transfer_operators);
+        }
+
+        void set_transfer_operators(const std::vector<std::shared_ptr<Transfer>> &transfer_operators,
+                                    const std::shared_ptr<TruncatedTransfer> &last_level) {
+            algo_.set_transfer_operators(transfer_operators);
+            truncated_transfer_ = last_level;
+        }
+
+        void init(const std::shared_ptr<QPSmoother> &qp_smoother) { qp_smoother_ = fine_smoother; }
+
+        inline static std::shared_ptr<TruncatedTransfer> new_fine_level_transfer(
+            const std::shared_ptr<Matrix> &interpolation) {
+            return std::make_shared<IPTruncatedTransfer<Matrix, Vector>>(interpolation);
+        }
+
+        inline static std::shared_ptr<TruncatedTransfer> new_coarse_level_transfer(
+            const std::shared_ptr<Matrix> &interpolation) {
+            return std::make_shared<IPTransfer<Matrix, Vector>>(interpolation);
         }
 
         /*! @brief if overriden the subclass has to also call this one first
@@ -148,203 +126,27 @@ namespace utopia {
             Super::update(op);
             init_memory(row_layout(*op));
 
-            if (perform_galerkin_assembly_) {
-                this->galerkin_assembly(op);
+            if (algo_.must_perform_galerkin_assembly()) {
+                std::shared_ptr<Matrix> op_H = std::make_shared<Matrix>();
+                truncated_transfer_->restrict(*op, *op_H);
+
+                if (algo_.fix_semidefinite_operators()) {
+                    algo_.fix_semidefinite_operator(*op_H);
+                }
+
+                algo_.update(op_H);
             }
-
-            update();
-        }
-
-        // rememebr to put level -1, as c++ indexing goes from 0
-        void set_smoother(const std::shared_ptr<Smoother> &smoother, const SizeType &level) {
-            if (level > this->n_levels() || level < 1) {
-                utopia_error("MonotoneMultigrid:: set_smoother, invalid level.");
-            }
-
-            if (smoothers_.size() - 1 < level) {
-                utopia_error("MonotoneMultigrid:: set_smoother, smoothers array was not allocated.");
-            }
-
-            smoothers_[level] = smoother;
         }
 
         void init_memory(const Layout &l) override {
             Super::init_memory(l);
-            VariableBoundSolverInterface::init_memory(l);
             active_set_->init(l);
         }
 
-        /**
-         * @brief      The solve function for multigrid method.
-         *
-         * @param[in]  rhs   The right hand side.
-         * @param      x   The initial guess.
-         *
-         */
-        bool apply(const Vector &rhs, Vector &x_fine) override {
-            Scalar r_norm, diff_norm;
-            SizeType it = 0;
-            bool converged = false;
-            bool ok = true;
-            UTOPIA_UNUSED(ok);
-            SizeType L = this->n_levels();
-
-            memory.init(L);
-            SizeType l = L - 1;
-
-            memory.x[l] = x_fine;
-            memory.rhs[l] = rhs;
-
-            r_norm = norm2(memory.rhs[l] - level(l).A() * memory.x[l]);
-            // r0_norm = r_norm;
-
-            Vector x_old = memory.x[l];
-
-            std::string mg_header_message = "MonotoneMultigrid: " + std::to_string(L) + " levels";
-            this->init_solver(mg_header_message, {" it. ", "|| r_N ||", "||x_old - x_new||"});
-
-            if (this->verbose()) PrintInfo::print_iter_status(it, {r_norm, 1});
-            it++;
-
-            if (this->cycle_type() == FULL_CYCLE) {
-                this->max_it(1);
-            }
-
-            while (!converged) {
-                if (this->cycle_type() == MULTIPLICATIVE_CYCLE) {
-                    ok = standard_cycle(l);
-                    assert(ok);
-                } else {
-                    utopia::err() << "[Error] MonotoneMultigrid: unknown MG type... \n";
-                }
-
-#ifdef CHECK_NUM_PRECISION_mode
-                if (has_nan_or_inf(x_fine)) {
-                    x_fine.set(0.0);
-                    return true;
-                }
-#else
-                // assert(!has_nan_or_inf(x));
-#endif
-                diff_norm = norm2(x_old - memory.x[l]);
-                x_old = memory.x[l];
-
-                r_norm = norm2(memory.rhs[l] - level(l).A() * memory.x[l]);
-
-                // print iteration status on every iteration
-                if (this->verbose()) PrintInfo::print_iter_status(it, {r_norm, diff_norm});
-
-                // check convergence and print interation info
-                converged = this->check_convergence(it, r_norm, diff_norm, 1);
-                it++;
-            }
-
-            x_fine = memory.x[l];
-            this->print_statistics(it);
-
-            return true;
-        }
-
-        inline Level &level(const SizeType &l) { return this->levels_[l]; }
-
-        void update() {
-            smoothers_.resize(this->n_levels());
-            // smoothers_[0] = nullptr;
-
-            for (std::size_t l = 1; l != smoothers_.size(); ++l) {
-                if (smoothers_[l] == nullptr) {
-                    assert(smoothers_[l]);
-                }
-                smoothers_[l]->update(level(l).A_ptr());
-            }
-            coarse_solver_->update(level(0).A_ptr());
-        }
-
-        void set_perform_galerkin_assembly(const bool val) { perform_galerkin_assembly_ = val; }
-
-        /*=======================================================================================================================================
-         =
-         =========================================================================================================================================*/
-    private:
-        bool standard_cycle(const SizeType &l) {
-            assert(memory.valid(this->n_levels()) && l < this->n_levels());
-
-            ////////////////////////////////////
-            if (l == 0) {
-                coarse_solve(memory.rhs[l], memory.x[l]);
-                return true;
-            }
-            ////////////////////////////////////
-
-            // recursive call into mg
-            for (SizeType k = 0; k < this->mg_type(); k++) {
-                // presmoothing
-
-                if (l == this->n_levels() - 1) {
-                    // do fine level smoothing
-                    smoothing_fine(l, memory.rhs[l], memory.x[l], this->pre_smoothing_steps(), true);
-                } else if (l > 0) {
-                    smoothing(l, memory.rhs[l], memory.x[l], this->pre_smoothing_steps());
-                }
-
-                memory.r[l] = memory.rhs[l] - level(l).A() * memory.x[l];
-                // residual transfer
-                this->transfer(l - 1).restrict(memory.r[l], memory.rhs[l - 1]);
-
-                // BC conditions treatment ...
-                if (this->must_generate_masks()) {
-                    this->apply_mask(l - 1, memory.rhs[l - 1]);
-                }
-
-                if (empty(memory.x[l - 1])) {
-                    memory.x[l - 1] = 0.0 * memory.rhs[l - 1];
-                } else {
-                    memory.x[l - 1].set(0.0);
-                }
-
-                standard_cycle(l - 1);
-
-                // correction transfer
-                this->transfer(l - 1).interpolate(memory.x[l - 1], memory.r[l]);
-
-                memory.x[l] += memory.r[l];
-
-                // postsmoothing
-                if (l == this->n_levels() - 1) {
-                    smoothing_fine(l, memory.rhs[l], memory.x[l], this->post_smoothing_steps(), false);
-                } else if (l > 0) {
-                    smoothing(l, memory.rhs[l], memory.x[l], this->post_smoothing_steps());
-                }
-            }
-
-            return true;
-        }
-
-        /**
-         * @brief      The function invokes smoothing.
-         *
-         * @param[in]  level The level we are at.
-         * @param[in]  rhs   The right hand side.
-         * @param      x     The current iterate.
-         * @param[in]  nu    The number of smoothing steps.
-         *
-         * @return
-         */
-        inline bool smoothing(const SizeType l, const Vector &rhs, Vector &x, const SizeType &nu = 1) {
-            smoothers_[l]->sweeps(nu);
-            smoothers_[l]->smooth(rhs, x);
-            return true;
-        }
-
-        inline bool smoothing_fine(const SizeType l,
-                                   const Vector &rhs,
-                                   Vector &x,
-                                   const SizeType &nu = 1,
-                                   const bool &pre_sm = false) {
+        inline bool step(const Vector &rhs, Vector &x, const SizeType &smoothing_steps = 1) {
             assert(l != this->n_levels() - 2);
 
-            auto fine_smoother = smoothers_[l].get();
-            fine_smoother->sweeps(nu);
+            qp_smoother_->sweeps(smoothing_steps);
 
             if (auto *fine_smoother_vi = dynamic_cast<VariableBoundSolverInterface *>(fine_smoother)) {
                 fine_smoother_vi->set_box_constraints(this->get_box_constraints());
@@ -399,26 +201,12 @@ namespace utopia {
             }
             return true;
         }
-        /**
-         * @brief      The functions invokes coarse solve.
-         *
-         * @param[in]  rhs  The right hand side.
-         * @param      x    The current iterate.
-         *
-         * @return
-         */
-        bool coarse_solve(const Vector &rhs, Vector &x) {
-            if (!coarse_solver_->apply(rhs, x)) return false;
-            // assert(approxeq(level(0).A() * x, rhs, 1e-6));
-            return true;
-        }
 
     public:
         MonotoneMultigrid *clone() const override {
-            return new MonotoneMultigrid(std::shared_ptr<Smoother>(smoothers_[smoothers_.size() - 1]->clone()),
-                                         std::shared_ptr<Smoother>(smoothers_[1]->clone()),
-                                         std::shared_ptr<Solver>(coarse_solver_->clone()),
-                                         this->n_levels());
+            auto ret = utopia::make_unique<MonotoneMultigrid>();
+            ret->algo_ = algo_;
+            return ret.release();
         }
 
         /**
@@ -429,13 +217,8 @@ namespace utopia {
          * @return
          */
         bool change_coarse_solver(const std::shared_ptr<Solver> &coarse_solver) {
-            coarse_solver_ = coarse_solver;
-            return true;
+            return algo_.change_coarse_solver(coarse_solver);
         }
-
-        void use_line_search(const bool val) { use_line_search_ = val; }
-
-        bool use_line_search() const { return use_line_search_; }
 
         inline ActiveSet<Vector> &active_set() { return *active_set_; }
 
@@ -444,13 +227,10 @@ namespace utopia {
         }
 
     protected:
-        std::vector<SmootherPtr> smoothers_;
-        std::shared_ptr<Solver> coarse_solver_;
+        Multigrid<Matrix, Vector> algo_;
+        std::shared_ptr<QPSmoother> qp_smoother_;
+        std::shared_ptr<TruncatedTransfer> truncated_transfer_;
         std::shared_ptr<ActiveSet<Vector>> active_set_;
-
-    private:
-        bool use_line_search_{false};
-        bool perform_galerkin_assembly_{true};
     };
 
 }  // namespace utopia
