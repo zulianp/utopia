835bb21c99466375f710c7f58d634ad150fa66d8
Filippo Cesana <filippo.cesana@gmail.com>
2021-08-17 15:43:53 +0100

    added methods for creating and manipulating vectors from my bachelor thesis

--- utopia/scripting/utopia_script.cpp
+++ utopia/scripting/utopia_script.cpp
@@ -1,14 +1,20 @@
 #include "utopia_script.hpp"
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <vector>
+#include "../utopia.hpp"
 #include "utopia.hpp"
+#include "utopia_AbstractVector.hpp"
 #include "utopia_Instance.hpp"
 #include "utopia_ObjectFactory.hpp"
 #include "utopia_Version.hpp"
+#include "utopia_script.hpp"
 
 #include <iostream>
 
 void print_array(double *seq, int n) {
-    printf("array with length %d:\n", n);
-
+    printf("array with length %d :\n", n);
     for (int i = 0; i < n; ++i) {
         printf("%g\n", seq[i]);
     }
@@ -65,6 +71,37 @@ namespace scripting {
 
     void SparseMatrix::print_info() { utopia::out() << "SparseMatrix::print()" << std::endl; }
 
+    Communicator::Communicator() : impl_(nullptr) {
+        auto comm = Factory::new_communicator();
+
+        if (!comm) {
+            utopia::out() << "[Error] Communicator could not be constructed" << std::endl;
+            return;
+        }
+        impl_ = comm.get();
+        comm.release();
+    }
+
+    Communicator::~Communicator() { delete impl_; }
+
+    Layout::Layout(const Communicator &comm, LocalSizeType local_size, SizeType global_size)
+        :
+
+          impl_(nullptr) {
+        auto layout = std::make_unique<LayoutImpl>(*comm.get_communicator(), local_size, global_size);
+
+        if (!layout) {
+            utopia::out() << "[Error] Vector could not be constructed" << std::endl;
+            return;
+        }
+
+        impl_ = layout.get();
+        layout.release();
+    }
+
+    Layout::~Layout() { delete impl_; }
+
+    // input inside here, vcector of double
     Vector::Vector() : impl_(nullptr) {
         auto vec = Factory::new_vector();
 
@@ -80,5 +117,108 @@ namespace scripting {
     Vector::~Vector() { delete impl_; }
 
     void Vector::print_info() { utopia::out() << "Vector::print()" << std::endl; }
+    void Vector::values(const Layout &l, const Scalar &value) {
+        auto ll = *l.get_layout();
+        impl_->values(ll, value);
+    }
+    void Vector::add(const SizeType &i, const Scalar &value) { impl_->add(i, value); }
+    void Vector::axpy(Scalar alpha, Vector *x) { impl_->axpy(alpha, *x->impl_); }
+    void Vector::describe() const { impl_->describe(); }
+    bool Vector::equals(const Vector *other, const Scalar tol) const { return impl_->equals(*other->impl_, tol); }
+    Scalar Vector::dot(const Vector *x) const { return impl_->dot(*x->impl_); }
+    void Vector::set(const SizeType &i, const Scalar &value) { impl_->set(i, value); }
+    Scalar Vector::get(const SizeType &i) const { return impl_->get(i); }
+    void Vector::scale(const Scalar &a) { impl_->scale(a); }
+
+    void Vector::print_array(double *seq, int n) {
+        printf("array with length %d :\n", n);
+        for (int i = 0; i < n; ++i) {
+            printf("%g\n", seq[i]);
+        }
+    }
+
+    SizeType Vector::local_size() { return impl_->local_size(); }
+
+    void Vector::serial_uconversion(double *seq, int n) {
+        {
+            if (impl_->empty()) {
+                auto l = utopia::serial_layout(n);
+                impl_->values(l, 0);
+            }
+
+            impl_->write_lock(utopia::LOCAL);
+            for (auto i = 0; i < n; ++i) {
+                impl_->set(i, *seq);
+                ++seq;
+            }
+
+            impl_->write_unlock(utopia::LOCAL);
+        }
+    }
+
+    // double *Vector::from_utopia_to_carray() {
+    //     int size = impl_->size();
+    //     double *temp = new double[size];
+    //     {
+    //         impl_->write_lock(utopia::LOCAL);
+    //         utopia::Range rr = impl_->range();
+    //         for (auto i = rr.begin(); i < rr.end(); ++i) {
+    //             temp[i] = impl_->get(i);
+    //         }
+
+    //         impl_->write_unlock(utopia::LOCAL);
+    //     }
+    //     for (int j = 0; j < size; j++) {
+    //         printf("%g\n", temp[j]);
+    //     }
+    //     return temp;
+    // }
+
+    void Vector::parallel_uconversion(float *values, const Layout &l) {
+        if (impl_->empty()) {
+            impl_->values(*l.get_layout(), 0.0);
+        }
+
+        {
+            impl_->write_lock(utopia::LOCAL);
+            utopia::Range rr = impl_->range();
+            for (auto i = rr.begin(); i < rr.end(); ++i) {
+                impl_->set(i, *values);
+                ++values;
+            }
+
+            impl_->write_unlock(utopia::LOCAL);
+        }
+    }
+
+    void Vector::write_into_carray(double *double_array) {
+        {
+            impl_->write_lock(utopia::LOCAL);
+            utopia::Range rr = impl_->range();
+            for (auto i = rr.begin(); i < rr.end(); ++i) {
+                double_array[i] = impl_->get(i);
+            }
+
+            impl_->write_unlock(utopia::LOCAL);
+        }
+    }
+
+    // void Vector::parallel_uconversion(std::vector<double> values, const Layout &l) {
+    //     if (impl_->empty()) {
+    //         impl_->values(*l.get_layout(), 0.0);
+    //     }
+
+    //     {
+    //         impl_->write_lock(utopia::LOCAL);
+    //         auto it = values.begin();
+    //         utopia::Range rr = impl_->range();
+    //         for (auto i = rr.begin(); i < rr.end(); ++i) {
+    //             impl_->set(i, *it);
+    //             ++it;
+    //         }
+
+    //         impl_->write_unlock(utopia::LOCAL);
+    //     }
+    // }
 
 }  // namespace scripting
--- utopia/scripting/utopia_script.hpp
+++ utopia/scripting/utopia_script.hpp
@@ -1,12 +1,16 @@
+// #include <stdlib.h>
+// #include <vector>
+
 #ifndef UTOPIA_SCRIPT_HPP
 #define UTOPIA_SCRIPT_HPP
 
-// #include "utopia_AbstractVector.hpp"
+//#include "utopia_AbstractVector.hpp"
 
-void print_array(double* seq, int n);
+void print_array(double *seq, int n);
 
 namespace utopia {
     // Forward declarations
+
     template <typename Scalar, typename SizeType>
     class AbstractVector;
 
@@ -15,17 +19,86 @@ namespace utopia {
 
     template <typename Scalar, typename SizeType>
     class AlgebraFactory;
+
+    template <class Comm, int Order, typename LocalSizeType_, typename SizeType_>
+    class Layout;
+
+    class Communicator;
+
+    template <typename Derived, int Order>
+    class Tensor;
+
+    template <class T, int Order>
+    class DeviceView;
+
 }  // namespace utopia
 
 namespace scripting {
-
     using Scalar = double;
     using SizeType = int;
+    using LocalSizeType = int;
     using Factory = utopia::AlgebraFactory<Scalar, SizeType>;
+    using Derived = utopia::AbstractVector<Scalar, SizeType>;
+    //  using T = utopia::AbstractVector<Scalar, SizeType>;
 
     void init();
     void finalize();
     void print_info();
+
+    class Communicator {
+    public:
+        using CommunicatorImpl = utopia::Communicator;
+
+        Communicator();
+        ~Communicator();
+
+        CommunicatorImpl *get_communicator() const { return impl_; }
+
+    private:
+        CommunicatorImpl *impl_;
+    };
+
+    class Layout {
+    public:
+        using LayoutImpl = const utopia::Layout<utopia::Communicator, 1, LocalSizeType, SizeType>;
+
+        Layout(const Communicator &comm, LocalSizeType local_size, SizeType global_size);
+        ~Layout();
+
+        LayoutImpl *get_layout() const { return impl_; }
+
+    private:
+        const LayoutImpl *impl_;
+    };
+
+    class Vector {
+    public:
+        using VectorImpl = utopia::AbstractVector<Scalar, SizeType>;
+
+        Vector();
+        ~Vector();
+        void print_info();
+        SizeType local_size();
+        void values(const Layout &l, const Scalar &value);
+        void add(const SizeType &i, const Scalar &value);
+        void axpy(Scalar alpha, Vector *x);
+        Scalar dot(const Vector *x) const;
+        void describe() const;
+        bool equals(const Vector *other, const Scalar tol) const;
+        void set(const SizeType &i, const Scalar &value);
+        void print_array(double *seq, int n);
+        // void convert_into_uvector(std::vector<double> values, const Layout &l);
+        void serial_uconversion(double *seq, int n);
+        void parallel_uconversion(float *values, const Layout &l);
+        void scale(const Scalar &a);
+        // void double *from_utopia_to_carray();
+        void write_into_carray(double *double_array);
+        Scalar get(const SizeType &i) const;
+        // void convert(int *seq, int n);
+
+    private:
+        VectorImpl *impl_;
+    };
 
     class SparseMatrix {
     public:
@@ -36,19 +109,7 @@ namespace scripting {
         void print_info();
 
     private:
-        MatrixImpl* impl_;
-    };
-
-    class Vector {
-    public:
-        using VectorImpl = utopia::AbstractVector<Scalar, SizeType>;
-
-        Vector();
-        ~Vector();
-        void print_info();
-
-    private:
-        VectorImpl* impl_;
+        MatrixImpl *impl_;
     };
 
 }  // namespace scripting
