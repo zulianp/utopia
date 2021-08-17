a0a59f147f5ff675cf2b68b8e6c0754cf87936bb
Alena Kopanicakova <alenakopanicakova@Alenas-MBP.usilu.net>
2021-08-17 19:16:18 +0200

    small fix in iteration process of CG

--- utopia/solvers/smoothers/utopia_ConjugateGradient_impl.hpp
+++ utopia/solvers/smoothers/utopia_ConjugateGradient_impl.hpp
@@ -111,6 +111,13 @@ namespace utopia {
         // }
 
         this->init_solver("Utopia Conjugate Gradient", {"it. ", "||r||"});
+        r_norm = norm2(r);
+        if (this->verbose()) {
+            PrintInfo::print_iter_status(it, {r_norm});
+        }
+
+        it++;
+
         bool converged = false;
 
         SizeType check_norm_each = 1;
