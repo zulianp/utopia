68e6fcb9a89023f5283eb46053abf20aebabd2ca
zulianp <patrick.zulian@gmail.com>
2021-09-25 06:52:25 +0200

    ...

--- utopia_fe/frontend/solve/utopia_NLSolve.hpp
+++ utopia_fe/frontend/solve/utopia_NLSolve.hpp
@@ -81,10 +81,18 @@ namespace utopia {
 
             this->status("Solving linear problem (matrix free)");
             // Solve linear problem
+
+            //////////////////////////////////////
+
             // ConjugateGradient<Matrix_t, Vector_t, HOMEMADE> cg;
             // cg.apply_gradient_descent_step(true);
 
+            //////////////////////////////////////
+
             BiCGStab<Matrix_t, Vector_t, HOMEMADE> cg;
+
+            //////////////////////////////////////
+
             cg.verbose(verbose_);
             cg.solve(*function_, rhs, x);
             assert(ok);
