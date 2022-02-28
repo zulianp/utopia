320b0d17b9ebdeb9d55cb9a47502511216c366c6
Patrick Zulian <patrick.zulian@gmail.com>
2022-02-28 16:47:03 +0100

    Added sketch for SaddlePoint app

---
+++ utopia_fe/apps/stk/utopia_stk_SaddlePointSolveApp.cpp
@@ -0,0 +1,36 @@
+#include "utopia_Main.hpp"
+
+#include "utopia.hpp"
+
+#include "utopia_SimpleNewton.hpp"
+
+#include "utopia_ImplicitEulerIntegrator.hpp"
+#include "utopia_NewmarkIntegrator.hpp"
+#include "utopia_SaddlePointSolve.hpp"
+#include "utopia_SemiGeometricMultigridNew.hpp"
+
+#include "utopia_moonolith_stk_FETransfer.hpp"
+#include "utopia_stk.hpp"
+
+#include "utopia_stk_intrepid2.hpp"
+#include "utopia_stk_intrepid2_OmniAssembler.hpp"
+
+#include "utopia_moonolith_stk_Obstacle.hpp"
+
+namespace utopia {
+    template class NewmarkIntegrator<utopia::stk::FunctionSpace>;
+    template class ImplicitEulerIntegrator<utopia::stk::FunctionSpace>;
+}  // namespace utopia
+
+void stk_saddle_point_solve(utopia::Input &in) {
+    utopia::SaddlePointSolve<utopia::stk::FunctionSpace> app;
+    app.read(in);
+
+    if (app.valid()) {
+        app.run();
+    } else {
+        utopia::err() << "stk_saddle_point_solve: invalid app setup\n";
+    }
+}
+
+UTOPIA_REGISTER_APP(stk_saddle_point_solve);
