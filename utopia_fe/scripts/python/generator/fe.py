f6ad082cc7306a5958496388a459ded1b2522dae
Patrick Zulian <patrick.zulian@gmail.com>
2022-03-11 15:24:09 +0100

    specialization for simplex

--- utopia_fe/scripts/python/generator/fe.py
+++ utopia_fe/scripts/python/generator/fe.py
@@ -1,4 +1,5 @@
 from sympy import *
+from time import perf_counter
 
 # from sympy import Matrix
 # from sympy import cse
@@ -35,13 +36,18 @@ def point2(x, y):
 def point3(x, y, z):
     return Matrix(3, 1, [x, y, z])
 
+def point4(x, y, z, t):
+    return Matrix(4, 1, [x, y, z, t])
+
 class FE:
-    def __init__(self, name, dim, symplify_expr = True):
+    def __init__(self, name, dim, symplify_expr = False):
         self.name = name
         self.dim = dim
         self.symplify_expr = symplify_expr
 
     def transform(self, x_ref):
+        console.print("FE.transform")
+
         f = self.fun(x_ref)
         n = self.nodes
 
@@ -53,6 +59,8 @@ class FE:
         return x
 
     def jacobian(self, x_ref):
+        console.print("FE.jacobian")
+
         x = self.transform(x_ref)
         rows = self.dim
         cols = x_ref.shape[0]
@@ -69,6 +77,8 @@ class FE:
             return G
 
     def grad(self, x_ref):
+        console.print("FE.grad")
+
         f = self.fun(x_ref)
         G_inv = self.jacobian_inverse(x_ref)
 
@@ -97,6 +107,8 @@ class FE:
         return grads
 
     def jacobian_inverse(self, x_ref):
+        console.print("FE.jacobian_inverse")
+        
         G = self.jacobian(x_ref)
         G_inv = Inverse(G)
        
@@ -106,15 +118,9 @@ class FE:
             return G_inv
 
     def generate_code(self, x):
-        G = self.jacobian(x)
-        G_inv = self.jacobian_inverse(x)
-        det_G = det(G)
-
-        printer = C99CodePrinter()
-        lines = []
-
-        rows = G.shape[0]
-        cols = G.shape[1]
+        console.print("--------------------------")
+        console.print(f'{self.name}: creating expressions')
+        start = perf_counter()
 
         f = self.fun(x)
         n_fun = f.shape[0]
@@ -122,11 +128,27 @@ class FE:
         grads = self.grad(x)
         expr = []
 
+        stop = perf_counter()
+        console.print(f'Elapsed {stop - start} seconds')
+        console.print("--------------------------")
+        console.print(f'{self.name}: assignements')
+
+        start = perf_counter()
+
         for i in range(0, n_fun):
             for d in range(0, self.dim):
                 expr.append(Assignment(symbols(f"g{d}[{i}]"), grads[i][d]))
 
+        stop = perf_counter()
+        console.print(f'Elapsed {stop - start} seconds')
+        start = perf_counter()
+        console.print("--------------------------")
+        console.print(f'{self.name}: running cse')
+
         sub_expr, simpl_expr = cse(expr)
+        
+        printer = C99CodePrinter()
+        lines = []
 
         for var,expr in sub_expr:
             lines.append(f'T {var} = {printer.doprint(expr)};')
@@ -136,14 +158,37 @@ class FE:
 
         code_string='\n'.join(lines)
 
+        stop = perf_counter()
+        console.print(f'Elapsed  {stop - start} seconds')
         console.print("--------------------------")
-        console.print(self.name)
+        console.print(f'{self.name}: generated code')
+
         console.print(code_string)
         return code_string
 
-class Tri3(FE):
-    def __init__(self):
-        super().__init__('Tri3', 2)
+class Simplex(FE):
+    def __init__(self, name, dim, symplify_expr = False):
+        super().__init__(name, dim, symplify_expr)
+
+    def jacobian(self, x_ref):        
+        console.print("Simplex.jacobian")
+
+        n = self.nodes
+        n_fun = len(n)
+        G = zeros(n_fun-1, self.dim) 
+
+        for i in range(1, n_fun):
+            for j in range(0, self.dim):
+                G[i-1, j] = n[i][j] - n[0][j]
+
+        if self.symplify_expr:
+            return simplify(G)
+        else:
+            return G
+
+class Tri3(Simplex):
+    def __init__(self, symplify_expr = False):
+        super().__init__('Tri3', 2, symplify_expr)
         self.nodes = point_symbols(3, 2)
 
     def n_shape_functions(self):
@@ -153,7 +198,7 @@ class Tri3(FE):
         ret = Matrix(3, 1, [ 1 - x[0] - x[1], x[0], x[1] ])
         return ret
 
-class Tet4(FE):
+class Tet4(Simplex):
     def __init__(self, symplify_expr = False):
         super().__init__('Tet4', 3, symplify_expr)
         self.nodes = point_symbols(4, 3)
@@ -165,9 +210,21 @@ class Tet4(FE):
         ret = Matrix(4, 1, [ 1 - x[0] - x[1] - x[2], x[0], x[1], x[2] ])
         return ret
 
+class Pentatope5(Simplex):
+    def __init__(self, symplify_expr = False):
+        super().__init__('Pentatope5', 4, symplify_expr)
+        self.nodes = point_symbols(5, 4)
+
+    def n_shape_functions(self):
+        return len(self.nodes)
+
+    def fun(self, x):
+        ret = Matrix(4, 1, [ 1 - x[0] - x[1] - x[2] - x[3], x[0], x[1], x[2], x[3]])
+        return ret
+
 class Quad4(FE):
-    def __init__(self):
-        super().__init__('Quad4', 2)
+    def __init__(self, symplify_expr = False):
+        super().__init__('Quad4', 2, symplify_expr)
         self.nodes = point_symbols(4, 2)
 
     def n_shape_functions(self):
@@ -199,19 +256,22 @@ class Hex8(FE):
         return ret
 
 def main(args):
-    x, y, z = symbols('x y z')
+    x, y, z, t = symbols('x y z t')
     p2 = point2(x, y);
     p3 = point3(x, y, z);
+    p4 = point4(x, y, z, t);
 
     tri3 = Tri3()
-    tet4 = Tet4()
     quad4 = Quad4()
+    tet4 = Tet4()
     hex8 = Hex8()
+    pentatope5 = Pentatope5()
 
-    # tri3.generate_code(p2)
-    # tet4.generate_code(p3)
+    tri3.generate_code(p2)
     quad4.generate_code(p2)
-    # hex8.generate_code(p3)
+    tet4.generate_code(p3)
+    hex8.generate_code(p3)
+    pentatope5.generate_code(p4)
 
 if __name__ == '__main__':
     main(sys.argv[1:])
