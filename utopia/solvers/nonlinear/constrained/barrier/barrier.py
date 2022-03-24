48b7134f46bc0e9fe4f41c314bc30e2047adca69
Patrick Zulian <patrick.zulian@gmail.com>
2022-03-24 09:45:49 +0100

    Added initialization of measure in generated fe, for avoiding compilation failures on certain settings

---
+++ utopia/solvers/nonlinear/constrained/barrier/barrier.py
@@ -0,0 +1,34 @@
+import sympy as sp
+from sympy.printing.c import C99CodePrinter
+import rich 
+
+
+console = rich.get_console()
+
+d, d_hat = sp.symbols('d d_hat')
+
+param = (d - d_hat) / d_hat;
+
+# Quadratic barrier
+f = 0.5 * (param * param)
+
+# Cubic barrier
+# f = sp.Rational(1,3) * (param * param * param)
+
+grad_f = sp.simplify(sp.diff(f, d))
+H_f = sp.simplify(sp.diff(grad_f, d))
+
+
+# console.print(f)
+# console.print(grad_f)
+# console.print(H_f)
+
+printer = sp.printing.c.C99CodePrinter()
+
+
+console.print('f')
+console.print(printer.doprint(f))
+console.print('grad_f')
+console.print(printer.doprint(grad_f))
+console.print('H_f')
+console.print(printer.doprint(H_f))
