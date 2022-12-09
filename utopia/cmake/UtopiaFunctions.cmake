dfe5e8bef17b45f5f6bf3f75782eba999f889402
Dylan Ramelli <rameld@usi.ch>
2022-12-09 08:37:15 +0100

    moved functions, options in respective files

---
+++ utopia/cmake/UtopiaFunctions.cmake
@@ -0,0 +1,145 @@
+# ##############################################################################
+# ##############################################################################
+# ##############################################################################
+# Utitlity functions
+
+function(create_absolute_paths root paths absolutePaths)
+    foreach(path ${${paths}})
+        set(temp; ${temp} ${${root}}/${path})
+    endforeach()
+    set(${absolutePaths}
+        ${temp}
+        PARENT_SCOPE)
+endfunction()
+
+# ##############################################################################
+macro(print_dependency_table)
+    set(DEP_TABLE
+        "\n____________________________________________________\n\n   BACKENDS and STATUS TABLE\n"
+    )
+    set(DEP_TABLE
+        "${DEP_TABLE}--------------------------------------------------\n")
+    set(DEP_TABLE
+        "${DEP_TABLE}backend\t\t| active\t| developement status\n--------------------------------------------------\n"
+    )
+    set(DEP_TABLE "${DEP_TABLE}petsc\t\t| ${UTOPIA_PETSC}\t\t| usable\n")
+    set(DEP_TABLE "${DEP_TABLE}slepc\t\t| ${UTOPIA_SLEPC}\t\t| usable\n")
+    set(DEP_TABLE "${DEP_TABLE}blas\t\t| ${UTOPIA_BLAS}\t\t| usable\n")
+    set(DEP_TABLE
+        "${DEP_TABLE}trilinos\t| ${UTOPIA_TRILINOS}\t\t| basic algebra usable\n"
+    )
+    # SET(DEP_TABLE "${DEP_TABLE}openblas\t| ${OPEN_BLAS_FOUND}\t\t| usable\n")
+    set(DEP_TABLE "${DEP_TABLE}lapack\t\t| ${UTOPIA_LAPACK}\t\t| usable\n")
+    set(DEP_TABLE "${DEP_TABLE}umfpack\t\t| ${UTOPIA_UMFPACK}\t\t| disabled\n")
+    # SET(DEP_TABLE "${DEP_TABLE}cuda+thrust\t| ${UTOPIA_CUDA}\t\t| not usable
+    # but can be easily developed further\n") SET(DEP_TABLE
+    # "${DEP_TABLE}opencl\t\t| ${UTOPIA_OPENCL}\t\t| prototyping\n")
+    set(DEP_TABLE
+        "${DEP_TABLE}m3elinsol\t| ${UTOPIA_M3ELINSOL}\t\t| work in progress\n")
+    set(DEP_TABLE
+        "${DEP_TABLE}____________________________________________________\n")
+    message(STATUS ${DEP_TABLE})
+endmacro()
+
+# ##############################################################################
+function(utopia_add_library libraryRootDir subDirs)
+    set(THIS_MODULE_SOURCES "")
+    set(THIS_MODULE_HEADERS "")
+    find_project_files(${libraryRootDir} "${subDirs}" THIS_MODULE_HEADERS
+                       THIS_MODULE_SOURCES)
+    target_sources(
+        utopia
+        PRIVATE ${THIS_MODULE_SOURCES}
+        PRIVATE ${THIS_MODULE_HEADERS})
+
+    install(FILES ${THIS_MODULE_HEADERS} DESTINATION include)
+    foreach(MODULE ${subDirs})
+        target_include_directories(
+            utopia BEFORE PUBLIC $<BUILD_INTERFACE:${libraryRootDir}/${MODULE}>)
+    endforeach(MODULE)
+endfunction()
+
+# ##############################################################################
+
+function(find_project_files rootPath dirPaths headers sources)
+    set(verbose TRUE)
+
+    set(theaders ${${headers}})
+    set(tsources ${${sources}})
+
+    foreach(INCLUDE_PATH ${dirPaths})
+        # INCLUDE_DIRECTORIES(${rootPath}/${INCLUDE_PATH})
+
+        file(GLOB TEMP_HPPSRC CONFIGURE_DEPENDS
+             "${rootPath}/${INCLUDE_PATH}/*.cpp")
+        file(GLOB TEMP_SRC CONFIGURE_DEPENDS "${rootPath}/${INCLUDE_PATH}/*.c")
+        file(GLOB TEMP_HPPDR CONFIGURE_DEPENDS
+             "${rootPath}/${INCLUDE_PATH}/*.hpp")
+        file(GLOB TEMP_HDR CONFIGURE_DEPENDS "${rootPath}/${INCLUDE_PATH}/*.h")
+
+        source_group(
+            ${INCLUDE_PATH} FILES ${TEMP_HPPDR}; ${TEMP_HDR}; ${TEMP_HPPSRC};
+                                  ${TEMP_SRC}; ${TEMP_UI})
+
+        set(tsources ${tsources}; ${TEMP_SRC}; ${TEMP_HPPSRC})
+        set(theaders ${theaders}; ${TEMP_HDR}; ${TEMP_HPPDR})
+    endforeach(INCLUDE_PATH)
+
+    set(${headers}
+        ${theaders}
+        PARENT_SCOPE)
+    set(${sources}
+        ${tsources}
+        PARENT_SCOPE)
+endfunction()
+
+# ##############################################################################
+
+function(append_list_to_string_for_makefile OUTPUT_STRING PRE_LIST POST_LIST
+         ITEM_PREFIX LIST_OF_STRINGS)
+    # http://stackoverflow.com/questions/5248749/passing-a-list-to-a-cmake-macro
+    list(REMOVE_DUPLICATES LIST_OF_STRINGS)
+
+    set(TEMPSTR ${${OUTPUT_STRING}} ${PRE_LIST})
+
+    foreach(LIST_EL ${LIST_OF_STRINGS})
+
+        string(REGEX MATCH "(.*)framework" IS_FRAME_WORK "${LIST_EL}")
+        if(IS_FRAME_WORK)
+            get_filename_component(FRAMEWORK_NAME "${LIST_EL}" NAME)
+            string(REGEX REPLACE "/${FRAMEWORK_NAME}" "" FRAMEWORK_PATH
+                                 "${LIST_EL}")
+            string(REGEX REPLACE ".framework" "" FRAMEWORK_NAME
+                                 "${FRAMEWORK_NAME}")
+            set(TEMPSTR
+                "${TEMPSTR}-F${FRAMEWORK_PATH} -framework ${FRAMEWORK_NAME} ")
+        else()
+            set(TEMPSTR "${TEMPSTR}${ITEM_PREFIX}${LIST_EL} ")
+        endif()
+    endforeach(LIST_EL)
+
+    set(TEMPSTR "${TEMPSTR} ${POST_LIST}")
+    set(${OUTPUT_STRING}
+        ${TEMPSTR}
+        PARENT_SCOPE)
+endfunction()
+
+# ##############################################################################
+
+function(append_list_to_string_for_cmake OUTPUT_STRING PRE_LIST POST_LIST
+         LIST_OF_STRINGS)
+    # http://stackoverflow.com/questions/5248749/passing-a-list-to-a-cmake-macro
+
+    list(REMOVE_DUPLICATES LIST_OF_STRINGS)
+
+    set(TEMPSTR ${${OUTPUT_STRING}} ${PRE_LIST})
+
+    foreach(LIST_EL ${LIST_OF_STRINGS})
+        set(TEMPSTR "${TEMPSTR}\t\"${LIST_EL}\"\n")
+    endforeach(LIST_EL)
+
+    set(TEMPSTR "${TEMPSTR} ${POST_LIST}")
+    set(${OUTPUT_STRING}
+        ${TEMPSTR}
+        PARENT_SCOPE)
+endfunction()
---
+++ utopia/cmake/UtopiaOptions.cmake
@@ -0,0 +1,68 @@
+# option(UTOPIA_ENABLE_VC "SIMD wrapper" ON)
+option(UTOPIA_ENABLE_VC "SIMD wrapper" OFF)
+option(UTOPIA_STATIC_DEPENDENCIES_ONLY
+       "Allows to restrict the linking to static libraries" OFF)
+
+option(UTOPIA_ENABLE_MPI "Enable the cuda backend" ON)
+# option(UTOPIA_ENABLE_OPENCL "Enable the opencl backend" OFF)
+option(UTOPIA_ENABLE_PETSC "Enable the petsc backend" ON)
+option(UTOPIA_ENABLE_SLEPC "Enable the slepc backend" OFF)
+option(UTOPIA_ENABLE_METIS "Enable the Metis backend" OFF)
+option(UTOPIA_ENABLE_PARMETIS "Enable the ParMetis backend" OFF)
+option(UTOPIA_ENABLE_TRILINOS "Enable the Trilinos backend" ON)
+# option(UTOPIA_ENABLE_KOKKOS_SIMD "Enable kokkos intriniscs wrapper" OFF)
+option(UTOPIA_ENABLE_BLAS "Enable the blas backend" ON)
+option(UTOPIA_ENABLE_M3ELINSOL
+       "Enable the m3elinsol algebraic multigrid backend" OFF)
+
+option(UTOPIA_ENABLE_TRACE "enables utopia tracing facilities for regions" OFF)
+option(UTOPIA_ENABLE_TRACE_EXPR
+       "enables utopia tracing facilities for every expression" OFF)
+
+option(UTOPIA_ENABLE_NO_ALLOC_REGIONS"enables utopia alloc-check facilities"
+       OFF)
+option(UTOPIA_ENABLE_SANITIZER "Enable clang -fsanitize=address flag" OFF)
+# option(UTOPIA_ENABLE_PASSO_EXTENSIONS "Enable non-standard petsc solvers
+# developed in the PASSO library" OFF)
+option(UTOPIA_ENABLE_CXX14_FEATURES "Enable usage of cxx14 standard" ON)
+option(UTOPIA_ENABLE_CXX17_FEATURES "Enable usage of cxx17 standard" ON)
+
+option(UTOPIA_ENABLE_MOOSE_ENV_MODE
+       "Allows to use the moose installation compilers" OFF)
+option(UTOPIA_ENABLE_DEPRECATED_API
+       "Decprecated functionality that will be removed in the future" ON)
+option(UTOPIA_ENABLE_TINY_EXPR "String expressions support" ON)
+option(UTOPIA_ENABLE_RAPIDJSON "Enable support for JSON input files" ON)
+option(UTOPIA_ENABLE_YAML_CPP "Enable YAML support" ON)
+
+option(UTOPIA_ENABLE_CLANG_TIDY "Use clang tidy static analyzer" OFF)
+option(
+  UTOPIA_ENABLE_PETSC_DM_PLEX
+  "For the moment is to allow the \"make petsc\" command to install DMPlex dependencies"
+  OFF)
+option(UTOPIA_PULL_REQUEST_MODE
+       "Mode for ensuring that pull request standards are respected" OFF)
+
+option(UTOPIA_ENABLE_EIGEN_3 "Look for eigen for comparing performance" OFF)
+option(UTOPIA_BUILD_DOCUMENTATION
+       "Use Doxygen to create the HTML based API documentation" ON)
+
+option(UTOPIA_ENABLE_SCRIPTING
+       "Enable exports for other languages (C, Python, etc...)" OFF)
+
+option(UTOPIA_INSTALL_TRILINOS "Install trilinos directly" ON)
+option(UTOPIA_INSTALL_PETSC "Install petsc directly" ON)
+option(UTOPIA_INSTALL_PETSC_DEBUG "Install petsc directly" ON)
+option(
+  UTOPIA_ENABLE_GPERFTOOLS
+  "use Google perf tools (https://gperftools.github.io/gperftools/cpuprofile.html)"
+  OFF)
+
+option(UTOPIA_SPACK_MODE "Spack env hacks" OFF)
+
+option(BUILD_SHARED_LIBS "Build shared libraries instead of static" OFF)
+option(UTOPIA_REMOVE_TRILINOS_DEPRECATED_CODE
+       "Removed trilinos deprecated code" OFF)
+
+option(UTOPIA_ENABLE_FLUYA_MODE "Create utopia configuration required by Fluya"
+       OFF)
