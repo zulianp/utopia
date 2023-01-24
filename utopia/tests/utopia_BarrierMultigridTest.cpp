e239748a235c5f03075074f5fa4940b87a33071d
zulianp <patrick.zulian@gmail.com>
2023-01-24 14:19:43 +0100

    Fixed compilation bugs when petsc not present

--- utopia/tests/utopia_BarrierMultigridTest.cpp
+++ utopia/tests/utopia_BarrierMultigridTest.cpp
@@ -1,5 +1,9 @@
 #include "utopia_Testing.hpp"
 
+#include "utopia_Base.hpp"
+
+#ifdef UTOPIA_WITH_PETSC
+
 #include "utopia.hpp"
 
 #include "test_problems/utopia_QPSolverTestProblem.hpp"
@@ -201,3 +205,5 @@ namespace utopia {
 
     UTOPIA_REGISTER_TEST_FUNCTION(barrier_mg);
 }  // namespace utopia
+
+#endif 
