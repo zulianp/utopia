f4818d2c4af683bdebc322ec52da5c1472a70def
Dylan Ramelli <rameld@usi.ch>
2023-02-20 16:45:27 +0100

    more dependency table

--- utopia/cmake/UtopiaFunctions.cmake
+++ utopia/cmake/UtopiaFunctions.cmake
@@ -26,19 +26,17 @@ macro(print_dependency_table)
     set(DEP_TABLE
         "${DEP_TABLE}------------------------\n")
     set(DEP_TABLE
-        "${DEP_TABLE}backend\t\t| active\n------------------------\n"
+        "${DEP_TABLE}backend\t\t| status| location\t\t| version\n------------------------\n"
     )
 
-    set(DEP_TABLE "${DEP_TABLE}mpi\t\t| ${MPI_DIR}\n")
-    set(DEP_TABLE "${DEP_TABLE}petsc\t\t| ${UTOPIA_PETSC}\n")
-    set(DEP_TABLE "${DEP_TABLE}slepc\t\t| ${UTOPIA_SLEPC}\n")
-    set(DEP_TABLE "${DEP_TABLE}blas\t\t| ${UTOPIA_BLAS}\n")
-    set(DEP_TABLE
-        "${DEP_TABLE}trilinos\t| ${UTOPIA_TRILINOS}\n"
-    )
+    set(DEP_TABLE "${DEP_TABLE}mpi\t\t| ${UTOPIA_MPI}| ${UTOPIA_MPI_DIR}| ${UTOPIA_MPI_VERSION}\n")
+    set(DEP_TABLE "${DEP_TABLE}petsc\t\t| ${UTOPIA_PETSC}| ${UTOPIA_PETSC_DIR}| ${UTOPIA_PETSC_VERSION}\n")
+    set(DEP_TABLE "${DEP_TABLE}slepc\t\t| ${UTOPIA_SLEPC}| ${UTOPIA_SLEPC_DIR}| ${UTOPIA_SLEPC_VERSION}\n")
+    set(DEP_TABLE "${DEP_TABLE}trilinos\t| ${UTOPIA_TRILINOS}| ${UTOPIA_TRILINOS_DIR}| ${UTOPIA_TRILINOS_VERSION}\n")
     # SET(DEP_TABLE "${DEP_TABLE}openblas\t| ${OPEN_BLAS_FOUND}\n")
-    set(DEP_TABLE "${DEP_TABLE}lapack\t\t| ${UTOPIA_LAPACK}\n")
-    set(DEP_TABLE "${DEP_TABLE}umfpack\t\t| ${UTOPIA_UMFPACK}\n")
+    set(DEP_TABLE "${DEP_TABLE}blas\t\t| ${UTOPIA_BLAS}| ${UTOPIA_BLAS_DIR}| ${UTOPIA_BLAS_VERSION}\n")
+    set(DEP_TABLE "${DEP_TABLE}lapack\t\t| ${UTOPIA_LAPACK}| ${UTOPIA_BLAS_DIR}| ${UTOPIA_BLAS_VERSION}\n")
+    set(DEP_TABLE "${DEP_TABLE}umfpack\t\t| ${UTOPIA_UMFPACK}| ${UTOPIA_BLAS_DIR}| ${UTOPIA_BLAS_VERSION}\n")
     # SET(DEP_TABLE "${DEP_TABLE}cuda+thrust\t| ${UTOPIA_CUDA}\t\t| not usable
     # but can be easily developed further\n") SET(DEP_TABLE
     # "${DEP_TABLE}opencl\t\t| ${UTOPIA_OPENCL}\t\t| prototyping\n")
