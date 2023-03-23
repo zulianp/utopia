22b9eeb2f753d3558b1a03d122605728993ec25c
edoardo <edoardo.pezzulli@erdw.ethz.ch>
2023-03-23 15:38:38 +0100

    Added yaml file and app for running CHZ model with Homogeneous bar without recompiling.

--- utopia/apps/PF_frac/utopia_franetg.cpp
+++ utopia/apps/PF_frac/utopia_franetg.cpp
@@ -145,7 +145,7 @@ namespace utopia {
         stats.start();
 
         MLIncrementalLoading<FunctionSpace,
-                             utopia::IsotropicGenericPhaseField<FunctionSpace,Dim,CHZ_Linear>,
+                             utopia::IsotropicGenericPhaseField<FunctionSpace,Dim,AT1>,
                              UniaxialLoading2D<FunctionSpace>,
                              HomogeneousBar<FunctionSpace>>
             time_stepper(space);
@@ -162,6 +162,50 @@ namespace utopia {
     }
 
     UTOPIA_REGISTER_APP(HomogeneousBarPseudo1D);
+
+    // // // // // // // // // // // // // // // // // // // // // // // // // // //
+    // // // // // // // // //
+    static void HomogeneousBarPseudo1D_CHZ(Input &in) {
+        static const int Dim = 2;
+        static const int NVars = Dim + 1;
+
+        using Comm = utopia::PetscCommunicator;
+        using Mesh = utopia::PetscStructuredGrid<Dim>;
+        using Elem = utopia::PetscUniformQuad4;
+        using FunctionSpace = utopia::FunctionSpace<Mesh, NVars, Elem>;
+
+        Comm world;
+
+        MPITimeStatistics stats(world);
+        stats.start();
+
+        FunctionSpace space;
+        space.read(in);
+        stats.stop_and_collect("space-creation");
+
+        if (mpi_world_rank() == 0)
+            std::cout << "Starting HomogeneousBarPseudo1D Model" << std::endl;
+
+        stats.start();
+
+        MLIncrementalLoading<FunctionSpace,
+                             utopia::IsotropicGenericPhaseField<FunctionSpace,Dim,CHZ_Linear>,
+                             UniaxialLoading2D<FunctionSpace>,
+                             HomogeneousBar<FunctionSpace>>
+            time_stepper(space);
+
+        time_stepper.read(in);
+        time_stepper.run();
+
+        stats.stop_collect_and_restart("end");
+
+        space.comm().root_print(std::to_string(space.n_dofs()) + " dofs");
+        stats.stop_and_collect("output");
+        stats.describe(std::cout);
+
+    }
+
+    UTOPIA_REGISTER_APP(HomogeneousBarPseudo1D_CHZ);
 
 
     // // // // // // // // // // // // // // // // // // // // // // // // // // //
