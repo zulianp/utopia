1a6232da5de3af14f82ecd52643dda76085f948a
Dylan Ramelli <rameld@usi.ch>
2023-04-05 11:01:03 +0200

    added local install script

---
+++ utopia/scripts/make_local_install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+today=$(date)
+printf "%s\n" "$today"
+printf "Testing Cmake Script Local Install of dependencies petsc and trilinos:\n"
+
+
+# Basic build, petsc, lapack.
+_basic_build(){
+	cmake .. -DUTOPIA_ENABLE_LOCAL_DEPENDENCIES_INSTALL=ON -DCMAKE_INSTALL_PREFIX=/Users/dylan/Documents/Summer-Internship/Installations/utopia
+
+}
+
+if [[ -d build_local_install ]]
+then
+	cd build_local_install
+	rm -rf *
+	_basic_build
+fi
+
+if [[ ! -d build_local_install ]]
+then
+	mkdir build_local_install
+	cd build_local_install
+	rm -rf *
+	_basic_build
+fi
