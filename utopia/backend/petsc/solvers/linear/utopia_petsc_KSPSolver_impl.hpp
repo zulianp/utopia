9597325c48f89c55d98fe4e8cb991a4216afdbb0
Patrick Zulian <patrick.zulian@gmail.com>
2023-09-12 15:30:14 +0200

    Added KSPSolver message

--- utopia/backend/petsc/solvers/linear/utopia_petsc_KSPSolver_impl.hpp
+++ utopia/backend/petsc/solvers/linear/utopia_petsc_KSPSolver_impl.hpp
@@ -1130,6 +1130,19 @@ namespace utopia {
         this->ksp_type(new_ksp_type);
         this->pc_type(new_pc_type);
         this->solver_package(new_solver_package);
+
+        if (this->verbose()) {
+            int rank = PetscCommunicator::world().rank();
+
+            if (!rank) {
+                utopia::out() "------------------------------------\n";
+                utopia::out() << "KSPSolver\n";
+                utopia::out() << "ksp_type:       " << ksp_type() << "\n";
+                utopia::out() << "pc_type:        " << pc_type() << "\n";
+                utopia::out() << "solver_package: " << solver_package() << "\n";
+                utopia::out() "------------------------------------\n";
+            }
+        }
     }
 
     template <typename Matrix, typename Vector>
