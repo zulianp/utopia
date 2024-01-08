77f3bf363a78ae6f37e5fd5ab66d64721f1ddf09
Dylan Ramelli <dylan.reid.ramelli@usi.ch>
2024-01-08 18:30:25 +0100

    ...

--- utopia_fe/cmake/FindParMoonolith.cmake
+++ utopia_fe/cmake/FindParMoonolith.cmake
@@ -8,11 +8,17 @@ if(UTOPIA_ENABLE_MOONOLITH)
         set(UTOPIA_MOONOLITH_SEARCH_PATHS "${UTOPIA_MOONOLITH_SEARCH_PATHS};$ENV{MOONOLITH_DIR};$ENV{MOONOLITH_DIR}/lib/cmake")
     endif()
 
+    if(UTOPIA_INSTALL_MOONOLITH)
+        set(UTOPIA_MOONOLITH_SEARCH_PATHS "${MOONOLITH_INSTALL_DIR}")
+        message(STATUS ${UTOPIA_MOONOLITH_SEARCH_PATHS})
+    endif()
+
     find_package(
         ParMoonolith
         CONFIG
         HINTS
         ${UTOPIA_MOONOLITH_SEARCH_PATHS}
+        PATH_SUFFIXES lib/cmake
         )
 
     if(ParMoonolith_FOUND)
@@ -33,56 +39,4 @@ if(UTOPIA_ENABLE_MOONOLITH)
     else()
         message(WARNING "Could not find ParMoonolith by config file")
     endif()
-
-    find_path(MOONOLITH_INSTALLATION_PATH NAME config/moonolith_config.cmake
-              HINTS ${MOONOLITH_DIR} $ENV{MOONOLITH_DIR})
-
-    if(MOONOLITH_INSTALLATION_PATH)
-        message(
-            STATUS
-                "Found moonolith installation at ${MOONOLITH_INSTALLATION_PATH}"
-        )
-        include(${MOONOLITH_INSTALLATION_PATH}/config/moonolith_config.cmake)
-        include(FindPackageHandleStandardArgs)
-
-        find_package_handle_standard_args(
-            MOONOLITH REQUIRED_VARS MOONOLITH_LIBRARIES MOONOLITH_INCLUDES)
-
-        mark_as_advanced(MOONOLITH_INCLUDES MOONOLITH_LIBRARIES)
-
-        if(MOONOLITH_FOUND)
-            add_custom_target(par_moonolith)
-        endif()
-    else()
-
-    endif()
-
 endif()
-
-# if(NOT MOONOLITH_FOUND OR FORCE_INSTALL_MOONOLITH)
-#     include(FetchContent)
-#     message(STATUS "Fetching par_moonolith, since it could not be found.")
-
-#     set(MOONOLITH_ENABLE_BENCHMARK
-#         OFF
-#         CACHE INTERNAL "")
-
-#     set(MOONOLITH_ENABLE_TESTING
-#         OFF
-#         CACHE INTERNAL "")
-
-#     FetchContent_Declare(
-#         moonolith
-#         GIT_REPOSITORY https://bitbucket.org/zulianp/par_moonolith.git
-#         # GIT_TAG origin/sampler
-#         GIT_TAG origin/development
-#     )
-    
-#     FetchContent_MakeAvailable(moonolith)
-
-#     add_library(ParMoonolith::par_moonolith ALIAS par_moonolith)
-
-#     list(APPEND UTOPIA_FE_LIBRARIES_TRAILING "par_moonolith")
-#     set(UTOPIA_FE_LIBRARIES_TRAILING ${UTOPIA_FE_LIBRARIES_TRAILING} PARENT_SCOPE)
-
-# endif()
