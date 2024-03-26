5737d8e53720777dca7e9fe8b4b6c400196b5155
Dylan Ramelli <dylan.reid.ramelli@usi.ch>
2024-03-26 13:41:50 +0100

    ...

--- utopia/scripts/setup_fluya_env_daint.sh
+++ utopia/scripts/setup_fluya_env_daint.sh
@@ -12,7 +12,9 @@ module load cray-netcdf-hdf5parallel
 
 
 # Temporary.
-# export STAGE_DIR=$SCRATCH/code
-# export INSTALL_DIR=$STAGE_DIR/installations
-# export UTOPIA_DIR=$INSTALL_DIR/utopia_fluya
-# export MOONOLITH_DIR=$INSTALL_DIR/par_moonolith
+export STAGE_DIR=$SCRATCH/code
+export INSTALL_DIR=$STAGE_DIR/installations
+export UTOPIA_DIR=$INSTALL_DIR/utopia_fluya
+export MOONOLITH_DIR=$INSTALL_DIR/par_moonolith
+export UTOPIA_FE_EXEC=$INSTALL_DIR/utopia_fluya/utopia_fe_exec
+export TRILINOS_DIR=$STAGE_DIR/utopia/external/Trilinos
