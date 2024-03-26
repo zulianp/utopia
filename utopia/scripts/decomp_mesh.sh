05c381d8d12cbb607cc703c758c5c43239e4e005
Dylan Ramelli <dylan.reid.ramelli@usi.ch>
2024-03-26 14:41:47 +0100

    ...

--- utopia/scripts/decomp_mesh.sh
+++ utopia/scripts/decomp_mesh.sh
@@ -1,4 +1,4 @@
 n_procs=$1
 
-$TRILINOS_DIR/bin/decomp -p $n_procs mesh_matrix.e
-$TRILINOS_DIR/bin/decomp -p $n_procs mesh_fracture.e
+srun $TRILINOS_DIR/bin/decomp -p $n_procs mesh_matrix.e
+srun $TRILINOS_DIR/bin/decomp -p $n_procs mesh_fracture.e
