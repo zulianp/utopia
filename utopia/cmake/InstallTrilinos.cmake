62a84e0d67e6e839460294a926805f8b9f96ba84
Dylan Ramelli <dylan.reid.ramelli@usi.ch>
2024-04-16 10:58:58 +0200

    ...

--- utopia/cmake/InstallTrilinos.cmake
+++ utopia/cmake/InstallTrilinos.cmake
@@ -137,7 +137,7 @@ if(NOT Trilinos_FOUND)
       "-DKokkos_ENABLE_CUDA=ON"
       "-DKokkos_ENABLE_CUDA_CONSTEXPR=ON"
       "-DKokkos_ENABLE_CUDA_LAMBDA=ON"
-      "-DCMAKE_CXX_STANDARD=17"
+      "-DCMAKE_CXX_STANDARD=14"
       "-DKokkos_ARCH_PASCAL61=ON "
       "-DTpetra_INST_CUDA=ON")
   endif()
