c4e30a086106e178ba3cf496c63f214d10e5fa66
Dylan Ramelli <dylan.reid.ramelli@usi.ch>
2024-04-30 13:32:52 +0200

    changed core folder to utopia_core for cluster fix

--- utopia/scripts/setup_fluya_env_daint.sh
+++ utopia/scripts/setup_fluya_env_daint.sh
@@ -1,21 +1,35 @@
 #!/bin/bash
 
-module load daint-mc
-
-# TODO: Add if statement.
-module switch PrgEnv-cray PrgEnv-gnu
-module load CMake
-module load gcc
-module load cray-mpich
-module load cray-hdf5-parallel
-module load cray-netcdf-hdf5parallel
-
+# Setup environment for franetg.
+source /apps/daint/UES/anfink/gpu_2023-04-17_Release/environment
 
 # Temporary.
 export STAGE_DIR=$SCRATCH/code
 export INSTALL_DIR=$STAGE_DIR/installations
-export UTOPIA_DIR=$INSTALL_DIR/utopia_fluya
-export MOONOLITH_DIR=$INSTALL_DIR/par_moonolith
-export UTOPIA_FE_EXEC=$INSTALL_DIR/utopia_fe_fluya/bin/utopia_fe_exec
-export TRILINOS_DIR=$STAGE_DIR/utopia/external/Trilinos
+export METIS_DIR=$INSTALL_DIR/metis
+export UTOPIA_DIR=$INSTALL_DIR/utopia_fluya_gpu
+export UTOPIA_FE_EXEC=$INSTALL_DIR/utopia_fe_fluya_gpu/bin/utopia_fe_exec
+export SuperLU_DIR=$INSTALL_DIR/superlu
+export CRAYPE_LINK_TYPE=dynamic
+export HDF5_DIR=$HDF5_ROOT
+
+
+# module load daint-mc
+
+# # TODO: Add if statement.
+# module switch PrgEnv-cray PrgEnv-gnu
+# module load CMake
+# module load gcc
+# module load cray-mpich
+# module load cray-hdf5-parallel
+# module load cray-netcdf-hdf5parallel
+
+
+# # Temporary.
+# export STAGE_DIR=$SCRATCH/code
+# export INSTALL_DIR=$STAGE_DIR/installations
+# export UTOPIA_DIR=$INSTALL_DIR/utopia_fluya
+# export MOONOLITH_DIR=$INSTALL_DIR/par_moonolith
+# export UTOPIA_FE_EXEC=$INSTALL_DIR/utopia_fe_fluya/bin/utopia_fe_exec
+# export TRILINOS_DIR=$STAGE_DIR/utopia/external/Trilinos
 # export KOKKO_DIR=$TRILINOS_DIR/lib/cmake/Kokkos
