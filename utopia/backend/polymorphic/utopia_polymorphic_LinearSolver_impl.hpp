301c4d82282e19599e4ea4597b9dbf28651e15c5
Patrick Zulian <patrick.zulian@gmail.com>
2024-06-28 16:19:14 +0200

    ....

--- utopia/backend/polymorphic/utopia_polymorphic_LinearSolver_impl.hpp
+++ utopia/backend/polymorphic/utopia_polymorphic_LinearSolver_impl.hpp
@@ -1,7 +1,16 @@
 #include "utopia_LinearSolverFactory.hpp"
+#include "utopia_PreconditionedSolverInterface.hpp"
 #include "utopia_polymorphic_LinearSolver.hpp"
 
 namespace utopia {
+
+#define UTOPIA_READ_ENV(name, conversion) \
+    do {                                  \
+        char *var = getenv(#name);        \
+        if (var) {                        \
+            name = conversion(var);       \
+        }                                 \
+    } while (0)
 
     template <class Matrix, class Vector>
     void OmniLinearSolver<Matrix, Vector>::read(Input &in) {
@@ -13,7 +22,33 @@ namespace utopia {
         // in.get("backend", backend);
         in.get("type", type);
 
+        // utopia::out() << "[OmniLinearSolver] type: " << type << "\n";
+
         impl_ = LinearSolverFactory<Matrix, Vector>::new_linear_solver(type);
+
+        std::string preconditioner = "";
+        in.get("precond", [&](Input &node) {
+            std::string preconditioner_type = "";
+            node.get("type", preconditioner_type);
+
+            // utopia::out() << "[OmniLinearSolver] preconditioner_type: " << preconditioner_type << "\n";
+
+            auto ps = dynamic_cast<PreconditionedSolverInterface<Vector> *>(impl_.get());
+
+            if (!ps) {
+                assert(false);
+                Utopia::Abort("[Error] OmniLinearSolver unable to pass preconditioner to solver!");
+            }
+
+            auto prec = LinearSolverFactory<Matrix, Vector>::new_linear_solver(preconditioner_type);
+            if (prec) {
+                ps->set_preconditioner(std::move(prec));
+            } else {
+                assert(false);
+                Utopia::Abort("[Error] OmniLinearSolver (no such preconditioner)");
+            }
+        });
+
         impl_->read(in);
     }
 
